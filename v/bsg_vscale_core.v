71dea088bcae77ea718bf4bc8d424b0431234fa4
proftaylor <prof.taylor@gmail.com>
2016-05-14 22:37:26 -0700

    remove hasti interface; use vscale_pipeline directly to eliminate stall bugs in vscale

--- v/bsg_vscale_core.v
+++ v/bsg_vscale_core.v
@@ -1,140 +1,118 @@
-module bsg_vscale_core
+module
+ bsg_vscale_core
+
 import bsg_vscale_pkg::*;
 
+   #(parameter x_cord_width_p = "inv"
+     , y_cord_width_p = "inv")
   ( input clk_i
    ,input reset_i
-   ,input stall_i
+   ,input freeze_i
 
    // to banked crossbar
    ,output [1:0]                          m_v_o
    ,output [1:0]                          m_w_o
    ,output [1:0] [haddr_width_p-1:0]      m_addr_o
    ,output [1:0] [hdata_width_p-1:0]      m_data_o
-   ,output [1:0] [(hdata_width_p>>3)-1:0] m_mask_o
+   ,output logic [1:0] [(hdata_width_p>>3)-1:0] m_mask_o
    ,input  [1:0]                          m_yumi_i
    ,input  [1:0]                          m_v_i
    ,input  [1:0] [hdata_width_p-1:0]      m_data_i
+
+   ,input   [x_cord_width_p-1:0] my_x_i
+   ,input   [y_cord_width_p-1:0] my_y_i
   );
 
-  // vscale core signals
-  logic [1:0][haddr_width_p-1:0]  haddr,     haddr_r;
-  logic [1:0]                     hwrite,    hwrite_r;
-  logic [1:0][hsize_width_p-1:0]  hsize,     hsize_r;
-  logic [1:0][hburst_width_p-1:0] hburst,    hburst_r;
-  logic [1:0]                     hmastlock, hmastlock_r;
-  logic [1:0][hprot_width_p-1:0]  hprot,     hprot_r;
-  logic [1:0][htrans_width_p-1:0] htrans,    htrans_r;
-  logic [1:0][hdata_width_p-1:0]  hwdata,    hwdata_r;
-  logic [1:0][hdata_width_p-1:0]  hrdata,    hrdata_r;
-  logic [1:0]                     hready,    hready_r;
-  logic [1:0][hresp_width_p-1:0]  hresp,     hresp_r;
-
-  logic stall_r;
-
-  always_ff @(posedge clk_i)
-  begin
-    if(reset_i)
-      stall_r <= 1'b0;
-    else
-      stall_r <= stall_i;
-
-    if(stall_i & ~stall_r)
-      begin
-        haddr_r     <= haddr;
-        hwrite_r    <= hwrite;
-        hsize_r     <= hsize;
-        hburst_r    <= hburst;
-        hmastlock_r <= hmastlock;
-        hprot_r     <= hprot;
-        htrans_r    <= htrans;
-        hwdata_r    <= hwdata;
-        hrdata_r    <= hrdata;
-        hready_r    <= hready;
-        hresp_r     <= hresp;
-      end
-  end
-
-   // htif
-   wire                         htif_id_i = 1'b0;
-   wire                         htif_pcr_req_valid_i = 1'b0;
-   wire                         htif_pcr_req_rw_i = 1'b0;
-   wire [csr_addr_width_p-1:0]  htif_pcr_req_addr_i = 0;
-   wire [htif_pcr_width_p-1:0]  htif_pcr_req_data_i = 0;
-   wire                         htif_pcr_resp_ready_i = 1'b1;
-   wire                         htif_ipi_req_ready_i = 1'b0;
-   wire                         htif_ipi_resp_valid_i = 1'b0;
-   wire                         htif_ipi_resp_data_i = 0;
+   if (0)
+     always @(negedge clk_i)
+       if (~freeze_i)
+         $display("%m v=%x addr=%x mask=%b yumi=%b vi=%x, data_i=%x "
+                  ,m_v_o[0], m_addr_o[0], m_mask_o[0], m_yumi_i[0], m_v_i[0], m_data_i[0]);
 
 
-   // synopsys translate off
+   assign m_data_o[0] = 0;
+
+   logic freeze_r;
+
+   always_ff @(posedge clk_i)
+     freeze_r <= freeze_i;
+
+   // always be fetching, apparently;
+   // may be better to be more precise about this signal
+   // based on more outputs from vscale pipeline
+
+   assign m_v_o    [0] = ~freeze_r;
+   assign m_w_o    [0] = 1'b0;
+   assign m_mask_o [0] = 4'b1111;
+
+   wire [`HASTI_SIZE_WIDTH-1:0]                    dmem_size;
+
+   // generate byte write mask
    always_comb
-        assert(hwrite[0] != 1) else $display("imem should never write");
-   // synopsys translate on
+     begin
+        m_mask_o[1] = 4'b0000;
 
-  vscale_core vscale( .clk                   (clk_i)
-                     ,.imem_haddr            (haddr[0])
-                     ,.imem_hwrite           (hwrite[0])
-                     ,.imem_hsize            (hsize[0])
-                     ,.imem_hburst           (hburst[0])
-                     ,.imem_hmastlock        (hmastlock[0])
-                     ,.imem_hprot            (hprot[0])
-                     ,.imem_htrans           (htrans[0])
-                     ,.imem_hwdata           (hwdata[0])
-                     ,.imem_hrdata           (stall_r ? hrdata_r[0] : hrdata[0])
-                     ,.imem_hready           ((stall_r & ~stall_i) ? hready_r[0] : (~stall_i & hready[0]))
-                     ,.imem_hresp            (stall_r ? hresp_r[0] : hresp[0])
-                     ,.dmem_haddr            (haddr[1])
-                     ,.dmem_hwrite           (hwrite[1])
-                     ,.dmem_hsize            (hsize[1])
-                     ,.dmem_hburst           (hburst[1])
-                     ,.dmem_hmastlock        (hmastlock[1])
-                     ,.dmem_hprot            (hprot[1])
-                     ,.dmem_htrans           (htrans[1])
-                     ,.dmem_hwdata           (hwdata[1])
-                     ,.dmem_hrdata           (stall_r ? hrdata_r[1] : hrdata[1])
-                     ,.dmem_hready           ((stall_r & ~stall_i) ? hready_r[1] : (~stall_i & hready[1]))
-                     ,.dmem_hresp            (stall_r ? hresp_r[1] : hresp[1])
-                     ,.htif_reset            (reset_i)
-                     ,.htif_id               (htif_id_i)
-                     ,.htif_pcr_req_valid    (htif_pcr_req_valid_i)
-                     ,.htif_pcr_req_ready    ()
-                     ,.htif_pcr_req_rw       (htif_pcr_req_rw_i)
-                     ,.htif_pcr_req_addr     (htif_pcr_req_addr_i)
-                     ,.htif_pcr_req_data     (htif_pcr_req_data_i)
-                     ,.htif_pcr_resp_valid   ()
-                     ,.htif_pcr_resp_ready   (htif_pcr_resp_ready_i)
-                     ,.htif_pcr_resp_data    ()
-                     ,.htif_ipi_req_ready    (htif_ipi_req_ready_i)
-                     ,.htif_ipi_req_valid    ()
-                     ,.htif_ipi_req_data     ()
-                     ,.htif_ipi_resp_ready   ()
-                     ,.htif_ipi_resp_valid   (htif_ipi_resp_valid_i)
-                     ,.htif_ipi_resp_data    (htif_ipi_resp_data_i)
-                     ,.htif_debug_stats_pcr  ()
-                    );
+        case (dmem_size)
+          2: m_mask_o[1] = 4'b1111;
+          1: m_mask_o[1] = { m_addr_o[1][1],    m_addr_o[1][1], ~m_addr_o[1][1], ~m_addr_o[1][1] };
+          0: m_mask_o[1] = { &m_addr_o[1][1:0]
+                             ,  m_addr_o[1][1] & ~m_addr_o[1][0]
+                             , ~m_addr_o[1][1] &  m_addr_o[1][0]
+                             , ~(|m_addr_o[1][1:0])
+                             };
+         default:
+          if (m_v_o[1])
+            $error("%m unhandled dmem size %x", dmem_size);
+        endcase // unique case dmem_size
+     end // always_comb
 
-  bsg_vscale_hasti_converter hasti_converter
-    ( .clk_i       (clk_i)
-     ,.reset_i     (reset_i)
-     ,.haddr_i     (stall_r ? haddr_r     : haddr)
-     ,.hwrite_i    (stall_r ? hwrite_r    : hwrite)
-     ,.hsize_i     (stall_r ? hsize_r     : hsize)
-     ,.hburst_i    (stall_r ? hburst_r    : hburst)
-     ,.hmastlock_i (stall_r ? hmastlock_r : hmastlock)
-     ,.hprot_i     (stall_r ? hprot_r     : hprot)
-     ,.htrans_i    (stall_i ? {2{htrans_idle_p}} : (stall_r ? htrans_r : htrans))
-     ,.hwdata_i    (stall_r ? hwdata_r    : hwdata)
-     ,.hrdata_o    (hrdata)
-     ,.hready_o    (hready)
-     ,.hresp_o     (hresp)
-     ,.m_v_o       (m_v_o   )
-     ,.m_w_o       (m_w_o   )
-     ,.m_addr_o    (m_addr_o)
-     ,.m_data_o    (m_data_o)
-     ,.m_mask_o    (m_mask_o)
-     ,.m_yumi_i    (m_yumi_i)
-     ,.m_v_i       (m_v_i   )
-     ,.m_data_i    (m_data_i)
+   always @(negedge clk_i)
+     if (~reset_i & m_v_o[1] )
+       assert (~(dmem_size == 2 && (|m_addr_o[1][1:0]))
+               && ~(dmem_size == 1 && (|m_addr_o[1][0])))
+         else $error("%m unaligned access to %x",m_addr_o[1]);
+
+   // the vscale_pipeline wants a _ready signal
+   // but the crossbar requires a yumi signal
+
+   logic imem_wait;
+
+   assign imem_wait = ~m_yumi_i[0];
+
+   vscale_pipeline #(.x_cord_width_p(x_cord_width_p)
+                     ,.y_cord_width_p(y_cord_width_p))
+                     vscale
+                     (.clk  (clk_i)
+                      ,.reset(reset_i)
+                      ,.freeze(freeze_i)
+
+                      ,.imem_wait     (imem_wait) // i
+
+                      ,.imem_addr     (m_addr_o[0])  // o
+                      ,.imem_rdata    (m_data_i[0])  // i
+                      ,.imem_badmem_e (1'b0)         // i
+
+                      ,.dmem_wait     (~m_yumi_i[1]) // i
+                      ,.dmem_en       (m_v_o[1])     // o
+                      ,.dmem_wen      (m_w_o[1])     // o
+                      ,.dmem_size     (dmem_size)    // o
+                      ,.dmem_addr     (m_addr_o[1])  // o
+                      ,.dmem_wdata    (m_data_o[1])  // o
+                      ,.dmem_rdata    (m_data_i[1])  // i
+                      ,.dmem_badmem_e(1'b0)          // i
+
+                      ,.htif_reset         (reset_i)
+                      ,.htif_pcr_req_valid (1'b0)
+                      ,.htif_pcr_req_ready ()
+                      ,.htif_pcr_req_rw    (1'b0)
+                      ,.htif_pcr_req_addr  ()
+                      ,.htif_pcr_req_data  ()
+                      ,.htif_pcr_resp_valid()
+                      ,.htif_pcr_resp_ready(1'b1)
+                      ,.htif_pcr_resp_data ()
+                      ,.my_x_i(my_x_i)
+                      ,.my_y_i(my_y_i)
     );
+
 
 endmodule
