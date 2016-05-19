7c21937f091c41f2d397d4817c6519faab28ba91
proftaylor <prof.taylor@gmail.com>
2016-05-19 16:09:24 -0700

    extend test

--- software/spmd/remote_byte/hello.c
+++ software/spmd/remote_byte/hello.c
@@ -9,8 +9,11 @@ int bsg_y = 0;
 
 int main()
 {
-  bsg_remote_store_char(0,0,&t[3],1);
-  bsg_wait_while(bsg_volatile_access(foo) != 0x01FFFFFF);
+  bsg_remote_store_char(0,0,&t[3],3);
+  bsg_remote_store_char(0,0,&t[2],2);
+  bsg_remote_store_char(0,0,&t[1],1);
+  bsg_remote_store_char(0,0,&t[0],0);
+  bsg_wait_while(bsg_volatile_access(foo) != 0x03020100);
 
   bsg_finish();
 }
