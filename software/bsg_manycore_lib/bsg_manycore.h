295deb1e940b5c5a215b035c20b648768c968e43
proftaylor <prof.taylor@gmail.com>
2016-05-19 19:04:53 -0700

    add some macros for remote char/short writes

--- software/bsg_manycore_lib/bsg_manycore.h
+++ software/bsg_manycore_lib/bsg_manycore.h
@@ -1,4 +1,9 @@
-typedef volatile int  *bsg_remote_int_ptr;
+#ifndef _BSG_MANYCORE_H
+#define _BSG_MANYCORE_H
+
+typedef volatile int   *bsg_remote_int_ptr;
+typedef volatile unsigned char  *bsg_remote_uint8_ptr;
+typedef volatile unsigned short  *bsg_remote_uint16_ptr;
 typedef volatile void *bsg_remote_void_ptr;
 
 
@@ -65,6 +70,9 @@ typedef volatile void *bsg_remote_void_ptr;
 
 #define bsg_remote_store(x,y,local_addr,val) do { *(bsg_remote_ptr((x),(y),(local_addr))) = (int) (val); } while (0)
 
+#define bsg_remote_store_uint8(x,y,local_addr,val)  do { *((bsg_remote_uint8_ptr)  (bsg_remote_ptr((x),(y),(local_addr)))) = (unsigned char) (val); } while (0)
+#define bsg_remote_store_uint16(x,y,local_addr,val) do { *((bsg_remote_uint16_ptr) (bsg_remote_ptr((x),(y),(local_addr)))) = (unsigned short) (val); } while (0)
+
 #define bsg_remote_control_store(x,y,local_addr,val) bsg_remote_store((x),(y), (1 << (bsg_remote_addr_bits-1))+(local_addr),(val))
 #define bsg_remote_unfreeze(x,y) bsg_remote_control_store((x),(y),0,0)
 #define bsg_remote_freeze(x,y)   bsg_remote_control_store((x),(y),0,1)
@@ -79,14 +87,17 @@ typedef volatile void *bsg_remote_void_ptr;
 #define bsg_finish()       do {  bsg_remote_int_ptr ptr = bsg_remote_ptr_io(0,0xDEAD0); *ptr = ((bsg_y << 16) + bsg_x); while (1); } while(0)
 #define bsg_print_time()   do {  bsg_remote_int_ptr ptr = bsg_remote_ptr_io(0,0xDEAD4); *ptr = ((bsg_y << 16) + bsg_x); } while(0)
 
-#define bsg_id_to_x(id) (id % bsg_tiles_X)
-#define bsg_id_to_y(id) (id / bsg_tiles_X)
+#define bsg_id_to_x(id)    (id % bsg_tiles_X)
+#define bsg_id_to_y(id)    (id / bsg_tiles_X)
+#define bsg_x_y_to_id(x,y) (bsg_tiles_X*y + x)
 #define bsg_num_tiles (bsg_tiles_X*bsg_tiles_Y)
 
 // later, we can add some mechanisms to save power
 #define bsg_wait_while(cond) do {} while ((cond))
 
-#define bsg_volatile_access(var) (*((bsg_remote_int_ptr) (&(var))))
+#define bsg_volatile_access(var)        (*((bsg_remote_int_ptr) (&(var))))
+#define bsg_volatile_access_uint16(var) (*((bsg_remote_uint16_ptr) (&(var))))
+#define bsg_volatile_access_uint8(var)  (*((bsg_remote_uint8_ptr) (&(var))))
 
 // prevents compiler from reordering memory operations across
 // this line in the code
@@ -94,3 +105,7 @@ typedef volatile void *bsg_remote_void_ptr;
 // see also atomic_signal_fence(std::memory_order_seq_cst) for C11
 //
 #define bsg_compiler_memory_barrier() asm volatile("" ::: "memory")
+
+#define bsg_commit_stores() do { /* fixme: add commit stores instr */  bsg_compiler_memory_barrier() } while (0)
+
+#endif
