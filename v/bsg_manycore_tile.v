a0d5ee28410e74e4623aa0362fdd5fe4de338917
Shaolin_Xie <shx089@eng.ucsd.edu>
2016-09-11 12:23:49 -0700

    modified the router instation according to the changed bsg_mesh_router_buffered interface

--- v/bsg_manycore_tile.v
+++ v/bsg_manycore_tile.v
@@ -1,4 +1,5 @@
 `include "bsg_manycore_packet.vh"
+`include "bsg_noc_links.vh"
 
 `ifdef bsg_FPU
  `include "float_definitions.v"
@@ -49,6 +50,33 @@ import bsg_noc_pkg::*; // {P=0, W, E, N, S}
    logic [packet_width_lp-1:0] proc_to_router_data, router_to_proc_data;
    logic                       proc_to_router_ready, router_to_proc_ready, proc_to_router_v, router_to_proc_v;
 
+   `declare_bsg_ready_and_link_sif_s(packet_width_lp,bsg_ready_and_link_sif_s);
+    
+   //The processor will add one direction;
+   bsg_ready_and_link_sif_s [dirs_p:0] link_i_cast, link_o_cast;
+
+   ///////////////////////////////////////////////
+    genvar i;
+    for( i=1; i<dirs_p +1; i++ ) 
+    begin
+        assign link_i_cast[i].v                 = v_i       [ i-1 ];
+        assign link_i_cast[i].ready_and_rev     = ready_i   [ i-1 ];
+        assign link_i_cast[i].data              = data_i    [ i-1 ];
+
+        assign  v_o       [ i-1 ]   =link_o_cast[i].v                 ;
+        assign  ready_o   [ i-1 ]   =link_o_cast[i].ready_and_rev     ;
+        assign  data_o    [ i-1 ]   =link_o_cast[i].data              ;
+    end 
+
+    assign link_i_cast[0].v                 = proc_to_router_v      ;
+    assign link_i_cast[0].ready_and_rev     = router_to_proc_ready  ;
+    assign link_i_cast[0].data              = proc_to_router_data   ;
+
+    assign router_to_proc_v         = link_o_cast[0].v              ;  
+    assign proc_to_router_ready     = link_o_cast[0].ready_and_rev  ; 
+    assign router_to_proc_data      = link_o_cast[0].data           ; 
+   ///////////////////////////////////////////////
+
    bsg_mesh_router_buffered #(.width_p(packet_width_lp)
                               ,.x_cord_width_p(x_cord_width_p)
                               ,.y_cord_width_p(y_cord_width_p)
@@ -58,14 +86,8 @@ import bsg_noc_pkg::*; // {P=0, W, E, N, S}
                               ) bmrb
      (.clk_i   (clk_i)
       ,.reset_i(reset_i)
-      ,.v_i     ({ v_i,     proc_to_router_v}    )
-      ,.data_i  ({ data_i,  proc_to_router_data })
-      ,.ready_o ({ ready_o, proc_to_router_ready})
-
-      ,.v_o     ({ v_o,     router_to_proc_v}    )
-      ,.data_o  ({ data_o,  router_to_proc_data} )
-      ,.ready_i ({ ready_i, router_to_proc_ready})
-
+      ,.link_i (link_i_cast )
+      ,.link_o (link_o_cast ) 
       ,.my_x_i
       ,.my_y_i
       );
