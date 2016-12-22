6bbffa839764d6263210d71f6c574ac3a54b1713
Shaolin_Xie <shx089@eng.ucsd.edu>
2016-12-22 14:23:07 -0800

    extended the bypass test

--- software/spmd/bypass_core/bypass_core_test.c
+++ software/spmd/bypass_core/bypass_core_test.c
@@ -7,48 +7,56 @@ extern unsigned int input[N];
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //    bypass core
 unsigned int  bypass_core_output[N]  = {0};
-unsigned int  bypass_core_expect[N]  = {0x3, 0x4, 0x5};
+unsigned int  bypass_core_expect[N]  = {0x3, 0x4, 0x5, 0x6};
 
 int bypass_core(unsigned int *src, unsigned int *dst){
 
   //MEM --> EXE
-  __asm__ __volatile__ ("lw t0, 0(%0)"::"r"(src) ); 
-  __asm__ __volatile__ ("lw t1, 4(%0)"::"r"(src) ); 
+  __asm__ __volatile__ ("lw t0, 0(%0)"::"r"(src) );
+  __asm__ __volatile__ ("lw t1, 4(%0)"::"r"(src) );
   __asm__ __volatile__ ("add t4, t0, t1" );  //3 = 1+ 2
 
   //WB --> EXE
   __asm__ __volatile__ ("lw t1, 8(%0)"::"r"(src) ); //3
-  __asm__ __volatile__ ("nop"  ); 
+  __asm__ __volatile__ ("nop"  );
   __asm__ __volatile__ ("add t5, t0, t1" );  //4 = 1+ 3
 
-  //READ/WRITE RF at the same time
+  //WB --> ID
   __asm__ __volatile__ ("lw t1, 12(%0)"::"r"(src) ); //4
-  __asm__ __volatile__ ("nop"  ); 
-  __asm__ __volatile__ ("nop"  ); 
+  __asm__ __volatile__ ("nop"  );
+  __asm__ __volatile__ ("nop"  );
   __asm__ __volatile__ ("add t6, t0, t1" );  //5 = 1+ 4
 
-  __asm__ __volatile__ ("nop"  ); 
-  __asm__ __volatile__ ("nop"  ); 
-  __asm__ __volatile__ ("nop"  ); 
-  
+  //WB --> IF, READ/WRITE RF at the same time
+  __asm__ __volatile__ ("lw t1, 16(%0)"::"r"(src) ); //5
+  __asm__ __volatile__ ("nop"  );
+  __asm__ __volatile__ ("nop"  );
+  __asm__ __volatile__ ("nop"  );
+  __asm__ __volatile__ ("add s2, t0, t1" );  //6 = 1+ 5
+
+  __asm__ __volatile__ ("nop"  );
+  __asm__ __volatile__ ("nop"  );
+  __asm__ __volatile__ ("nop"  );
+
   //Write back the result
-  __asm__ __volatile__ ("sw t4, 0(%0)" : :"r"(dst) ); 
-  __asm__ __volatile__ ("sw t5, 4(%0)" : :"r"(dst) ); 
-  __asm__ __volatile__ ("sw t6, 8(%0)" : :"r"(dst) ); 
+  __asm__ __volatile__ ("sw t4, 0(%0)" : :"r"(dst) );
+  __asm__ __volatile__ ("sw t5, 4(%0)" : :"r"(dst) );
+  __asm__ __volatile__ ("sw t6, 8(%0)" : :"r"(dst) );
+  __asm__ __volatile__ ("sw s2, 12(%0)" : :"r"(dst) );
 
 }
 
 void bypass_core_test(unsigned int  *input){
-    
+
     int i, error =0;
     unsigned int * int_output;
 
     bypass_core( input, bypass_core_output);
-    
+
     int_output = (unsigned int *) bypass_core_output;
-    for( i=0; i<3; i++){
+    for( i=0; i<4; i++){
         if ( int_output[i]  !=  bypass_core_expect[i] ) {
-            error = 1; 
+            error = 1;
             break;
         }
     }
