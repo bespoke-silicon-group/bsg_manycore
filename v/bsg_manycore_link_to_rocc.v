743195b2d0f6a399a74f23b3571424d9f3a85dd3
Shaolin_Xie <shx089@eng.ucsd.edu>
2017-01-29 21:06:18 -0800

    added the logic for manycore store

--- v/bsg_manycore_link_to_rocc.v
+++ v/bsg_manycore_link_to_rocc.v
@@ -17,6 +17,7 @@ module  bsg_manycore_link_to_rocc
     , parameter y_cord_width_p="inv"
     , parameter fifo_els_p    = 4
     , parameter bsg_manycore_link_sif_width_lp=`bsg_manycore_link_sif_width(addr_width_p,data_width_p,x_cord_width_p,y_cord_width_p)
+    , parameter debug_lp      =0
     )
   ( 
    // manycore side: manycore_link_sif
@@ -127,20 +128,22 @@ module  bsg_manycore_link_to_rocc
     if( write_seg_en ) 
         seg_addr_r <= core_cmd_s_i.rs1_val[ rocc_addr_width_gp-1 : addr_width_p ];
 
-  //store data into manycore
+  //write manycore memory
   assign rocc2manycore_v =   core_cmd_valid_i 
                          & ( core_cmd_s_i.instr.funct7 == eRoCC_core_write );
 
   assign rocc2manycore_packet = get_manycore_pkt( core_cmd_s_i.rs1_val, 
                                                   core_cmd_s_i.rs2_val);
    
-  //TODO:store data into rocket
-  assign manycore2rocc_yumi = manycore2rocc_v; 
-
   ///////////////////////////////////////////////////////////////////////////////////
-  // assign the outputs to rocc_mem
-   assign   mem_req_valid_o     =   1'b0   ;
-   assign   mem_req_s_o         =    'b0   ;
+  // Code for handling manycore store
+  assign mem_req_valid_o    = manycore2rocc_v ;
+  //if the rocket memory is ready, we complete the request
+  assign manycore2rocc_yumi = manycore2rocc_v & mem_req_ready_i ; 
+  assign mem_req_s_o        = get_rocket_mem_req(   manycore2rocc_data,
+                                                    manycore2rocc_mask,
+                                                    manycore2rocc_addr  );
+  ///////////////////////////////////////////////////////////////////////////////////
 
   // assign the outputs to rocc_core
    assign   core_cmd_ready_o    =   rocc2manycore_ready   ;
@@ -183,5 +186,36 @@ module  bsg_manycore_link_to_rocc
 
   endfunction 
 
+  //functions to encode the rocket memory request 
+  function rocc_mem_req_s get_rocket_mem_req(input [data_width_p-1:0        ] data,
+                                             input [(data_width_p>>3)-1:0   ] mask,
+                                             input [addr_width_p-1:0        ] addr
+                                            );
+    assign get_rocket_mem_req.req_addr =  get_rocket_addr( addr )   ;
+    assign get_rocket_mem_req.req_tag  =  rocc_mem_tag_width_gp'(0) ;
+    assign get_rocket_mem_req.req_cmd  =  eRoCC_mem_store           ; 
+    //currently only support 32bits 
+    assign get_rocket_mem_req.req_typ  =  eRoCC_mem_32bits          ;
+    assign get_rocket_mem_req.req_phys =  1'b1                      ;
+    assign get_rocket_mem_req.req_data =  rocc_data_width_gp'(data) ; 
+    
+  endfunction
+
+  ///////////////////////////////////////////////////////////////////////////////////
+  //synopsys translate_off
+  if( debug_lp ) begin:debug_link_to_rocc
+    always@(negedge rocket_clk_i ) begin
+      if( write_seg_en ) begin
+        $display("Configuring Segment Register with value :\
+             %h, Seg Reg bitwidth=%d, Maycore Addr bitwidth=%d", seg_addr_r, seg_addr_width_lp, addr_width_p); 
+      end 
+    end
+  end
+
+  always@(negedge rocket_clk_i ) begin
+    if( manycore2rocc_v ) assert ( & manycore2rocc_mask) else
+        $error("Only supports word access to rocket right now");
+  end
+  //synopsys translate_on
 endmodule
 
