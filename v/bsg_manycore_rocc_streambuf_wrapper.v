3e4bccf943fa5c1b1cb840054dd77a50cfa2f2ac
Shaolin_Xie <shx089@eng.ucsd.edu>
2017-03-31 10:42:12 -0700

    1. Added the bsg_manycore_rocc_streambuf_wrapper 2. Revert the streambuf related changes back to the master branch. So it won't change the master branch when this branch is merged

--- v/bsg_manycore_rocc_streambuf_wrapper.v
+++ v/bsg_manycore_rocc_streambuf_wrapper.v
@@ -5,14 +5,13 @@
 // This module wrapper the bsg_manycore with RoCC interface
 // The RoCC distributed at the south of the manycore.
 // ******* ID of each RoCC is (num_tiles_y, x)
-// ******* NUMBER of RoCC IS  LIMITED to 8
 //
 // Pleas contact Prof Taylor for the document.
 //
 `include "bsg_manycore_packet.vh"
 `include "bsg_rocc.v"
 
-module bsg_manycore_rocc_wrapper
+module bsg_manycore_rocc_streambuf_wrapper
    import  bsg_noc_pkg   ::*; // {P=0, W, E, N, S}
  #(
     //////////////////////////////////////////////////////
@@ -25,6 +24,16 @@ module bsg_manycore_rocc_wrapper
     //   indicates there are two rocc interface, their x_cords are 1 and 3.
     //3. rocc_num_p must not bigger than tiles_x
    ,parameter rocc_dist_vec_p = 0
+
+    //////////////////////////////////////////////////////
+    //Parameters for out_fifo interface
+   ,parameter               out_fifo_num_p      = "inv"
+    //The distribution of the out_fifo interface.
+    //The same meaning with rocc_dist_vect_p
+   ,parameter out_fifo_dist_vec_p = 0
+    //The output fifo width must be multiple times of data_width_p
+   ,parameter out_fifo_width_scale_p  = 2
+   ,parameter out_fifo_in_channel_num_lp = out_fifo_num_p / out_fifo_width_scale_p 
 
     //////////////////////////////////////////////////////
     //Parameters for manycore
@@ -52,6 +61,7 @@ module bsg_manycore_rocc_wrapper
    // snew * y * x bits
    ,parameter repeater_output_p = 0
 
+   ,parameter out_fifo_width_lp   = data_width_p       * out_fifo_width_scale_p 
   )
   ( input clk_i
    ,input manycore_clk_i
@@ -77,9 +87,14 @@ module bsg_manycore_rocc_wrapper
 
    , input                      [rocc_num_p-1:0]  mem_resp_valid_i
    , input  rocc_mem_resp_s     [rocc_num_p-1:0]  mem_resp_s_i
+
+   //FIFO output 
+   , output                             v_o
+   , output [out_fifo_width_lp-1:0]     data_o
+   , input                              yumi_i
   );
 
-  //get one byte from the parameter p
+  //get one hex from the parameter vector p
  `define GET_HEX(p, ind)       ( (p >> (ind* 4)) & 4'hF )
  `define GET_HEX_MIN_1(p, ind) ( `GET_HEX(p,ind) - 4'h1 )
 
@@ -193,11 +208,15 @@ module bsg_manycore_rocc_wrapper
          );
      end
    /////////////////////////////////////////////////////////////////////////////////
-   // Instantiate the RoCC interface
-   localparam  rocc_index_limit_lp =  num_tiles_x_p ;
+   // Instantiate the RoCC & out_fifo  interface
+   wire [out_fifo_num_p-1:0] [bsg_manycore_link_sif_width_lp-1:0] out_fifo_link_sif_li ;
+   wire [out_fifo_num_p-1:0] [bsg_manycore_link_sif_width_lp-1:0] out_fifo_link_sif_lo ;
+   wire [out_fifo_num_p-1:0] [x_cord_width_lp-1:0]                out_fifo_x_cord      ;
+   wire [out_fifo_num_p-1:0] [y_cord_width_lp-1:0]                out_fifo_y_cord      ;
 
+   localparam  rocc_index_limit_lp =  num_tiles_x_p ;
    genvar io_ind;
-   for( io_ind=0; io_ind < rocc_index_limit_lp; io_ind ++) begin: rocc_inst
+   for( io_ind=0; io_ind < rocc_index_limit_lp; io_ind ++) begin: rocc_out_fifo_inst
         if( `GET_HEX(rocc_dist_vec_p, io_ind)  != 0 ) begin: rocc_inst_real
 
             bsg_manycore_link_sif_s  RC_rocc_link_input, RC_rocc_link_output;
@@ -256,6 +275,32 @@ module bsg_manycore_rocc_wrapper
 
                 ,.reset_manycore_r_o   (RC_rocc_output_reset[`GET_HEX_MIN_1(rocc_dist_vec_p, io_ind) ] )
             );
+        //----------------------------------------------------
+        //Check if we need to instantiate out_fifo interface
+        end else if( `GET_HEX(out_fifo_dist_vec_p, io_ind)  != 0 ) begin: out_fifo_inst_real
+
+            bsg_manycore_link_sif_async_buffer
+           #(  .addr_width_p  (addr_width_p)
+              ,.data_width_p  (data_width_p)
+              ,.x_cord_width_p(x_cord_width_lp)
+              ,.y_cord_width_p(y_cord_width_lp)
+              ,.fifo_els_p    (4)
+            )manycore_out_fifo_async_buffer(
+            .L_clk_i     ( manycore_clk_i  )
+           ,.L_reset_i   ( MC_reset_r  )
+           ,.L_link_sif_i( MC_ver_link_lo[S][ io_ind ])
+           ,.L_link_sif_o( MC_ver_link_li[S][ io_ind ])
+
+           ,.R_clk_i     ( clk_i        )
+           //This FIFO will be reseted as Manycore is reseted.
+           ,.R_reset_i   ( RC_reset_n   )
+           ,.R_link_sif_i( out_fifo_link_sif_lo [ `GET_HEX_MIN_1(out_fifo_dist_vec_p, io_ind) ] )
+           ,.R_link_sif_o( out_fifo_link_sif_li [ `GET_HEX_MIN_1(out_fifo_dist_vec_p, io_ind) ] )
+           );
+
+           assign out_fifo_x_cord [ `GET_HEX_MIN_1(out_fifo_dist_vec_p, io_ind) ] =  x_cord_width_lp'(io_ind ); 
+           assign out_fifo_y_cord [ `GET_HEX_MIN_1(out_fifo_dist_vec_p, io_ind) ] =  y_cord_width_lp'(num_tiles_y_p); 
+        //----------------------------------------------------
         //otherwise tieoff
         end else begin: rocc_inst_tieoff
             bsg_manycore_link_sif_tieoff #(.addr_width_p   (addr_width_p)
@@ -269,7 +314,37 @@ module bsg_manycore_rocc_wrapper
              ,.link_sif_o(MC_ver_link_li[S][ io_ind ])
              );
        end:rocc_inst_tieoff
-   end:rocc_inst
+   end:rocc_out_fifo_inst
+   /////////////////////////////////////////////////////////////////////////////////
+   // instantiate the link_to_fifo
+    bsg_manycore_link_to_fifo #(  
+     .addr_width_p   ( addr_width_p     )
+    ,.data_width_p   ( data_width_p     )
+    ,.x_cord_width_p ( x_cord_width_lp  )
+    ,.y_cord_width_p ( y_cord_width_lp  )
+
+    //The output fifo width must be multiple times of data_width_p
+    , .out_fifo_width_scale_p  ( out_fifo_width_scale_p )
+    //multiple channel are merged to increase the output bandwidth 
+    , .in_channel_scale_p      ( out_fifo_in_channel_num_lp )
+    )link_to_fifo_inst
+  (
+    //this will work under rocket clock domain
+     .clk_i     ( clk_i         )
+   , .reset_i   ( reset_i       )
+
+   , .my_x_i    ( out_fifo_x_cord)
+   , .my_y_i    ( out_fifo_y_cord)
+
+   //input from the manycore, in the same clock domain with the FIFO
+   , .link_sif_i( out_fifo_link_sif_li )
+   , .link_sif_o( out_fifo_link_sif_lo )
+
+   //FIFO output 
+   , .v_o     ( v_o     ) 
+   , .data_o  ( data_o  )
+   , .yumi_i  ( yumi_i  )
+   );
 
    /////////////////////////////////////////////////////////////////////////////////
    // parameter check
