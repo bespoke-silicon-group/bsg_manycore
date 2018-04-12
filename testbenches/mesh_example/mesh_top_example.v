9801c3130d4268615ac6aad8446c8ee5fa294912
Shaolin_Xie <shx089@eng.ucsd.edu>
2018-04-12 15:12:09 -0700

    [mesh example] attached the master to South and slave to East

--- testbenches/mesh_example/mesh_top_example.v
+++ testbenches/mesh_example/mesh_top_example.v
@@ -4,15 +4,15 @@
 //====================================================================
 // This module instantiate an mesh router, a slave and master
 //
-//     (Y,X=0,0)   MASTER   TIED
-//                    \      |
-//                     \     |
-//                      |----------|
-//               TIED---| ROUTER   | --- SLAVE (Y,X = 0,1)
-//                      |----------|
-//                           |
-//                           |
-//                         TIED
+//     TIED       TIED          TIED       TIED      (Y, X)
+//        \        |               \        |
+//         \       |                \       |
+//          |-------------|          |-------------|
+//   TIED---| ROUTER(0,0) | ---------| ROUTER(0,1) | --- SLAVE(0,2)
+//          |-------------|          |-------------|
+//                 |                        |
+//                 |                        |
+//              MASTER(1,0)               TIED
 //
 `include "bsg_manycore_packet.vh"
 
@@ -31,32 +31,39 @@ module mesh_top_example #(   x_cord_width_p         = "inv"
    );
 
     //which direction of the router should be stubbed
-    localparam stub_lp  ={1'b1, 1'b1, 1'b0, 1'b1}  ;// {s,n,e,w}
     localparam w_idx_lp =bsg_noc_pkg::W            ;// index of the direction starts from West
     localparam s_idx_lp =bsg_noc_pkg::S            ;// index of the direction ends at South
+    localparam nodes_lp =2                         ;// two nodes
 
     // the interconnections
-    wire [bsg_manycore_link_sif_width_lp-1:0]                       master_link_sif_li, master_link_sif_lo;
-    wire [s_idx_lp : w_idx_lp][bsg_manycore_link_sif_width_lp-1:0]  router_link_sif_li, router_link_sif_lo;
+    wire [nodes_lp-1:0] [s_idx_lp : w_idx_lp][bsg_manycore_link_sif_width_lp-1:0]  router_link_sif_li, router_link_sif_lo;
+    wire [nodes_lp-1:0] [bsg_manycore_link_sif_width_lp-1:0]                       proc_link_sif_li, proc_link_sif_lo;
 
    // Instantiate the router and connects the master
-    bsg_manycore_mesh_node #(
-       .stub_p           ( stub_lp       )
-      ,.x_cord_width_p   (x_cord_width_p )
-      ,.y_cord_width_p   (y_cord_width_p )
-      ,.data_width_p     (data_width_p   )
-      ,.addr_width_p     (addr_width_p   )
-    )mesh_node
-    (
-       .clk_i            (clk_i                  )
-      ,.reset_i          (reset_i                )
-      ,.links_sif_i      (router_link_sif_li     )
-      ,.links_sif_o      (router_link_sif_lo     )
-      ,.proc_link_sif_i  (master_link_sif_lo     )
-      ,.proc_link_sif_o  (master_link_sif_li     )
-      ,.my_x_i           ( x_cord_width_p'(0)    )
-      ,.my_y_i           ( y_cord_width_p'(0)    )
-    );
+   genvar i;
+   for( i=0; i< nodes_lp; i++) begin
+         bsg_manycore_mesh_node #(
+                              // S         N         E         W
+            .stub_p           ({(i==1),   1'b1,     1'b0,  (i==0)} )
+           ,.x_cord_width_p   (x_cord_width_p )
+           ,.y_cord_width_p   (y_cord_width_p )
+           ,.data_width_p     (data_width_p   )
+           ,.addr_width_p     (addr_width_p   )
+         )mesh_node
+         (
+            .clk_i            (clk_i                  )
+           ,.reset_i          (reset_i                )
+           ,.links_sif_i      (router_link_sif_li[i]  )
+           ,.links_sif_o      (router_link_sif_lo[i]  )
+           ,.proc_link_sif_i  (proc_link_sif_lo[i]    )
+           ,.proc_link_sif_o  (proc_link_sif_li[i]    )
+           ,.my_x_i           ( x_cord_width_p'(i)    )
+           ,.my_y_i           ( y_cord_width_p'(0)    )
+         );
+  end
+
+  assign router_link_sif_li[0][ bsg_noc_pkg::E ] = router_link_sif_lo[1][ bsg_noc_pkg::W ] ;
+  assign router_link_sif_li[1][ bsg_noc_pkg::W ] = router_link_sif_lo[0][ bsg_noc_pkg::E ] ;
 
   //Instantiate the master
   mesh_master_example
@@ -70,13 +77,13 @@ module mesh_top_example #(   x_cord_width_p         = "inv"
     , .reset_i
 
     // mesh network
-    , .link_sif_i       ( master_link_sif_li )
-    , .link_sif_o       ( master_link_sif_lo )
+    , .link_sif_i       ( router_link_sif_lo[0][ bsg_noc_pkg::S ] )
+    , .link_sif_o       ( router_link_sif_li[0][ bsg_noc_pkg::S ] )
 
     , .my_x_i           ( x_cord_width_p'(0) )
-    , .my_y_i           ( y_cord_width_p'(0) )
+    , .my_y_i           ( y_cord_width_p'(1) )
 
-    , .dest_x_i         ( x_cord_width_p'(1) )
+    , .dest_x_i         ( x_cord_width_p'(2) )
     , .dest_y_i         ( y_cord_width_p'(0) )
 
     , .finish_o
@@ -94,31 +101,46 @@ module mesh_top_example #(   x_cord_width_p         = "inv"
     , .reset_i
 
     // mesh network
-    , .link_sif_i       ( router_link_sif_lo[ bsg_noc_pkg::E ] )
-    , .link_sif_o       ( router_link_sif_li[ bsg_noc_pkg::E ] )
+    , .link_sif_i       ( router_link_sif_lo[1][ bsg_noc_pkg::E ] )
+    , .link_sif_o       ( router_link_sif_li[1][ bsg_noc_pkg::E ] )
 
-    , .my_x_i           ( x_cord_width_p'(1) )
+    , .my_x_i           ( x_cord_width_p'(2) )
     , .my_y_i           ( x_cord_width_p'(0) )
   );
 
 
   //tie up the stubbed routers
-  genvar i;
-  for( i= w_idx_lp; i <= s_idx_lp; i++) begin:tie_up // {P=0, W, E, N, S}
-        if( stub_lp [  (i-w_idx_lp) ] ) begin //this direction is stubbed
-                bsg_manycore_link_sif_tieoff
-                #(.addr_width_p  (addr_width_p  )
-                 ,.data_width_p  (data_width_p  )
-                 ,.x_cord_width_p(x_cord_width_p)
-                 ,.y_cord_width_p(y_cord_width_p)
-                ) bmlst
-                (        .clk_i
-                        ,.reset_i
-                        ,.link_sif_i( router_link_sif_lo [ i ] )
-                        ,.link_sif_o( router_link_sif_li [ i ] )
-                );
+  genvar j;
+  for( i=0; i< nodes_lp ; i++) begin
+        for( j= w_idx_lp; j <= s_idx_lp; j++) begin:tie_up // {P=0, W, E, N, S}
+              if(      ( j != bsg_noc_pkg::E )
+                    && ( i==1  && j != bsg_noc_pkg::W )
+                )begin //this direction is stubbed
+                      bsg_manycore_link_sif_tieoff
+                      #(.addr_width_p  (addr_width_p  )
+                       ,.data_width_p  (data_width_p  )
+                       ,.x_cord_width_p(x_cord_width_p)
+                       ,.y_cord_width_p(y_cord_width_p)
+                      ) bmlst_router
+                      (        .clk_i
+                              ,.reset_i
+                              ,.link_sif_i( router_link_sif_lo [ i ][ j ] )
+                              ,.link_sif_o( router_link_sif_li [ i ][ j ] )
+                      );
 
-        end
+              end
+         end
+          bsg_manycore_link_sif_tieoff
+          #(.addr_width_p  (addr_width_p  )
+           ,.data_width_p  (data_width_p  )
+           ,.x_cord_width_p(x_cord_width_p)
+           ,.y_cord_width_p(y_cord_width_p)
+          ) bmlst_proc
+          (        .clk_i
+                  ,.reset_i
+                  ,.link_sif_i( proc_link_sif_li  [ i ] )
+                  ,.link_sif_o( proc_link_sif_lo  [ i ] )
+          );
    end
 
 endmodule
