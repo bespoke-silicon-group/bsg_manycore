e0a4da4d60355a7952522e6fadb729fd2594d100
Shaolin_Xie <shx089@eng.ucsd.edu>
2018-05-04 14:44:07 -0700

    [40nm-tapeout] added the cross-row and cross-bank cases

--- software/spmd/bsg_dram_scatter/main.c
+++ software/spmd/bsg_dram_scatter/main.c
@@ -30,6 +30,12 @@
 #define ADDR_VECT_BANK    {0*ROW_BYTES, 1*ROW_BYTES, 2*ROW_BYTES, 3*ROW_BYTES}
 #define BANK_PATTEN_ID    0x33333333
 
+#define ADDR_VECT_CROSS_ROWS    {COL_BYTES-2*4, COL_BYTES-1*4, COL_BYTES, COL_BYTES+ 1*4}
+#define CROSS_ROWS_PATTEN_ID    0x44444444
+
+#define ADDR_VECT_CROSS_BANK    {ROW_BYTES-2*4, ROW_BYTES-1*4, ROW_BYTES, ROW_BYTES+ 1*4}
+#define CROSS_BANK_PATTEN_ID    0x55555555
+
 #define DRAM_X_CORD       1
 #define DRAM_Y_CORD       1
 
@@ -38,6 +44,9 @@ int  data_vect[VECTOR_LEN] = DATA_VECT;
 int  addr_vect_cols[VECTOR_LEN] = ADDR_VECT_COLS;
 int  addr_vect_rows[VECTOR_LEN] = ADDR_VECT_ROWS;
 int  addr_vect_bank[VECTOR_LEN] = ADDR_VECT_BANK;
+
+int  addr_vect_cross_rows[VECTOR_LEN] = ADDR_VECT_CROSS_ROWS;
+int  addr_vect_cross_bank[VECTOR_LEN] = ADDR_VECT_CROSS_BANK;
 
 void scatter( int * addr_vect, int* data_vect, int patten_id){
         bsg_remote_ptr_io_store(0, 0x0, patten_id);
@@ -70,6 +79,9 @@ int main()
        scatter( addr_vect_cols, data_vect, COLS_PATTEN_ID);
        scatter( addr_vect_rows, data_vect, ROWS_PATTEN_ID);
        scatter( addr_vect_bank, data_vect, BANK_PATTEN_ID);
+
+       scatter( addr_vect_cross_rows, data_vect, CROSS_ROWS_PATTEN_ID);
+       scatter( addr_vect_cross_bank, data_vect, CROSS_BANK_PATTEN_ID);
        bsg_finish();
   }
 
