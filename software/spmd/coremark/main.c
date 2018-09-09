d136755eb81fd3496ab305a5065923a61bdf18ec
shaolin <shawnless.xie@gmail.com>
2018-09-08 23:03:30 -0700

    added the coremark target. compile successfuly

---
+++ software/spmd/coremark/main.c
@@ -0,0 +1,17 @@
+#include "bsg_manycore.h"
+
+#define MANYCORE_PROG
+
+//this macro will defined in Makefile, indicates which rocc can be wrote into
+//#define bsg_active_rocc_index
+int main()
+{
+  int i;
+
+  bsg_wait_while(1);
+}
+
+////////////////////////////////////////////////////////////////
+//Print the current manycore configurations
+#pragma message (bsg_VAR_NAME_VALUE( bsg_tiles_X )  )
+#pragma message (bsg_VAR_NAME_VALUE( bsg_tiles_Y )  )
