7706e3877e1d2cbec851087bfcaa858ded79717f
shaolin <shawnless.xie@gmail.com>
2018-11-04 11:27:10 -0800

    [hello example] improved the code with bsg_set_x_y library

--- software/spmd/hello/main.c
+++ software/spmd/hello/main.c
@@ -1,50 +1,6 @@
 
 #include "bsg_manycore.h"
-
-// these are private variables
-// we do not make them volatile
-// so that they may be cached
-
-int bsg_x = -1;
-int bsg_y = -1;
-
-int bsg_set_tile_x_y()
-{
-  // everybody stores to tile 0,0
-  bsg_remote_store(0,0,&bsg_x,0);
-  bsg_remote_store(0,0,&bsg_y,0);
-
-  // make sure memory ops above are not moved down
-  bsg_compiler_memory_barrier();
-
-  // wait for my tile number to change
-  bsg_wait_while((bsg_volatile_access(bsg_x) == -1) || (bsg_volatile_access(bsg_y) == -1));
-
-  // make sure memory ops below are not moved above
-  bsg_compiler_memory_barrier();
-
-  // head of each column is responsible for
-  // propagating to next column
-  if ((bsg_x == 0)
-      && ((bsg_y + 1) != bsg_tiles_Y)
-    )
-  {
-    bsg_remote_store(0,bsg_y+1,&bsg_x,bsg_x);
-    bsg_remote_store(0,bsg_y+1,&bsg_y,bsg_y+1);
-  }
-
-  // propagate across each row
-  if ((bsg_x+1) != bsg_tiles_X)
-  {
-    bsg_remote_store(bsg_x+1,bsg_y,&bsg_x,bsg_x+1);
-    bsg_remote_store(bsg_x+1,bsg_y,&bsg_y,bsg_y);
-  }
-}
-
-int square(int c)
-{
-  return c*c;
-}
+#include "bsg_set_tile_x_y.h"
 
 int main()
 {
