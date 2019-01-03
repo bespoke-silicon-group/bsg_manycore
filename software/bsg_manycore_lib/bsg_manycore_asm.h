458c65f2e6e75486ad34231511626679d1208a8c
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-01-03 10:33:15 -0800

    Some more asm macros

--- software/bsg_manycore_lib/bsg_manycore_asm.h
+++ software/bsg_manycore_lib/bsg_manycore_asm.h
@@ -1,7 +1,12 @@
 #ifndef _BSG_MANYCORE_ASM_H
 #define _BSG_MANYCORE_ASM_H
 
-// Only use temporary registers (t0-6) here
+/*******************************************************
+ * Some useful macros for manycore assembly programming
+ * Only use temporary registers (t0-6) here.
+ *******************************************************/
+
+// Loads and stores
 
 #define bsg_asm_remote_ptr(x,y,local_addr)         \
     ((REMOTE_EPA_PREFIX << REMOTE_EPA_MASK_SHIFTS) \
@@ -15,13 +20,17 @@
     li t1, val;                                  \
     sw t1, 0x0(t0);
 
+#define bsg_asm_remote_store_reg(x,y,local_addr,reg) \
+    li t0, bsg_asm_remote_ptr(x,y,local_addr);       \
+    sw reg, 0x0(t0);
+
 #define bsg_asm_local_store(local_addr,val) \
     li t0, local_addr;                      \
     li t1, val;                             \
     sw t1, 0x0(t0);
 
-#define bsg_asm_remote_store_reg(x,y,local_addr,reg) \
-    li t0, bsg_asm_remote_ptr(x,y,local_addr);       \
+#define bsg_asm_local_store_reg(local_addr,reg) \
+    li t0, local_addr;                          \
     sw reg, 0x0(t0);
 
 #define bsg_asm_remote_load(reg,x,y,local_addr) \
@@ -32,17 +41,34 @@
     li t0, local_addr;                     \
     lw reg, 0x0(t0);
 
+
+// IO macros
+
+// print value in IO #x
+#define bsg_asm_print(x,val)                      \
+    li t0, bsg_asm_remote_ptr(x,bsg_tiles_Y,0x0); \
+    li t1, val;                                   \
+    sw t1, 0x0(t0);
+
 // print a register ("reg") in IO #x
 #define bsg_asm_print_reg(x,reg)                  \
     li t0, bsg_asm_remote_ptr(x,bsg_tiles_Y,0x0); \
     sw reg, 0x0(t0);
 
-// finish in IO #x
-#define bsg_asm_finish(x) \
-    bsg_asm_remote_store(x,bsg_tiles_Y,0xEAD0,0)
+// finish with value in IO #x
+#define bsg_asm_finish(x,val) \
+    bsg_asm_remote_store(x,bsg_tiles_Y,0xEAD0,val)
 
-// fail in IO #x
+// finish with value in a reg in IO #x
+#define bsg_asm_finish_reg(x,reg) \
+    bsg_asm_remote_store_reg(x,bsg_tiles_Y,0xEAD0,reg)
+
+// fail with value in IO #x
 #define bsg_asm_fail(x, value) \
     bsg_asm_remote_store(x,bsg_tiles_Y,0xEAD8,value)
+
+// fail with value in a reg in IO #x
+#define bsg_asm_fail_reg(x,reg) \
+    bsg_asm_remote_store_reg(x,bsg_tiles_Y,0xEAD8,reg)
 
 #endif
