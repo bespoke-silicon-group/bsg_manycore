aba8f556c9b3baa22e5483d2c2fd495012f1b5fa
Neil Ryan <soctar@gmail.com>
2019-03-26 19:08:50 +0000

    Merged in striped_structs (pull request #25)
    Striped structs
    
    * Added lots of tests for striping in striped_hello, currently not replacing loads/stores to structs?
    
    * Working version with struct striping -- pretty slow
    
    * Working version of striping for structs; no striped structs of structs, no striped pointers to fields of structs
    
    * Style fixes, whitespace fixes, added back inlining
    
    Approved-by: Dustin Alexander Richmond <dustinar@uw.edu>
    Approved-by: Shaolin Xie <shawnless.xie@gmail.com>

--- software/bsg_manycore_lib/bsg_tilegroup.h
+++ software/bsg_manycore_lib/bsg_tilegroup.h
@@ -15,7 +15,7 @@ extern unsigned _bsg_striped_data_start;
 /* NOTE: It's usually a cardinal sin to include code in header files, but LLVM
  * needs the definitions of runtime functions avaliable so that the pass can
  * replace loads and stores -- these aren't avaliable via declarations. */
- static volatile void *get_ptr_val(void STRIPE *arr_ptr, unsigned elem_size) {
+ static volatile int *get_ptr_val(void STRIPE *arr_ptr, unsigned elem_size, unsigned local_offset) {
     unsigned start_ptr = (unsigned) &_bsg_striped_data_start;
     unsigned ptr = (unsigned) arr_ptr;
 
@@ -23,9 +23,12 @@ extern unsigned _bsg_striped_data_start;
     // section. Since data is aligned on word_size * G, we can calculate the
     // "index" into the overall .striped.data segment. In hardware, this would
     // be the same as caluclating the offset from a segment register
-    unsigned index = ((ptr) - start_ptr) / elem_size;
-    unsigned core_id = index % (bsg_group_size);
+    unsigned index = (ptr - start_ptr) / elem_size;
+    unsigned core_id = index % bsg_group_size;
     unsigned local_addr = start_ptr + (index / bsg_group_size) * elem_size;
+    // We use local_offset to index into structs, since we stripe entire
+    // structs instead of striping words
+    local_addr += local_offset;
 
     // Get X & Y coordinates of the tile that holds the memory address
     unsigned tile_x = core_id / bsg_tiles_X;
@@ -36,10 +39,8 @@ extern unsigned _bsg_striped_data_start;
                               tile_x << X_CORD_SHIFTS |
                               tile_y << Y_CORD_SHIFTS |
                               local_addr;
-    // This check isn't strictly needed, but it's kept so that we can check the
-    // performance effect
-    unsigned ptr_val = (tile_x == bsg_x && tile_y == bsg_y) ?
-        local_addr : remote_ptr_val;
+    // It's faster to avoid a conditional than try to check in software
+    unsigned ptr_val = remote_ptr_val;
 #ifdef TILEGROUP_DEBUG
     bsg_printf("ID = %u, index = %u; striped_data_start = 0x%x\n",
             core_id, index, &_bsg_striped_data_start);
@@ -49,61 +50,102 @@ extern unsigned _bsg_striped_data_start;
     return (volatile int *) ptr_val;
 }
 
-__attribute__((always_inline)) void extern_store_int(int STRIPE *arr_ptr, unsigned elem_size, unsigned val) {
+
+__attribute__((always_inline))
+void extern_store_int(int STRIPE *arr_ptr, unsigned elem_size, unsigned offset, unsigned val) {
 #ifdef TILEGROUP_DEBUG
-    bsg_printf("\nCalling extern_store_int(0x%x, %d, %d)\n", (unsigned) arr_ptr,
-            elem_size, val);
+    bsg_printf("\nCalling extern_store_int(0x%x, %d, %d, %d)\n",
+            (unsigned) arr_ptr, elem_size, offset, val);
 #endif
-    volatile int *ptr = get_ptr_val(arr_ptr, elem_size);
+    volatile int *ptr = get_ptr_val(arr_ptr, elem_size, offset);
     *ptr = val;
 }
 
-__attribute__((always_inline)) void extern_store_short(int STRIPE *arr_ptr, unsigned elem_size, short val) {
+
+__attribute__((always_inline))
+void extern_store_short(int STRIPE *arr_ptr, unsigned elem_size, unsigned offset, short val) {
 #ifdef TILEGROUP_DEBUG
-    bsg_printf("\nCalling extern_store_short(0x%x, %d, %d)\n", (unsigned) arr_ptr,
-            elem_size, val);
+    bsg_printf("\nCalling extern_store_short(0x%x, %d, %d, %d)\n",
+            (unsigned) arr_ptr, elem_size, offset, val);
 #endif
-    volatile short *ptr = (volatile short *) get_ptr_val(arr_ptr, elem_size);
+    volatile short *ptr = (volatile short *) get_ptr_val(arr_ptr, elem_size, offset);
     *ptr = val;
 }
 
-__attribute__((always_inline)) void extern_store_char(int STRIPE *arr_ptr, unsigned elem_size, char val) {
+__attribute__((always_inline))
+void extern_store_char(int STRIPE *arr_ptr, unsigned elem_size, unsigned offset, char val) {
 #ifdef TILEGROUP_DEBUG
-    bsg_printf("\nCalling extern_store_char(0x%x, %d, %d)\n", (unsigned) arr_ptr,
-            elem_size, val);
+    bsg_printf("\nCalling extern_store_char(0x%x, %d, %d, %d)\n",
+            (unsigned) arr_ptr, elem_size, offset, val);
 #endif
-    volatile char *ptr = (volatile char *) get_ptr_val(arr_ptr, elem_size);
+    volatile char *ptr = (volatile char *) get_ptr_val(arr_ptr, elem_size, offset);
     *ptr = val;
 }
 
-__attribute__((always_inline)) int extern_load_int(int STRIPE *arr_ptr, unsigned elem_size) {
+__attribute__((always_inline))
+int extern_load_int(int STRIPE *arr_ptr, unsigned elem_size, unsigned offset) {
 #ifdef TILEGROUP_DEBUG
-    bsg_printf("\nCalling extern_load_int(0x%x, %d)\n",
-            (unsigned) arr_ptr,
-            elem_size);
+    bsg_printf("\nCalling extern_load_int(0x%x, %d, %d)\n",
+            (unsigned) arr_ptr, elem_size, offset);
 #endif
-    volatile int *ptr = get_ptr_val(arr_ptr, elem_size);
+    volatile int *ptr = get_ptr_val(arr_ptr, elem_size, offset);
     return *ptr;
 }
 
-__attribute__((always_inline)) short extern_load_short(int STRIPE *arr_ptr, unsigned elem_size) {
+__attribute__((always_inline))
+short extern_load_short(int STRIPE *arr_ptr, unsigned elem_size, unsigned offset) {
 #ifdef TILEGROUP_DEBUG
-    bsg_printf("\nCalling extern_load_short(0x%x, %d)\n",
-            (unsigned) arr_ptr,
-            elem_size);
+    bsg_printf("\nCalling extern_load_short(0x%x, %d, %d)\n",
+            (unsigned) arr_ptr, elem_size, offset);
 #endif
-    volatile short *ptr = (volatile short *) get_ptr_val(arr_ptr, elem_size);
+    volatile short *ptr = (volatile short *) get_ptr_val(arr_ptr, elem_size, offset);
     return *ptr;
 }
 
-__attribute__((always_inline)) char extern_load_char(int STRIPE *arr_ptr, unsigned elem_size) {
+__attribute__((always_inline))
+char extern_load_char(int STRIPE *arr_ptr, unsigned elem_size, unsigned offset) {
 #ifdef TILEGROUP_DEBUG
-    bsg_printf("\nCalling extern_load_char(0x%x, %d)\n",
-            (unsigned) arr_ptr,
-            elem_size);
+    bsg_printf("\nCalling extern_load_char(0x%x, %d, %d)\n",
+            (unsigned) arr_ptr, elem_size, offset);
 #endif
-    volatile char *ptr = (volatile char *) get_ptr_val(arr_ptr, elem_size);
+    volatile char *ptr = (volatile char *) get_ptr_val(arr_ptr, elem_size, offset);
     return *ptr;
+}
+
+__attribute__((always_inline))
+void extern_load_memcpy(char *dest, char STRIPE *src, unsigned len) {
+#ifdef TILEGROUP_DEBUG
+    bsg_printf("\nCalling extern_load_memcpy(0x%x<-0x%x; %u words)\n",
+            dest, src, len);
+#endif
+    // TODO need offset for structs of structs
+    volatile int *src_base = get_ptr_val(src, len, 0);
+    volatile int *tdest = (volatile int *) dest;
+
+    while (len) {
+        *tdest = *src_base;
+        tdest++;
+        src_base++;
+        len -= sizeof(int);
+    }
+}
+
+__attribute__((always_inline))
+void extern_store_memcpy(char STRIPE *dest, char *src, unsigned len) {
+#ifdef TILEGROUP_DEBUG
+    bsg_printf("\nCalling extern_store_memcpy(0x%x<-0x%x; %u words)\n",
+            dest, src, len);
+#endif
+
+    volatile int *dest_base = get_ptr_val(dest, len, 0);
+    volatile int *tsrc = (volatile int *) src;
+
+    while (len) {
+        *dest_base = *tsrc;
+        dest_base++;
+        tsrc++;
+        len -= sizeof(int);
+    }
 }
 
 #endif // _BSG_TILEGROUP_H
--- software/manycore-llvm-pass/manycore/Manycore.cpp
+++ software/manycore-llvm-pass/manycore/Manycore.cpp
@@ -2,6 +2,8 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Constants.h"
+#include "llvm/IR/DataLayout.h"
+
 using namespace llvm;
 
 #define STRIPE 1 // Needs to match address_space set by library
@@ -11,6 +13,32 @@ class FunctionNotFound: public std::exception {
         return "Error: Function not found\n";
     }
 } functionNotFoundException;
+
+
+// Get the size of the overall struct and the offset of the field being accessed.
+// Returns -1 if we're not accessing a struct, and the offset otherwise
+// The size of the struct is returned in *struct_size if the function returns
+// a non-negative value
+int get_struct_info(Module &M, Value *ptr_op, unsigned *struct_size) {
+    DataLayout layout = DataLayout(&M);
+    // Gives struct type
+    GEPOperator *gep = dyn_cast<GEPOperator>(ptr_op);
+    if (!gep) { return -1;}
+    Type *source_type = gep->getSourceElementType();
+    while (!isa<StructType>(source_type)) {
+        if (isa<SequentialType>(source_type)) {
+            source_type = source_type->getSequentialElementType();
+        } else {
+            return -1;
+        }
+    }
+    StructType *struct_type = cast<StructType>(source_type);
+    *struct_size = layout.getTypeAllocSizeInBits(struct_type) / 8;
+    // The last operand of GEP gives the index of the field in the struct definition
+    unsigned struct_idx = cast<ConstantInt>(gep->getOperand(gep->getNumOperands() - 1))->getSExtValue();
+    // Get the offset of the selected field into the struct
+    return layout.getStructLayout(struct_type)->getElementOffset(struct_idx);
+}
 
 
 void replace_extern_store(Module &M, StoreInst *op) {
@@ -24,18 +52,30 @@ void replace_extern_store(Module &M, StoreInst *op) {
     Value *ptr_bc = builder.CreatePointerCast(ptr_op, int32_ptr);
 
     args_vector.push_back(ptr_bc);
-    unsigned elem_size = op->getValueOperand()->getType()->getPrimitiveSizeInBits() / 8;
-    args_vector.push_back(ConstantInt::get(Type::getInt32Ty(M.getContext()),
-                elem_size, false));
-    if (elem_size == 1) {
+    unsigned value_elem_size = op->getValueOperand()->getType()->getPrimitiveSizeInBits() / 8;
+
+    if (value_elem_size == 1) {
         store_fn = M.getFunction("extern_store_char");
-    } else if (elem_size == 2) {
+    } else if (value_elem_size == 2) {
         store_fn = M.getFunction("extern_store_short");
     } else {
         store_fn = M.getFunction("extern_store_int");
     }
     if (store_fn == NULL) {
        throw functionNotFoundException;
+    }
+    unsigned struct_size;
+    int struct_off = get_struct_info(M, ptr_op, &struct_size);
+    if (struct_off < 0) { // Not accessing a struct field
+        args_vector.push_back(ConstantInt::get(Type::getInt32Ty(M.getContext()),
+                    value_elem_size, false));
+        args_vector.push_back(ConstantInt::get(Type::getInt32Ty(M.getContext()),
+                    0, false));
+    } else { // Accessing a struct field
+        args_vector.push_back(ConstantInt::get(Type::getInt32Ty(M.getContext()),
+                    struct_size, false));
+        args_vector.push_back(ConstantInt::get(Type::getInt32Ty(M.getContext()),
+                    struct_off, false));
     }
     args_vector.push_back(op->getValueOperand());
 
@@ -61,10 +101,10 @@ void replace_extern_load(Module &M, LoadInst *op) {
     Value *ptr_bc = builder.CreatePointerCast(ptr_op, int32_ptr);
 
     args_vector.push_back(ptr_bc);
-    unsigned elem_size = op->getType()->getPrimitiveSizeInBits() / 8;
-    if (elem_size == 1) {
+    unsigned value_elem_size = op->getType()->getPrimitiveSizeInBits() / 8;
+    if (value_elem_size == 1) {
         load_fn = M.getFunction("extern_load_char");
-    } else if (elem_size == 2) {
+    } else if (value_elem_size == 2) {
         load_fn = M.getFunction("extern_load_short");
     } else {
         load_fn = M.getFunction("extern_load_int");
@@ -73,8 +113,19 @@ void replace_extern_load(Module &M, LoadInst *op) {
        throw functionNotFoundException;
     }
 
-    args_vector.push_back(ConstantInt::get(Type::getInt32Ty(M.getContext()),
-                elem_size, false));
+    unsigned struct_size;
+    int struct_off = get_struct_info(M, ptr_op, &struct_size);
+    if (struct_off < 0) { // Not accessing a struct field
+        args_vector.push_back(ConstantInt::get(Type::getInt32Ty(M.getContext()),
+                    value_elem_size, false));
+        args_vector.push_back(ConstantInt::get(Type::getInt32Ty(M.getContext()),
+                    0, false));
+    } else { // Accessing a struct field
+        args_vector.push_back(ConstantInt::get(Type::getInt32Ty(M.getContext()),
+                    struct_size, false));
+        args_vector.push_back(ConstantInt::get(Type::getInt32Ty(M.getContext()),
+                    struct_off, false));
+    }
 
     ArrayRef<Value *> args = ArrayRef<Value *>(args_vector);
 
@@ -82,6 +133,36 @@ void replace_extern_load(Module &M, LoadInst *op) {
     op->replaceAllUsesWith(new_ld);
     errs() << "Replace done\n";
     new_ld->dump();
+}
+
+void replace_extern_memcpy(Module &M, CallInst *op, bool stripedStore) {
+    IRBuilder<> builder(op);
+    Function *memcpy_fn;
+    if (stripedStore) {
+        memcpy_fn = M.getFunction("extern_store_memcpy");
+    } else {
+        memcpy_fn = M.getFunction("extern_load_memcpy");
+    }
+    Type *char_ptr = Type::getInt8PtrTy(M.getContext(), 0);
+    Type *int_t = Type::getInt32Ty(M.getContext());
+    std::vector<Value *> args_vector;
+
+    assert(op->getNumArgOperands() == 4); // dest, src, len, isvolatile
+
+    // Make all arguments pointers to address space 0 so types match runtime fn
+    Value *dest_ptr = op->getArgOperand(0);
+    Value *src_ptr = op->getArgOperand(1);
+    Value *isvol = builder.CreateIntCast(op->getArgOperand(3), int_t, false);
+    // Create argument list to pass to memcpy
+    args_vector.push_back(dest_ptr);
+    args_vector.push_back(src_ptr);
+    args_vector.push_back(op->getArgOperand(2));
+    ArrayRef<Value *> args = ArrayRef<Value *>(args_vector);
+
+    Value *new_memcpy = builder.CreateCall(memcpy_fn, args);
+    op->replaceAllUsesWith(new_memcpy);
+    errs() << "Memcpy replace done\n";
+    new_memcpy->dump();
 }
 
 
@@ -96,6 +177,37 @@ namespace {
                 return cast<ConstantInt>(var->getInitializer())->getSExtValue();
             }
             return -1;
+        }
+
+        bool isMemcpy(Function *F) {
+            if (F != NULL) {
+                StringRef fname = F->getName();
+                if (fname.startswith(StringRef("llvm.memcpy"))) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        bool shouldReplaceMemcpy(CallInst *op, bool *stripedStore) {
+            for (int i = 0; i < op->getNumArgOperands(); i++) {
+                if (auto *bc = dyn_cast<BitCastInst>(op->getArgOperand(i))) {
+                    if (auto *pt = dyn_cast<PointerType>(bc->getSrcTy())) {
+                        if (pt->getAddressSpace() == STRIPE) {
+                            *stripedStore = (i == 0);
+                            return true;
+                        }
+                    }
+                    if (auto *pt = dyn_cast<PointerType>(bc->getDestTy())) {
+                        if (pt->getAddressSpace() == STRIPE) {
+                            *stripedStore = (i == 0);
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
         }
 
         class AddressSpaceException: public std::exception {
@@ -155,6 +267,16 @@ namespace {
                                 }
                                 errs() << "\n";
                             }
+                        } else if (auto* op = dyn_cast<CallInst>(&I)) {
+                            Function *F = op->getCalledFunction();
+                            if (!isMemcpy(F)) { continue;}
+                            op->dump();
+                            bool stripedStore;
+                            if (shouldReplaceMemcpy(op, &stripedStore)) {
+                                replace_extern_memcpy(M, op, stripedStore);
+                                insts_to_remove.push_back(op);
+                            }
+                            errs() << "\n";
                         }
                     }
                 }
@@ -175,7 +297,6 @@ namespace {
                 Attribute attr = Attribute::get(M.getContext(), "static", "true");
                 F->addAttribute(0, attr);
             }
-
 
             return true;
         }
