a07152e509a50b7ca564c7e16d20b73fb4c848b9
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-04-10 17:54:50 -0700

    Moved malloc data to dram

--- software/spmd/fhello/main.c
+++ software/spmd/fhello/main.c
@@ -3,11 +3,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <machine/bsg_newlib_fs.h>
 
 int main() {
   bsg_set_tile_x_y();
 
-  if ((bsg_x == 0) && (bsg_y == bsg_tiles_Y-1)) {
+  if ((__bsg_x == 0) && (__bsg_y == bsg_tiles_Y-1)) {
+    if(bsg_newlib_fs_init() < 0)
+      bsg_printf("bsg_newlib_fs_init failed!\n");
+
     FILE *stdinfile = fopen("stdin", "w");
     bsg_printf("(%d, %d): stdin opened: fd = %x\n", bsg_x, bsg_y, stdinfile);
     FILE *stdoutfile = fopen("stdout", "w");
@@ -15,7 +20,11 @@ int main() {
     FILE *stderrfile = fopen("stderr", "w");
     bsg_printf("(%d, %d): stderr opened: fd = %x\n", bsg_x, bsg_y, stderrfile);
 
-    size_t write_bytes = fprintf(stdoutfile, "Hello! This is Little FS!\n");
+
+    const char* msg = "Hello! This is Little FS!";
+    size_t read_buf_size = strlen(msg) + 1;
+
+    size_t write_bytes = fprintf(stdoutfile, msg);
     bsg_printf("(%d, %d): written %d chars to stdout\n", bsg_x, bsg_y, write_bytes);
     
     int ret = fclose(stdinfile);
@@ -25,15 +34,15 @@ int main() {
     ret = fclose(stderrfile);
     bsg_printf("(%d, %d): stderr closed: ret = %d\n", bsg_x, bsg_y, ret);
 
-    char *read_buf = (char *) malloc(50);
-
+    char *read_buf = (char *) malloc(sizeof(char) * read_buf_size);
     stdoutfile = fopen("stdout", "r");
     bsg_printf("(%d, %d): stdout opened for reading; fd = %x\n", bsg_x, bsg_y, stdoutfile);
-    size_t read_bytes = fread(read_buf, 1, 50, stdoutfile);
-    bsg_printf("(%d, %d): read %d bytes from stdout\n", bsg_x, bsg_y, read_bytes);
+    size_t read_bytes = fread(read_buf, 1, read_buf_size, stdoutfile);
+    bsg_printf("(%d, %d): read %d bytes from stdout and strlen= %d\n", bsg_x, bsg_y, read_bytes, strlen(read_buf));
     ret = fclose(stdoutfile);
     bsg_printf("(%d, %d): stdout closed: ret = %d\n", bsg_x, bsg_y, ret);
-    bsg_printf("(%d, %d): %s\n", bsg_x, bsg_y, read_buf);
+
+    bsg_printf("(%d, %d): %c\n", bsg_x, bsg_y, read_buf);
 
     bsg_finish();
   }
