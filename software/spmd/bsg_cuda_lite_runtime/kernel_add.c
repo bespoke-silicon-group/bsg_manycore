a8a16e25d6d0acb1220ab59c79c3e4beb089184c
Ashwin Hari <ashwin.hari1836@gmail.com>
2019-04-12 09:26:39 -0700

    Modified kernel addition program to start at an index based on the tile's coordinates.

--- software/spmd/bsg_cuda_lite_runtime/kernel_add.c
+++ software/spmd/bsg_cuda_lite_runtime/kernel_add.c
@@ -4,7 +4,8 @@
 #include "bsg_set_tile_x_y.h"
 
 int  __attribute__ ((noinline)) kernel_add(int *a, int *b, int *c, int n) {
-  for (int i = 0; i < n; i++) {
+  int id = bsg_x_y_to_id(__bsg_x, __bsg_y);
+  for (int i = (id * n); i < (id * n + n); i++) {
   	c[i] = a[i] + b[i];
   }
   return 0;
