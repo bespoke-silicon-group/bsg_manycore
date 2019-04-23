0d755c986a322dd274b1bffb208bbf3db661bf92
tommie <46542701+tommydcjung@users.noreply.github.com>
2019-04-23 15:23:15 -0700

    Remove fpu (#16)
    * minor refactor
    
    * minor refactor
    
    * fix non-blocking
    
    * removed fpu
    
    * minor refactor
    
    * minor refactor
    
    * fix non-blocking
    
    * removed fpu
    
    * remove fpu
    
    * minor refactor
    
    * minor refactor
    
    * fix non-blocking
    
    * removed fpu
    
    * minor refactor
    
    * remove fpu

--- v/bsg_manycore_hetero_socket.v
+++ v/bsg_manycore_hetero_socket.v
@@ -14,10 +14,6 @@
 
 `include "bsg_manycore_packet.vh"
 
-`ifdef bsg_FPU
-`include "float_definitions.vh"
-`endif
-
 `define HETERO_TYPE_MACRO(BMC_TYPE,BMC_TYPE_MODULE)             \
    if (hetero_type_p == (BMC_TYPE))                             \
      begin: h                                                   \
@@ -31,10 +27,9 @@
                           ,.dram_ch_addr_width_p ( dram_ch_addr_width_p )  \
                           ,.dram_ch_start_col_p  ( dram_ch_start_col_p) \
                           ,.debug_p(debug_p)                    \
-			  ,.icache_entries_p(icache_entries_p)            \
+			                    ,.icache_entries_p(icache_entries_p)            \
                           ,.icache_tag_width_p (icache_tag_width_p) \
-            		  ,.max_out_credits_p(max_out_credits_p)\
-                          ,.hetero_type_p(hetero_type_p)        \
+            		          ,.max_out_credits_p(max_out_credits_p)\
                           ) z                                   \
           (.clk_i                                               \
            ,.reset_i                                            \
@@ -42,67 +37,57 @@
            ,.link_sif_o                                         \
            ,.my_x_i                                             \
            ,.my_y_i                                             \
-           ,.freeze_o                                           \
- `ifdef bsg_FPU                                                 \
-           ,.fam_out_s_i                                        \
-           ,.fam_in_s_o                                         \
- `endif                                                         \
            );                                                   \
      end
 
-module bsg_manycore_hetero_socket #(  x_cord_width_p      = "inv"
-                                    , y_cord_width_p    = "inv"
-                                    , data_width_p      = 32
-                                    , addr_width_p      = "inv"
-                                    , load_id_width_p   = 5
-                                    , dmem_size_p       = "inv"
-                                    , epa_byte_addr_width_p  = "inv"
-                                    , dram_ch_addr_width_p = "inv"
-                                    , dram_ch_start_col_p = 0
-                                    , debug_p           = 0
-				    , icache_entries_p       = "inv" // in words
-                                    , icache_tag_width_p= "inv"
-                	            , max_out_credits_p = 200
-                                    , hetero_type_p     = 1
-                                    , bsg_manycore_link_sif_width_lp = `bsg_manycore_link_sif_width(addr_width_p,data_width_p,x_cord_width_p,y_cord_width_p,load_id_width_p)
-                                    )
-   (  input   clk_i
+module bsg_manycore_hetero_socket
+  #(parameter x_cord_width_p = "inv"
+    , parameter y_cord_width_p = "inv"
+    , parameter data_width_p = "inv"
+    , parameter addr_width_p = "inv"
+    , parameter load_id_width_p = "inv"
+    , parameter dmem_size_p = "inv"
+    , parameter epa_byte_addr_width_p  = "inv"
+    , parameter dram_ch_addr_width_p = "inv"
+		, parameter icache_entries_p = "inv" // in words
+    , parameter icache_tag_width_p = "inv"
+    , parameter dram_ch_start_col_p = 0
+    , parameter debug_p = 0
+    , parameter max_out_credits_p = 200
+    , parameter hetero_type_p = 0
+    , localparam bsg_manycore_link_sif_width_lp =
+      `bsg_manycore_link_sif_width(addr_width_p,data_width_p,x_cord_width_p,y_cord_width_p,load_id_width_p)
+  )
+  (
+    input clk_i
     , input reset_i
 
     // input and output links
-    , input  [bsg_manycore_link_sif_width_lp-1:0] link_sif_i
+    , input [bsg_manycore_link_sif_width_lp-1:0] link_sif_i
     , output [bsg_manycore_link_sif_width_lp-1:0] link_sif_o
 
     // tile coordinates
-    , input   [x_cord_width_p-1:0]                my_x_i
-    , input   [y_cord_width_p-1:0]                my_y_i
-    , output logic freeze_o
+    , input [x_cord_width_p-1:0] my_x_i
+    , input [y_cord_width_p-1:0] my_y_i
+  );
 
- `ifdef bsg_FPU
-    , input  f_fam_out_s                         fam_out_s_i 
-    , output f_fam_in_s                          fam_in_s_o 
- `endif
-
-    );
-
-   // add as many types as you like...
-   `HETERO_TYPE_MACRO(0,bsg_manycore_proc_vanilla) else
-   `HETERO_TYPE_MACRO(1,bsg_manycore_gather_scatter) else
-   `HETERO_TYPE_MACRO(2,bsg_manycore_accel_default) else
-   `HETERO_TYPE_MACRO(3,bsg_manycore_accel_default) else
-   `HETERO_TYPE_MACRO(4,bsg_manycore_accel_default) else
-   `HETERO_TYPE_MACRO(5,bsg_manycore_accel_default) else
-   `HETERO_TYPE_MACRO(6,bsg_manycore_accel_default) else
-   `HETERO_TYPE_MACRO(7,bsg_manycore_accel_default) else
-   `HETERO_TYPE_MACRO(8,bsg_manycore_accel_default) else
-     begin : nh
-	// synopsys translate_off
-        initial
-          begin
-             $error("## unidentified hetero core type ",hetero_type_p);
-             $finish();
-          end
-        // synopsys translate_on
-     end
+  // add as many types as you like...
+  `HETERO_TYPE_MACRO(0,bsg_manycore_proc_vanilla) else
+  `HETERO_TYPE_MACRO(1,bsg_manycore_gather_scatter) else
+  `HETERO_TYPE_MACRO(2,bsg_manycore_accel_default) else
+  `HETERO_TYPE_MACRO(3,bsg_manycore_accel_default) else
+  `HETERO_TYPE_MACRO(4,bsg_manycore_accel_default) else
+  `HETERO_TYPE_MACRO(5,bsg_manycore_accel_default) else
+  `HETERO_TYPE_MACRO(6,bsg_manycore_accel_default) else
+  `HETERO_TYPE_MACRO(7,bsg_manycore_accel_default) else
+  `HETERO_TYPE_MACRO(8,bsg_manycore_accel_default) else
+  begin : nh
+	  // synopsys translate_off
+    initial begin
+      $error("## unidentified hetero core type ",hetero_type_p);
+      $finish();
+    end
+    // synopsys translate_on
+  end
 
 endmodule
