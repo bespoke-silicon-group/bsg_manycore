39c421ed93da9df6ca82b3c147465e1e8a73e6cd
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-04-29 03:23:34 +0000

    Removed extra tests, working on only one test for now.

--- software/spmd/bsg_cuda_lite_runtime/kernel4.c
+++
@@ -1,8 +0,0 @@
-//This is the kernel with 4 arguments
-
-#include "bsg_manycore.h"
-
-int  __attribute__ ((noinline)) kernel4( int p0, int p1, int p2, int p3 ){
-        bsg_printf("Kernel4 param4=%08x,%08x,%08x,%08x\n", p0, p1, p2, p3);
-        return 0;
-}
