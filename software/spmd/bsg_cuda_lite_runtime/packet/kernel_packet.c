e9ef01172ecbd81d0040633d9e07e4daf087aad5
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-05-07 19:34:35 +0000

    1. Added a cuda packet test.

--- software/spmd/bsg_cuda_lite_runtime/packet/kernel_packet.c
+++ software/spmd/bsg_cuda_lite_runtime/packet/kernel_packet.c
@@ -1,4 +1,4 @@
-//This kernel adds 2 vectors 
+//This kernel sends an IO packet from each tile with the __bsg_id 
 
 #include "bsg_manycore.h"
 #include "bsg_set_tile_x_y.h"
@@ -6,15 +6,13 @@
 /*
 #define BSG_TILE_GROUP_X_DIM bsg_tiles_X
 #define BSG_TILE_GROUP_Y_DIM bsg_tiles_Y
-#include "bsg_tile_group_barrier.h"
+#include "bsg_tile_group_barrier.h" 
 
 INIT_TILE_GROUP_BARRIER(r_barrier, c_barrier, 0, bsg_tiles_X-1, 0, bsg_tiles_Y-1);
 */
 
-int  __attribute__ ((noinline)) kernel_vec_add(int *a, int *b, int *c, int n) {
-  for (int i = (__bsg_id * n); i < (__bsg_id * n + n); i++) {
-  	c[i] = a[i] + b[i];
-  }
+int  __attribute__ ((noinline)) kernel_packet() {
+  bsg_remote_ptr_io_store(IO_X_INDEX, 0x2000, __bsg_id);
   //bsg_tile_group_barrier(&r_barrier, &c_barrier);
   return 0;
 }
