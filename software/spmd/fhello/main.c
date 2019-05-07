81f5f59c03e8d0ffeaaecc28f3eb6a64e3ddd4bc
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-05-07 16:25:41 -0700

    fhello with C stdio

--- software/spmd/fhello/main.c
+++ software/spmd/fhello/main.c
@@ -1,26 +1,25 @@
-#include "bsg_manycore.h"
-#include "bsg_set_tile_x_y.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "bsg_manycore.h"
+#include "bsg_set_tile_x_y.h"
 
 int main() {
   if ((__bsg_x == 0) && (__bsg_y == bsg_tiles_Y-1)) {
+    char c;
+
+    // Read from a file
     FILE *hello = fopen("hello.txt", "r");
     if(hello == NULL)
-      bsg_printf("file open error!\n");
+      return -1;
 
-    char *line = (char *) malloc(sizeof(char) * 128);
-
-    if(fgets(line, 128, hello) != NULL) {
-      bsg_printf("%s\n", line);
-    } else {
-      bsg_printf("Error reading file\n");
+    while((c = fgetc(hello)) != '\n') {
+      putchar(c);
     }
-
+    putchar('\n');
+    
     fclose(hello);
-    bsg_finish();
+    return 0;
   }
 
   bsg_wait_while(1);
