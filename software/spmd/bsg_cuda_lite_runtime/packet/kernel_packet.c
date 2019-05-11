15bf8b07cfb66b16a12b23a77a56d048999af5be
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-05-11 05:02:18 +0000

    1. Added and tested barrier to the end of bsg_cuda_lite_runtime.h 2. Pinned down __bsg_x/y, __bsg_grp_org_x/y, and __bsg_id in linker script. 3. Cleaned up bsg_cuda_lite_runtime tests.

--- software/spmd/bsg_cuda_lite_runtime/packet/kernel_packet.c
+++ software/spmd/bsg_cuda_lite_runtime/packet/kernel_packet.c
@@ -3,16 +3,11 @@
 #include "bsg_manycore.h"
 #include "bsg_set_tile_x_y.h"
 
-/*
-#define BSG_TILE_GROUP_X_DIM bsg_tiles_X
-#define BSG_TILE_GROUP_Y_DIM bsg_tiles_Y
-#include "bsg_tile_group_barrier.h" 
-
-INIT_TILE_GROUP_BARRIER(r_barrier, c_barrier, 0, bsg_tiles_X-1, 0, bsg_tiles_Y-1);
-*/
-
 int  __attribute__ ((noinline)) kernel_packet() {
-  bsg_remote_ptr_io_store(IO_X_INDEX, 0x2000, __bsg_id);
-  //bsg_tile_group_barrier(&r_barrier, &c_barrier);
+  bsg_remote_ptr_io_store(IO_X_INDEX, 0x1000, __bsg_x);
+  bsg_remote_ptr_io_store(IO_X_INDEX, 0x2000, __bsg_y);
+  bsg_remote_ptr_io_store(IO_X_INDEX, 0x3000, __bsg_grp_org_x);
+  bsg_remote_ptr_io_store(IO_X_INDEX, 0x4000, __bsg_grp_org_y);
+  bsg_remote_ptr_io_store(IO_X_INDEX, 0x5000, __bsg_id);
   return 0;
 }
