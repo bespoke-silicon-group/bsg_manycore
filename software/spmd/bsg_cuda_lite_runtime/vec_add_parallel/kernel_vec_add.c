a82749d0ae30d3bfe0946bb08156193ef0cd9b4b
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-05-12 06:04:26 +0000

    Cleaned up parallel vector addition kernel.

--- software/spmd/bsg_cuda_lite_runtime/vec_add_parallel/kernel_vec_add.c
+++ software/spmd/bsg_cuda_lite_runtime/vec_add_parallel/kernel_vec_add.c
@@ -4,9 +4,11 @@
 #include "bsg_set_tile_x_y.h"
 
 int  __attribute__ ((noinline)) kernel_vec_add(int *a, int *b, int *c, int n) {
-  int id = bsg_x_y_to_id(__bsg_x, __bsg_y);
-  for (int i = (id * n); i < (id * n + n); i++) {
-  	c[i] = a[i] + b[i];
+  for (int i = (__bsg_id * n); i < (__bsg_id * n + n); i++) {
+	bsg_remote_ptr_io_store(IO_X_INDEX, 0x1000, a[i]);
+	bsg_remote_ptr_io_store(IO_X_INDEX, 0x2000, b[i]);
+    	c[i] = a[i] + b[i];
   }
+
   return 0;
 }
