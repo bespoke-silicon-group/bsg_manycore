c56a832730a6334abfc903023acf5e45dd73be76
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-05-13 01:59:07 +0000

    1. Added __bsg_tile_group_id and __bsg_grid_size symbols to the linker script. 2. Modified the bsg_cuda_lite_runtime/vec_add and vec_add_parallel tests to use the mentioned symbols. 3. Modified bsg_manycore_lib/bsg_set_tile_x_y.c to also reset the mentioned symbols.

--- software/spmd/bsg_cuda_lite_runtime/vec_add/kernel_vec_add.c
+++ software/spmd/bsg_cuda_lite_runtime/vec_add/kernel_vec_add.c
@@ -3,9 +3,12 @@
 #include "bsg_manycore.h"
 #include "bsg_set_tile_x_y.h"
 
-int  __attribute__ ((noinline)) kernel_vec_add(int *a, int *b, int *c, int n) {
-  for (int i = (__bsg_id * n); i < (__bsg_id * n + n); i++) {
-  	c[i] = a[i] + b[i];
+int  __attribute__ ((noinline)) kernel_vec_add(int *a, int *b, int *c, int array_size) {
+  int id = __bsg_tile_group_id * __bsg_grid_size + __bsg_id;
+  int block_size = array_size / (__bsg_grid_size * bsg_tiles_X * bsg_tiles_Y);
+
+  for (int i = id * block_size; i < (id + 1) * block_size; i ++) {
+    	c[i] = a[i] + b[i];
   }
   return 0;
 }
