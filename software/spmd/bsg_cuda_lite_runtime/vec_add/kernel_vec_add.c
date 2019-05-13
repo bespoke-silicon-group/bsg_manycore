9f4001a084eebf7c04cd67e0eef170e7bde7d72d
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-05-13 23:09:25 +0000

    1. Changed __bsg_grid_size into two dimensional __bsg_grid_dim_x & __bsg_grid_dim_y 2. Changed __bsg_tile_group_id into two dimensional __bsg_tile_group_id_x/y 3. Modified software/bsg_manycore_lig/bsg_set_tile_x_y.c/h accordingly 4. Added the new symbols to linker script. 5. Modified bsg_cuda_lite_runtime regression tests accordingly.

--- software/spmd/bsg_cuda_lite_runtime/vec_add/kernel_vec_add.c
+++ software/spmd/bsg_cuda_lite_runtime/vec_add/kernel_vec_add.c
@@ -4,8 +4,8 @@
 #include "bsg_set_tile_x_y.h"
 
 int  __attribute__ ((noinline)) kernel_vec_add(int *a, int *b, int *c, int array_size) {
-  int id = __bsg_tile_group_id * __bsg_grid_size + __bsg_id;
-  int block_size = array_size / (__bsg_grid_size * bsg_tiles_X * bsg_tiles_Y);
+  int id = __bsg_tile_group_id_x * __bsg_grid_dim_x + __bsg_id;
+  int block_size = array_size / (__bsg_grid_dim_x * bsg_tiles_X * bsg_tiles_Y);
 
   for (int i = id * block_size; i < (id + 1) * block_size; i ++) {
     	c[i] = a[i] + b[i];
