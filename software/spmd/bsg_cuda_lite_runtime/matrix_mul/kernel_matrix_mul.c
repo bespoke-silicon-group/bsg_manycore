334d224d11d5037484e5e2f0957c53a971081222
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-05-14 05:31:43 +0000

    1. Changed __bsg_grid_size to __bsg_grid_dim_x/y and __bsg_tile_group_id to __bsg_tile_group_id_x/y symbols. 2. Modified linker script and software/bsg_cuda_lite_runtime accordingly 3. Added Matrix Multiplication with Shared Memory cuda lite runtime test. (not working yet) 4. Small modification in matrix mul test.

--- software/spmd/bsg_cuda_lite_runtime/matrix_mul/kernel_matrix_mul.c
+++ software/spmd/bsg_cuda_lite_runtime/matrix_mul/kernel_matrix_mul.c
@@ -8,21 +8,23 @@
 
 int  __attribute__ ((noinline)) kernel_matrix_mul(int *A, int *B, int *C, int n) {
 
-	int block_size_x = n / (__bsg_grid_dim_x * bsg_tiles_X);
-	int block_size_y = n / (__bsg_grid_dim_y * bsg_tiles_Y);
+	int thread_count_x = n / (__bsg_grid_dim_x * bsg_tiles_X);
+	int thread_count_y = n / (__bsg_grid_dim_y * bsg_tiles_Y);
 
-	int start_y = (__bsg_tile_group_id_y * bsg_tiles_Y + __bsg_y) * block_size_y;
-	int start_x = (__bsg_tile_group_id_x * bsg_tiles_X + __bsg_x) * block_size_x;
+	int start_y = (__bsg_tile_group_id_y * bsg_tiles_Y + __bsg_y) * block_size_x;
+	int start_x = (__bsg_tile_group_id_x * bsg_tiles_X + __bsg_x) * block_size_y;
 
 
-	for (int iter_y = start_y; iter_y < start_y + block_size_y; iter_y ++) { 
-		for (int iter_x = start_x; iter_x < start_x + block_size_x; iter_x ++) { 
+	for (int iter_y = 0 ; iter_y < thread_count_y; iter_y ++) { 
+		for (int iter_x = 0; iter_x < thead_count_x; iter_x ++) { 
 
-			int res = 0;
+			int sum = 0;
+			int id_y = start_y + iter_y; 
+			int id_x = start_x + iter_x;
 			for (int k = 0; k < n; k ++) { 
-				res += A[iter_y * n + k] * B[k * n + iter_x];
+				sum += A[id_y * n + k] * B[k * n + id_x];
 			}
-			C[iter_y * n + iter_x] = res;
+			C[id_y * n + id_x] = res;
 		}
 	}
 
