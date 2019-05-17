adbb1583514d7e2cf673c187d0fb43ea1210b853
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-05-16 17:43:26 -0700

    Moved stdio closing calls to newlib

--- software/bsg_manycore_lib/bsg_newlib_intf.c
+++ software/bsg_manycore_lib/bsg_newlib_intf.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <unistd.h>
 #include <machine/bsg_newlib_fs.h>
 #include "bsg_manycore.h"
 #include "bsg_set_tile_x_y.h"
@@ -20,18 +19,11 @@ void bsg_newlib_init(void) {
 }
 
 void bsg_newlib_exit(int exit_status) {
-  // close stdio
-  close(0);
-  close(1);
-  close(2);
-
   if(exit_status == EXIT_SUCCESS) {
     bsg_finish();
   } else {
     bsg_fail();
   }
-
-  bsg_wait_while(1);
 }
 
 void bsg_newlib_sendchar(char ch) {
