b5fc91b492c96dcf1b4ef35fed80e72ed00ce7c3
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-05-17 05:35:09 +0000

    Modified bsg_cuda_lite_runtime matrix_mul_shared_mem. Still doesn't work.

--- software/spmd/bsg_cuda_lite_runtime/matrix_mul_shared_mem/kernel_matrix_mul_shared_mem.c
+++ software/spmd/bsg_cuda_lite_runtime/matrix_mul_shared_mem/kernel_matrix_mul_shared_mem.c
@@ -1,6 +1,5 @@
 /*!
  * This kernel performs tiled matrix multiplication with use of tile-group-shared memory
- * For now the matrices are assumed to have the same X/Y dimension n.
  */
 
 #include "bsg_manycore.h"
@@ -11,95 +10,120 @@
 #include "bsg_tile_group_barrier.h"
 INIT_TILE_GROUP_BARRIER(r_barrier, c_barrier, 0, bsg_tiles_X-1, 0, bsg_tiles_Y-1);
 
+#define BLOCK_WIDTH 4
 
 
 
+void __attribute__ ((noinline)) subblock2shmem (int *A, int *sh_dest, int M, int N, int block_size_y, int block_size_x, int sub_block_y, int sub_block_x) { 
 
-int  __attribute__ ((noinline)) kernel_matrix_mul_shared_mem(int *A, int *B, int *C, int n) {
-
-	int thread_count_x = n / (__bsg_grid_dim_x * bsg_tiles_X);
-	int thread_count_y = n / (__bsg_grid_dim_y * bsg_tiles_Y);
-
-	int start_y = (__bsg_tile_group_id_y * bsg_tiles_Y + __bsg_y) * thread_count_y;
-	int start_x = (__bsg_tile_group_id_x * bsg_tiles_X + __bsg_x) * thread_count_x;
-
-	int sh_dim_x = thread_count_x * bsg_tiles_X;
-	int sh_dim_y = thread_count_y * bsg_tiles_Y;	
-
-
-	int *sh_A, *sh_B, *sh_C;
-	bsg_tilegroup_int (sh_A, (sh_dim_y * sh_dim_x));
-	bsg_tilegroup_int (sh_B, (sh_dim_y * sh_dim_x));
-	bsg_tilegroup_int (sh_C, (sh_dim_y * sh_dim_x));
-
-
-
-	for (int square_id = 0; square_id < n / sh_dim_x; square_id ++) { 
-
-
-		// Each tile loads a (thread_count_x * thread_count_y) bloack of A & B from DRAM into shared memory 
-		// Each iteration of the loop represetns a thread
-		for (int iter_y = 0; iter_y < thread_count_y; iter_y ++) { 
-			for (int iter_x = 0; iter_x < thread_count_x; iter_x ++) { 
-
-				int A_id_y = start_y + iter_y;
-				int A_id_x = sh_dim_x * square_id + __bsg_x * thread_count_x + iter_x;
-				int B_id_y = sh_dim_y * square_id + __bsg_y * thread_count_y + iter_y;
-				int B_id_x = start_x + iter_x; 
-				
-				int sh_id_y = __bsg_y * thread_count_y + iter_y;
-				int sh_id_x = __bsg_x * thread_count_x + iter_x;
+	int start_y = sub_block_y * block_size_y;
+	int start_x = sub_block_x * block_size_x;
 	
-
-				bsg_tilegroup_store(sh_A, (sh_id_y * sh_dim_x + sh_id_x)  , A[A_id_y * n + A_id_x]);	
-				bsg_tilegroup_store(sh_B, (sh_id_y * sh_dim_x + sh_id_x)  , B[B_id_y * n + B_id_x]);	
-			}
+	for (int iter_y = __bsg_y; iter_y < block_size_y; iter_y += bsg_tiles_Y) { 
+		for (int iter_x = __bsg_x; iter_x < block_size_x; iter_x += bsg_tiles_X) { 
+			// sh_dest[iter_y][iter_x] <-- A[iter_y + start_y][iter_x + start_x]
+			bsg_tilegroup_store (sh_dest, (iter_y * block_size_x + iter_x), A[((iter_y + start_y) * N + iter_x + start_x)]);
 		}
-	
-		bsg_tile_group_barrier(&r_barrier, &c_barrier); 
-	
-		for (int iter_y = 0; iter_y < thread_count_y; iter_y ++) { 
-			for (int iter_x = 0; iter_x < thread_count_x; iter_x ++) { 
+	}
+	return; 
+}
 
-				int sum = 0; 
-				for (int k = 0; k < sh_dim_x; k ++) { 
-					int lc_A, lc_B;
-					bsg_tilegroup_load(sh_A, (iter_y * sh_dim_x + k), lc_A);
-					bsg_tilegroup_load(sh_B, (k * sh_dim_x + iter_x), lc_B);
-					
-					sum += lc_A * lc_B;
-				}
+
+void __attribute__ ((noinline)) subblock2shmem_xposed (int *A, int *sh_dest, int M, int N, int block_size_y, int block_size_x, int sub_block_y, int sub_block_x) { 
+
+	int start_y = sub_block_y * block_size_y;
+	int start_x = sub_block_x * block_size_x;
+	
+	for (int iter_y = __bsg_y; iter_y < block_size_y; iter_y += bsg_tiles_Y) { 
+		for (int iter_x = __bsg_x; iter_x < block_size_x; iter_x += bsg_tiles_X) { 
+			// sh_dest[iter_x][iter_y] <-- A[iter_y + start_y][iter_x + start_x]
+			bsg_tilegroup_store (sh_dest, (iter_x * block_size_x + iter_y), A[((iter_y + start_y) * N + iter_x + start_x)]);
+		}
+	}
+	return; 
+}
+
+
+void __attribute__ ((noinline)) shmem2subblock (int *A, int *sh_src, int M, int N, int block_size_y, int block_size_x, int sub_block_y, int sub_block_x) { 
+
+	int start_y = sub_block_y * block_size_y;
+	int start_x = sub_block_x * block_size_x;
+	
+	for (int iter_y = __bsg_y; iter_y < block_size_y; iter_y += bsg_tiles_Y) { 
+		for (int iter_x = __bsg_x; iter_x < block_size_x; iter_x += bsg_tiles_X) { 
+			// A[iter_y + start_y][iter_x + start_x] <-- sh_src[iter_y][iter_x]
+			bsg_tilegroup_load (sh_src, (iter_y * block_size_x + iter_x), A[((iter_y + start_y) * N + iter_x + start_x)]);
+		}
+	}
+	return; 
+}
+
+
+void __attribute__ ((noinline)) subblock_shmem_matrix_mul_xposed (int *sh_A, int *sh_B, int *sh_C, int M, int N, int P, int block_size_y, int block_size_x, int block_num) { 
+
+	
+	for (int iter_y = __bsg_y; iter_y < block_size_y; iter_y += bsg_tiles_Y) { 
+		for (int iter_x = __bsg_x; iter_x < block_size_x; iter_x += bsg_tiles_X) { 
+
+			int sum = 0; 
+			int lc_A, lc_B;
+			for (int k = 0; k < BLOCK_WIDTH; k ++) { 
+				// lc_A <-- sh_A[iter_y][iter_x]
+				bsg_tilegroup_load (sh_A, (iter_y * BLOCK_WIDTH + k), lc_A); 
+				// lc_B <-- sh_B[iter_y][iter_x]	remember B is transposed
+				bsg_tilegroup_load (sh_B, (iter_x * BLOCK_WIDTH + k), lc_B);
+				sum += lc_A * lc_B;
+			}
+
+			if (!block_num) { 
+				// sh_C[iter_y][iter_x] <-- sum
+				bsg_tilegroup_store (sh_C, (iter_y * block_size_x + iter_x), sum);
+			}
+			else { 
 				int lc_C;
-				// at first iteration initalize lc_C to zero
-				if (!square_id) { 
-					bsg_tilegroup_store(sh_C, (iter_y * sh_dim_x + iter_x), sum);
-				}
-				else {
-					
-					int lc_C;
-					bsg_tilegroup_load(sh_C, (iter_y * sh_dim_x + iter_x), lc_C);
-					bsg_tilegroup_store(sh_C, (iter_y * sh_dim_x + iter_x) , (lc_C + sum));
-				}
-			}
-		}
-
-		bsg_tile_group_barrier(&r_barrier, &c_barrier); 			
-	}
-
-	// Each tile stores a (thread_count_x * thread_count_y) block of results back into DRAM 
-	for (int iter_y = 0; iter_y < thread_count_y; iter_y ++) { 
-		for (int iter_x = 0; iter_x < thread_count_x; iter_x ++) { 
-			int C_id_y = start_y + iter_y;
-			int C_id_x = start_x + iter_x;
-			int sh_id_y = __bsg_y * thread_count_y + iter_y; 
-			int sh_id_x = __bsg_x * thread_count_x + iter_x;
-			
-			bsg_tilegroup_load (sh_C, (sh_id_y * sh_dim_x + sh_id_x), C[C_id_y * n + C_id_x]);
+				// sh_C[iter_y][iter_x] += sum
+				bsg_tilegroup_load (sh_C, (iter_y * block_size_x + iter_x), lc_C);
+				bsg_tilegroup_store (sh_C, (iter_y * block_size_x + iter_x), lc_C + sum);
+			} 
 		}
 	}
+	return;
+}
 
-	bsg_tile_group_barrier(&r_barrier, &c_barrier);
+
+
+
+
+
+
+int  __attribute__ ((noinline)) kernel_matrix_mul_shared_mem(int *A, int *B, int *C, int M, int N, int P, int block_size_y, int block_size_x) {
+
 	
+	// declare tile-group shared memory
+	int *sh_A, *sh_B, *sh_C;
+	bsg_tilegroup_int (sh_A, (block_size_y * BLOCK_WIDTH));
+	bsg_tilegroup_int (sh_B, (BLOCK_WIDTH * block_size_x));
+	bsg_tilegroup_int (sh_C, (block_size_y * block_size_x));
+
+
+	int num_blocks = N / BLOCK_WIDTH;	// *** Must divide evenly
+
+	for (int block_num = 0; block_num < num_blocks; block_num ++) { 
+
+		subblock2shmem (       A, sh_A, M, N, block_size_y, BLOCK_WIDTH, __bsg_tile_group_id_y, block_num);
+ 
+		subblock2shmem_xposed (B, sh_B, N, P, BLOCK_WIDTH, block_size_x, block_num, __bsg_tile_group_id_x);
+
+		bsg_tile_group_barrier (&r_barrier, &c_barrier);
+		
+		subblock_shmem_matrix_mul_xposed (sh_A, sh_B, sh_C, M, N, P, block_size_y, block_size_x, block_num);
+		
+		bsg_tile_group_barrier (&r_barrier, &c_barrier); 
+	}
+
+	shmem2subblock (C, sh_C, M, P, block_size_y, block_size_x, __bsg_tile_group_id_y, __bsg_tile_group_id_x); 
+
+	bsg_tile_group_barrier (&r_barrier, &c_barrier) ;
 
 	return 0;
 }
