4c47aa7d4f45feaf637ef368c3ea058947f1d59b
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-05-21 10:29:49 -0700

    Support for C cmd line args

---
+++ software/spmd/cmd_args/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+  printf("argc = %d\n", argc);
+  printf("argv[0] = %s\n", argv[0]);
+  printf("argv[1] = %s\n", argv[1]);
+}
