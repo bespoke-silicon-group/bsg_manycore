13f0fd673ff8efdb78b3275faba0dad9a6cfbed5
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-05-22 01:29:55 +0000

    Added some commentary.

--- software/spmd/bsg_cuda_lite_runtime/vec_add_shared_mem/kernel_vec_add_shared_mem.c
+++ software/spmd/bsg_cuda_lite_runtime/vec_add_shared_mem/kernel_vec_add_shared_mem.c
@@ -12,6 +12,7 @@ INIT_TILE_GROUP_BARRIER(r_barrier, c_barrier, 0, bsg_tiles_X-1, 0, bsg_tiles_Y-1
 
 int  __attribute__ ((noinline)) kernel_vec_add_shared_mem(int *A, int *B, int *C, int N, int block_size_x) {
 
+	// Declare tile-group shared memroy with specific size
 	int *sh_A, *sh_B, *sh_C;
 	bsg_tilegroup_int (sh_A, block_size_x);
 	bsg_tilegroup_int (sh_B, block_size_x); 
@@ -21,6 +22,7 @@ int  __attribute__ ((noinline)) kernel_vec_add_shared_mem(int *A, int *B, int *C
 	int start_x = block_size_x * (__bsg_tile_group_id_y * __bsg_grid_dim_x + __bsg_tile_group_id_x); 
 
 	for (int iter_x = __bsg_id; iter_x < block_size_x; iter_x += bsg_tiles_X * bsg_tiles_Y) { 
+		// Store from DRAM into tile-group shared memory
 		bsg_tilegroup_store(sh_A, iter_x, A[start_x + iter_x]); 
 		bsg_tilegroup_store(sh_B, iter_x, B[start_x + iter_x]); 
 	}
@@ -31,9 +33,11 @@ int  __attribute__ ((noinline)) kernel_vec_add_shared_mem(int *A, int *B, int *C
 
 	for (int iter_x = __bsg_id; iter_x < block_size_x; iter_x += bsg_tiles_X * bsg_tiles_Y) { 
 		int lc_A, lc_B;
-
+		// Load from tile group shared memory and store into local variable lc_A & lc_B
 		bsg_tilegroup_load (sh_A, iter_x, lc_A);
 		bsg_tilegroup_load (sh_B, iter_x, lc_B);
+
+		// Store the sum of lc_A and lc_B into tile-group shared memroy sh_C
 		bsg_tilegroup_store (sh_C, iter_x, (lc_A + lc_B));
 	}
 
@@ -42,6 +46,7 @@ int  __attribute__ ((noinline)) kernel_vec_add_shared_mem(int *A, int *B, int *C
 
 
 	for (int iter_x = __bsg_id; iter_x < block_size_x; iter_x += bsg_tiles_X * bsg_tiles_Y) { 
+		// Load from tile-group shared memory lc_C and store the result into DRAM 
 		bsg_tilegroup_load(sh_C, iter_x, C[start_x + iter_x]); 
 	}
 
