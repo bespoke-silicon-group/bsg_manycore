b1b777c407cafd8c115baf44b41ebdb2d7b971a8
tommy <tommy.dc.jung@gmail.com>
2019-05-22 15:24:04 -0700

    testbench to test fpu_float.v
    added to filelist

--- v/vanilla_bean/fpu_int.v
+++ v/vanilla_bean/fpu_int.v
@@ -17,9 +17,9 @@ module fpu_int
     , parameter data_width_p = RV32_reg_data_width_gp
   )
   (
-    input [data_width_p-1:0] rs1_i
-    , input [data_width_p-1:0] rs2_i
-    , input fp_decode_s fp_decode_i
+    input [data_width_p-1:0] a_i
+    , input [data_width_p-1:0] b_i
+    , input fp_int_decode_s fp_int_decode_i
     , output logic [data_width_p-1:0] result_o
   );
 
@@ -31,7 +31,7 @@ module fpu_int
     .e_p(e_p)
     ,.m_p(m_p)
   ) classify (
-    .a_i(rs1_i)
+    .a_i(a_i)
     ,.class_o(class_lo)
   );
 
@@ -44,20 +44,68 @@ module fpu_int
     .e_p(e_p)
     ,.m_p(m_p)
   ) f2i (
-    .a_i(rs1_i)
-    ,.signed_i()
+    .a_i(a_i)
+    ,.signed_i(f2i_signed_li)
 
-    ,.z_o()
+    ,.z_o(f2i_z_lo)
     ,.invalid_o()    
   );
 
   // cmp
   //
+  logic eq_lo;
+  logic lt_lo;
+  logic le_lo;  
+
   bsg_fpu_cmp #(
     .e_p(e_p)
     ,.m_p(m_p)
   ) cmp (
+    .a_i(a_i)
+    ,.b_i(b_i)
+
+    ,.eq_o(eq_lo)
+    ,.lt_o(lt_lo)
+    ,.le_o(le_lo)
+
+    ,.lt_le_invalid_o()
+    ,.eq_invalid_o()
+
+    ,.min_o()
+    ,.max_o()
+
+    ,.min_max_invalid_o()
   );
+
+  // ctrl logic
+  //
+  always_comb begin
+    f2i_signed_li = 1'b0;
+
+    if (fp_int_decode_i.feq_op) begin
+      result_o = {{(data_width_p-1){1'b0}}, eq_lo};
+    end
+    else if (fp_int_decode_i.fle_op) begin
+      result_o = {{(data_width_p-1){1'b0}}, le_lo};
+    end
+    else if (fp_int_decode_i.flt_op) begin
+      result_o = {{(data_width_p-1){1'b0}}, lt_lo};
+    end
+    else if (fp_int_decode_i.fcvt_w_s_op) begin
+      f2i_signed_li = 1'b1;
+      result_o = f2i_z_lo;
+    end
+    else if (fp_int_decode_i.fcvt_wu_s_op) begin
+      f2i_signed_li = 1'b0;
+      result_o = f2i_z_lo;
+    end
+    else if (fp_int_decode_i.fclass_op) begin
+      result_o = class_lo;
+    end
+    else begin
+      result_o = a_i; // covers fmv
+    end
+  end
 
 
 endmodule
