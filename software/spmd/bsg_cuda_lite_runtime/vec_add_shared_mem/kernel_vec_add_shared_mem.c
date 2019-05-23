ddeb3b81e4a16d9c3d7dd6b0517a1c27b269b78e
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-05-23 22:05:06 +0000

    Added temporary bug fix for shared memory probem in vec_add_shared_mem cuda lite runtime regression test.

--- software/spmd/bsg_cuda_lite_runtime/vec_add_shared_mem/kernel_vec_add_shared_mem.c
+++ software/spmd/bsg_cuda_lite_runtime/vec_add_shared_mem/kernel_vec_add_shared_mem.c
@@ -13,18 +13,28 @@ INIT_TILE_GROUP_BARRIER(r_barrier, c_barrier, 0, bsg_tiles_X-1, 0, bsg_tiles_Y-1
 int  __attribute__ ((noinline)) kernel_vec_add_shared_mem(int *A, int *B, int *C, int N, int block_size_x) {
 
 	// Declare tile-group shared memroy with specific size
-	int *sh_A, *sh_B, *sh_C;
-	bsg_tilegroup_int (sh_A, block_size_x);
-	bsg_tilegroup_int (sh_B, block_size_x); 
-	bsg_tilegroup_int (sh_C, block_size_x); 
+
+	// *** For now, declaration of tile-group shared memory is done by hand, as the macro is faulty. TODO: Fix *** 
+//	int *sh_A, *sh_B, *sh_C;
+//	bsg_tilegroup_int (sh_A, block_size_x);
+//	bsg_tilegroup_int (sh_B, block_size_x); 
+//	bsg_tilegroup_int (sh_C, block_size_x); 
+
+
+	int sh_A[block_size_x / (bsg_tiles_X * bsg_tiles_Y)];
+	int sh_B[block_size_x / (bsg_tiles_X * bsg_tiles_Y)];
+	int sh_C[block_size_x / (bsg_tiles_X * bsg_tiles_Y)];
+
 
 
 	int start_x = block_size_x * (__bsg_tile_group_id_y * __bsg_grid_dim_x + __bsg_tile_group_id_x); 
+
 
 	for (int iter_x = __bsg_id; iter_x < block_size_x; iter_x += bsg_tiles_X * bsg_tiles_Y) { 
 		// Store from DRAM into tile-group shared memory
 		bsg_tilegroup_store(sh_A, iter_x, A[start_x + iter_x]); 
 		bsg_tilegroup_store(sh_B, iter_x, B[start_x + iter_x]); 
+		
 	}
 
 
