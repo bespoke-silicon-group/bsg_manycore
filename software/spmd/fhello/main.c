6c4aa01269b0bd653b45b37d0237bd33028a1b54
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-06-02 02:43:40 -0700

    fhello working with spike

--- software/spmd/fhello/main.c
+++ software/spmd/fhello/main.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "bsg_manycore.h"
-#include "bsg_set_tile_x_y.h"
 
 int main() {
-  if ((__bsg_x == 0) && (__bsg_y == bsg_tiles_Y-1)) {
-    char c;
+  char c;
 
-    // Read from a file
-    FILE *hello = fopen("hello.txt", "r");
-    if(hello == NULL)
-      return -1;
+  // Read from a file
+  FILE *hello = fopen("hello.txt", "r");
+  if(hello == NULL)
+    return -1;
 
-    while((c = fgetc(hello)) != '\n') {
-      putchar(c);
-    }
-    putchar('\n');
-    
-    fclose(hello);
-    return 0;
+  while((c = fgetc(hello)) != '\n') {
+    putchar(c);
   }
+  putchar('\n');
 
-  bsg_wait_while(1);
+  fclose(hello);
+  return 0;
 }
