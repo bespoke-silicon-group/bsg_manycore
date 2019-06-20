3694e9326fe5d60ecb6d96faf6b4146720eacee9
tommy <tommy.dc.jung@gmail.com>
2019-06-19 19:28:37 -0700

    debug if; upgrade tracer

--- testbenches/common/v/vanilla_core_trace.v
+++ testbenches/common/v/vanilla_core_trace.v
@@ -16,6 +16,7 @@ module vanilla_core_trace
     , localparam icache_addr_width_lp=`BSG_SAFE_CLOG2(icache_entries_p)
     , localparam dmem_addr_width_lp=`BSG_SAFE_CLOG2(dmem_size_p)
     , localparam pc_width_lp=(icache_tag_width_p+icache_addr_width_lp)
+    , localparam reg_addr_width_lp=5
     , localparam bsg_data_end_lp = `_bsg_data_end_addr
   )
   (
@@ -23,61 +24,109 @@ module vanilla_core_trace
     , input reset_i
 
     , input stall
-    , input exe_signals_s exe_r
-    , input mem_signals_s mem_r
-    , input wb_signals_s wb_r
+    , input stall_fp
+    , input stall_depend
+    , input stall_ifetch_wait
+    , input stall_icache_store
+    , input stall_lr_aq
+    , input stall_fence
+    , input stall_md
+    , input stall_force_wb
+    , input stall_remote_req
+    , input stall_local_flw
 
-    , input dmem_v_li
-    , input dmem_w_li
-    , input [dmem_addr_width_lp-1:0] dmem_addr_li
-    , input [data_width_p-1:0] dmem_data_li
-    , input [data_width_p-1:0] dmem_data_lo
+    , input flush
+    , input exe_signals_s exe_r
+
+    , input [pc_width_lp-1:0] pc_n
 
     , input int_rf_wen
-    , input [4:0] int_rf_waddr
+    , input [reg_addr_width_lp-1:0] int_rf_waddr
     , input [data_width_p-1:0] int_rf_wdata
 
+    , input lsu_dmem_v_lo
+    , input lsu_dmem_w_lo
+    , input [dmem_addr_width_lp-1:0] lsu_dmem_addr_lo
+    , input [data_width_p-1:0] lsu_dmem_data_lo
+    , input [data_width_p-1:0] local_load_packed_data
+  
     , input [x_cord_width_p-1:0] my_x_i
-    , input [y_cord_width_p-1:0] my_y_i 
+    , input [y_cord_width_p-1:0] my_y_i
   );
 
-  exe_signals_s exe_rr, exe_rrr;
-  mem_signals_s mem_rr;
-  logic [data_width_p-1:0] exe_pc_r;
-  logic [data_width_p-1:0] exe_pc_rr;
-  logic dmem_read_r;
-  logic [dmem_addr_width_lp-1:0] dmem_addr_r;
+
+  exe_debug_s exe_debug;
+  mem_debug_s mem_debug;
+  wb_debug_s wb_debug;
+
+  assign exe_debug.pc = exe_r.pc_plus4 - 'd4;
+  assign exe_debug.instr = exe_r.instruction;
+  assign exe_debug.branch_or_jump = exe_r.decode.is_branch_op
+    | exe_r.decode.is_jump_op;
+  assign exe_debug.btarget = {{(32-2-pc_width_lp){1'b0}}, pc_n, 2'b00};
+  assign exe_debug.is_local_load = lsu_dmem_v_lo & ~lsu_dmem_w_lo;
+  assign exe_debug.is_local_store = lsu_dmem_v_lo & lsu_dmem_w_lo;
+  assign exe_debug.local_dmem_addr = lsu_dmem_addr_lo;
+  assign exe_debug.local_store_data = lsu_dmem_data_lo;
 
   always_ff @ (posedge clk_i) begin
     if (reset_i) begin
-      exe_rr <= '0;
-      exe_rrr <= '0;
-      mem_rr <= '0;
-      exe_pc_r <= '0;
-      exe_pc_rr <= '0;
-      dmem_read_r <= '0;
-      dmem_addr_r <= '0;
+      mem_debug <= '0;
+      wb_debug <= '0;
     end
     else begin
       if (~stall) begin
-        exe_rr <= exe_r;
-        exe_rrr <= exe_rr;
-        mem_rr <= mem_r;
-        exe_pc_r <= (exe_r.pc_plus4 - 'd4) | 32'h80000000;
-        exe_pc_rr <= exe_pc_r;
-        dmem_read_r <= dmem_v_li & ~dmem_w_li;
-        dmem_addr_r <= dmem_addr_li;
+
+        mem_debug <= {
+          pc: exe_debug.pc,
+          instr: exe_debug.instr,
+          branch_or_jump: exe_debug.branch_or_jump,
+          btarget: exe_debug.btarget,
+          is_local_load: exe_debug.is_local_load,
+          is_local_store: exe_debug.is_local_store,
+          local_dmem_addr: exe_debug.local_dmem_addr,
+          local_store_data: exe_debug.local_store_data
+
+        };
+  
+        wb_debug <= {
+          pc: mem_debug.pc,
+          instr: mem_debug.instr,
+          branch_or_jump: mem_debug.branch_or_jump,
+          btarget: mem_debug.btarget,
+          is_local_load: mem_debug.is_local_load,
+          is_local_store: mem_debug.is_local_store,
+          local_dmem_addr: mem_debug.local_dmem_addr,
+          local_store_data: mem_debug.local_store_data,
+          local_load_data: local_load_packed_data
+        };
+
       end
     end
   end
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+  // TRACER LOGIC
+  //
   integer fd;
   string stamp;
   string pc_instr;
-  string rf_write;
-  string dmem_read;
-  string dmem_write;
+  string stall_reason;
+  string int_rf_write;
+  string btarget;
+  string dmem_access;
 
   initial begin
     fd = $fopen("vanilla.log", "w");
@@ -85,69 +134,96 @@ module vanilla_core_trace
     $fclose(fd);
 
     forever begin
+
       @(negedge clk_i) begin
         stamp = "";
         pc_instr = "";
-        rf_write = "";
-        dmem_read = "";
-        dmem_write = "";
+        stall_reason = "";
+        int_rf_write = "";
+        btarget = "";
+        dmem_access = "";
 
-        if (my_x_i == 1'b0 & my_y_i == 2'b01 & ~reset_i) begin //
+        if (~reset_i) begin
+        if ((my_x_i == 1) & (my_y_i == 1)) begin // comment this out for global logging
+          fd = $fopen("vanilla.log", "a");
 
-        fd = $fopen("vanilla.log", "a");
-   
-        // stamp
-        stamp = $sformatf("t=%08t x=%2d y=%2d: ", $time, my_x_i, my_y_i);
+          // STAMP
+          stamp = $sformatf("%08t %2d %2d:", $time, my_x_i, my_y_i);
+
+          // PC INSTR
+          pc_instr = wb_debug.pc == 32'hfffffffc
+            ? "BUBBLE           "
+            : $sformatf("%08x %08x", wb_debug.pc, wb_debug.instr);
+
+          // STALL REASON
+          if (stall_ifetch_wait)
+            stall_reason = "STALL: IFETCH";
+          else if (stall_icache_store)
+            stall_reason = "STALL: ISTORE";
+          else if (stall_lr_aq)
+            stall_reason = "STALL: LR_AQ ";
+          else if (stall_fence)
+            stall_reason = "STALL: FENCE ";
+          else if (stall_md)
+            stall_reason = "STALL: MULDIV";
+          else if (stall_force_wb)
+            stall_reason = "STALL: LOADWB";
+          else if (stall_remote_req)
+            stall_reason = "STALL: MEMREQ";
+          else if (stall_local_flw)
+            stall_reason = "STALL: FLW   ";
+          else
+            stall_reason = "             ";
+
+          // RF WRITE
+          int_rf_write = int_rf_wen
+            ? $sformatf("x%02d=%08x",int_rf_waddr, int_rf_wdata)
+            : {(3+1+8){" "}};
+
+          // BTARGET
+          btarget = wb_debug.branch_or_jump
+            ? $sformatf("bt=%08x", wb_debug.btarget)
+            : {(3+8){" "}};
+
+          // DMEM ACCESS
+          if (wb_debug.is_local_load)
+            dmem_access = $sformatf("LL[%3x]=%08x",
+              wb_debug.local_dmem_addr, wb_debug.local_load_data);
+          else if (wb_debug.is_local_store)
+            dmem_access = $sformatf("LS[%3x]=%08x",
+              wb_debug.local_dmem_addr, wb_debug.local_store_data);
+          else
+            dmem_access = {(2+5+1+8){" "}};
+
+          $fwrite(fd, "%s %s %s %s %s %s\n",
+            stamp,
+            pc_instr,
+            stall_reason,
+            int_rf_write,
+            btarget,
+            dmem_access
+          );
     
-        // pc_instr
-        pc_instr = $sformatf("pc=%08x instr=%08x", exe_pc_rr, exe_rrr.instruction);
- 
-        // rf_write
-        if (int_rf_wen)
-          rf_write = $sformatf("x%2d=%08x", int_rf_waddr, int_rf_wdata);
-        else
-          rf_write = {(4+8){" "}};
+          $fclose(fd);
 
-        // dmem_read
-        if (dmem_read_r)
-          dmem_read = $sformatf("M[%03x]==%08x", dmem_addr_r, dmem_data_lo);
-        else
-          dmem_read = {(8+8){" "}};
-        // dmem_write
-        if (dmem_v_li & dmem_w_li) 
-          dmem_write = $sformatf("M[%03x]:=%08x", dmem_addr_li, dmem_data_li);
-        else
-          dmem_write = {(8+8){" "}};
-
-        $fwrite(fd, "%s %s %s %s %s\n", 
-          stamp,
-          pc_instr,
-          rf_write,
-          dmem_read,
-          dmem_write
-        );
-
-        $fclose(fd);
-
-        end //
-
+        end // comment this out for global logging
+        end
       end
     end
 
-  end 
+
+  end
 
 
-  // synopsys translate_off
+
   
   // SP (x2) overflow checking.
+  //
   always_ff @ (negedge clk_i) begin
     if (int_rf_wen & (int_rf_waddr == 2) & int_rf_wdata < bsg_data_end_lp) begin
       $display("[ERROR][VCORE] SP underflow. t=%0t data_end=%x, sp=%x",
         $time, bsg_data_end_lp, int_rf_wdata);
     end
   end
- 
-
-  // synopsys translate_on
 
 endmodule
