76797272c162bb73cba5bce8d467fc0df6a362bf
tommy <tommy.dc.jung@gmail.com>
2019-06-23 17:01:43 -0700

    temp commit

--- testbenches/basic/test_bsg_manycore.v
+++ testbenches/basic/test_bsg_manycore.v
@@ -13,7 +13,7 @@
 `error bsg_global_Y must be defined; pass it in through the makefile;
 `endif
 
-`define MAX_CYCLES 100000000
+`define MAX_CYCLES 1000000
 
 module test_bsg_manycore;
   import bsg_noc_pkg::*; // {P=0, W, E, N, S}
