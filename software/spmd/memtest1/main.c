37483e9a5c84179beee5817b96d4f7de7c0c5acc
tommy <tommy.dc.jung@gmail.com>
2019-07-06 15:56:56 -0700

    memtest1

---
+++ software/spmd/memtest1/main.c
@@ -0,0 +1,52 @@
+#include "bsg_manycore.h"
+#include "bsg_set_tile_x_y.h"
+
+#define BLOCK_SIZE 8
+#define INDEX 16384
+
+int main()
+{
+  bsg_set_tile_x_y();
+
+  if (__bsg_x == 0 && __bsg_y == 0) 
+  {
+
+    for (int b = 0; b < BLOCK_SIZE; b++)
+    {
+      for (int i = 0; i < INDEX; i++)
+      {
+        int addr = (b<<2) + (i<<5);
+        bsg_dram_store(addr, addr);
+        int load_val = -1;
+        bsg_dram_load(addr, load_val);
+        if (load_val != addr)
+          bsg_fail();
+      }
+    }
+
+
+    /*
+    while (i < NUM_SEQ)
+    {
+      bsg_dram_store(prn, prn);
+      int load_val = -1;
+      bsg_dram_load(prn,load_val);
+
+      if (prn != load_val)
+        bsg_fail();
+      else
+        bsg_printf("SEQ[%d]=%d\n", i, prn);
+  
+      // calculate next 30-bit prn
+      // 
+      
+    }
+*/
+ 
+
+    bsg_finish();
+  }
+
+  bsg_wait_while(1);
+}
+
