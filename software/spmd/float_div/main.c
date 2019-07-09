7bb4d873cf0eab067fb1fa5445cf66f122aaa3f5
tommy <tommy.dc.jung@gmail.com>
2019-07-08 19:39:01 -0700

    division by values from local dmem and DRAM

--- software/spmd/float_div/main.c
+++ software/spmd/float_div/main.c
@@ -5,6 +5,11 @@
 #define flt(X) (*(float*)&X)
 #define hex(X) (*(int*)&X)
 
+float A[4];
+float B[4];
+
+float dataA[4] __attribute__ ((section (".dram"))) = { 1.0, 1.0, 1.0, 1.0};
+float dataB[4] __attribute__ ((section (".dram"))) = { 3.0, 5.0, 7.0, 11.0};
 
 int main()
 {
@@ -17,12 +22,31 @@ int main()
     float c = a / b;
 
     bsg_printf("%x\n", hex(c));
-    if (hex(c) == 0x3eaaaaab)
-      bsg_finish();
-    else
+    if (hex(c) != 0x3eaaaaab)
       bsg_fail();
     
 
+    for (int i = 0; i < 4; i++)
+    {
+      A[i] = 1.0;
+      B[i] = 3.0;
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+      float C = A[i] / B[i];
+      bsg_printf("%x\n", hex(C));
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+      float d = dataA[i] / dataB[i];
+      bsg_printf("%x\n", hex(d));
+      
+    }
+
+    
+    bsg_finish();
   }
 
   bsg_wait_while(1);
