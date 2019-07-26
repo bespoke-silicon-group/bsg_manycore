2744f80c7008bcce89059a2b63e1199b1efa60ac
tommy <tommy.dc.jung@gmail.com>
2019-07-26 15:17:40 -0700

    vcache profiler in csv format

--- testbenches/common/v/vcache_profiler.v
+++ testbenches/common/v/vcache_profiler.v
@@ -68,22 +68,45 @@ module vcache_profiler
   integer fd;
 
   initial begin
-    fd = $fopen(logfile_lp, "w");
-    $fwrite(fd, "");
-    $fclose(fd);
+
+    my_name = $sformatf("%m");
+    if (str_match(my_name, "vcache[0]")) begin
+      fd = $fopen(logfile_lp, "w");
+      $fwrite(fd, "instance,global_ctr,tag,ld,st,ld_miss,st_miss\n");
+      $fclose(fd);
+    end
 
     forever begin
       @(negedge clk_i) begin
         if (~reset_i & print_stat_v_i) begin
-          my_name = $sformatf("%m");
+
           $display("[BSG_INFO][VCACHE_PROFILER] %s t=%0t printing stats.", my_name, $time);
+
           fd = $fopen(logfile_lp, "a");
-          $fwrite(fd, "%s,global_ctr=%0d,tag=%0d,ld=%0d,st=%0d,ld_miss=%0d,st_miss=%0d\n",
+          $fwrite(fd, "%s,%0d,%0d,%0d,%0d,%0d,%0d\n",
             my_name, global_ctr_i, print_stat_tag_i, ld_count_r, st_count_r, ld_miss_count_r, st_miss_count_r);   
           $fclose(fd);
         end
       end
     end
   end
+
+
+  // string match helper
+  //
+  function str_match(string s1, s2);
+
+    int len1, len2;
+    len1 = s1.len();
+    len2 = s2.len();
+
+    if (len2 > len1)
+      return 0;
+
+    for (int i = 0; i < len1-len2+1; i++)
+      if (s1.substr(i,i+len2-1) == s2)
+        return 1;
+  
+  endfunction
 
 endmodule
