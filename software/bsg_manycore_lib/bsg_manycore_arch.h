676230a45388d8892db1a5e9b0cb61d687462d7c
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-07-26 15:00:19 -0700

    1. Integreated bsg_tile_group_remote_int/float_ptr macros into one macro that takes in the type as input in the bsg_manycore_lib/bsg_manycore_arch.h

--- software/bsg_manycore_lib/bsg_manycore_arch.h
+++ software/bsg_manycore_lib/bsg_manycore_arch.h
@@ -72,19 +72,15 @@
                                                              )                                               \
                                         )
 
-#define bsg_tile_group_remote_int_ptr(x,y,local_addr) ((bsg_remote_int_ptr) (   (REMOTE_EPA_PREFIX << REMOTE_EPA_MASK_SHIFTS) \
-                                                                              | ((y) << Y_CORD_SHIFTS )                     \
-                                                                              | ((x) << X_CORD_SHIFTS )                     \
-                                                                              | ((int) (local_addr)   )                     \
-                                                                            )                                               \
-                                                      )
+#define CREATE_POINTER_BY_TYPE(type) bsg_remote_ ## type ## _ptr
 
-#define bsg_tile_group_remote_float_ptr(x,y,local_addr) ((bsg_remote_float_ptr) (   (REMOTE_EPA_PREFIX << REMOTE_EPA_MASK_SHIFTS) \
-                                                                                  | ((y) << Y_CORD_SHIFTS )                     \
-                                                                                  | ((x) << X_CORD_SHIFTS )                     \
-                                                                                  | ((int) (local_addr)   )                     \
-                                                                                )                                               \
-                                                        )
+#define bsg_tile_group_remote_ptr(type,x,y,local_addr) ( (CREATE_POINTER_BY_TYPE(type)) (   (REMOTE_EPA_PREFIX << REMOTE_EPA_MASK_SHIFTS)  \
+                                                                                          | ((y) << Y_CORD_SHIFTS )                        \
+                                                                                          | ((x) << X_CORD_SHIFTS )                        \
+                                                                                          | ((int) (local_addr)   )                        \
+                                                                                        )                                                  \
+                                                       )
+
 
 
 //Used for global network access
@@ -108,11 +104,11 @@
                                         & (   (1 << bsg_remote_addr_bits) - 1 )         \
                                      )
 
-#define CREATE_COMMAND_BY_TYPE(type) bsg_tile_group_remote_ ## type ## _ptr
-
-#define bsg_tile_group_shared_ptr(type,lc_sh,index) ( CREATE_COMMAND_BY_TYPE(type)  ( ((index)%bsg_tiles_X),                                  \
-                                                                                    (((index)/bsg_tiles_X)%bsg_tiles_Y),                      \
-                                                                                    (&((lc_sh)[((index)/(bsg_tiles_X * bsg_tiles_Y))]))) )
+#define bsg_tile_group_shared_ptr(type,lc_sh,index) ( bsg_tile_group_remote_ptr  ( type,                                                   \
+                                                                                   ((index)%bsg_tiles_X),                                  \
+                                                                                   (((index)/bsg_tiles_X)%bsg_tiles_Y),                    \
+                                                                                   (&((lc_sh)[((index)/(bsg_tiles_X * bsg_tiles_Y))]))) )
+ 
                                                     
 #define bsg_io_mutex_ptr(local_addr)  bsg_global_ptr( IO_X_INDEX, IO_Y_INDEX, (local_addr))  
 #endif
