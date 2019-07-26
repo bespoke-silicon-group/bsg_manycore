a782cbbf711f46762328420c06d40caa5dd2b56b
tommy <tommy.dc.jung@gmail.com>
2019-07-26 12:12:04 -0700

    bsg_defines in bsg_mem_cfg_pkg; remove unnecessary if

--- testbenches/common/v/spmd_testbench.v
+++ testbenches/common/v/spmd_testbench.v
@@ -333,29 +333,25 @@ module spmd_testbench;
     assign trace_en = (trace_arg == 1);
   end
 
-  if (1) begin
+  bind vanilla_core vanilla_core_trace #(
+    .x_cord_width_p(x_cord_width_p)
+    ,.y_cord_width_p(y_cord_width_p)
+    ,.icache_tag_width_p(icache_tag_width_p)
+    ,.icache_entries_p(icache_entries_p)
+    ,.data_width_p(data_width_p)
+    ,.dmem_size_p(dmem_size_p)
+  ) vtrace (
+    .*
+    ,.trace_en_i($root.spmd_testbench.trace_en)
+  );
 
-    bind vanilla_core vanilla_core_trace #(
-      .x_cord_width_p(x_cord_width_p)
-      ,.y_cord_width_p(y_cord_width_p)
-      ,.icache_tag_width_p(icache_tag_width_p)
-      ,.icache_entries_p(icache_entries_p)
-      ,.data_width_p(data_width_p)
-      ,.dmem_size_p(dmem_size_p)
-    ) vtrace (
-      .*
-      ,.trace_en_i($root.spmd_testbench.trace_en)
-    );
-
-    bind vanilla_core instr_trace #(
-      .x_cord_width_p(x_cord_width_p)
-      ,.y_cord_width_p(y_cord_width_p)
-    ) itrace(
-      .*
-      ,.trace_en_i($root.spmd_testbench.trace_en)
-    );
-
-  end
+  bind vanilla_core instr_trace #(
+    .x_cord_width_p(x_cord_width_p)
+    ,.y_cord_width_p(y_cord_width_p)
+  ) itrace(
+    .*
+    ,.trace_en_i($root.spmd_testbench.trace_en)
+  );
 
   // profiler
   //
