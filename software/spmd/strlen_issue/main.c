6a00371944613059d6c235c6ceab560966ccaf2f
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-08-08 00:06:02 -0700

    Fixed strlen_issue test

--- software/spmd/strlen_issue/main.c
+++ software/spmd/strlen_issue/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdarg.h>
 #include <bsg_manycore.h>
 
-int varfunc(const char*, ...);
+int varfunc(const char*, ...) __attribute__((noinline));
 
-int main() {
+int main(int argc, char** argv) {
   char title[] = "\n\nVPR FPGA Placement and Routing Program Version 4.00-spec"
-                 "\nSource completed August 19, 1997.\n\n";
+                 "\nSource completed August 19, 1997.\n\n ";
+
   int len = varfunc("%s", title);
   bsg_print_int(len);
   return 0;
