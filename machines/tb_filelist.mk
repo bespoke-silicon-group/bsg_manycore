abbed944108ea987bc1b96ce3494ad5794437ab8
Dustin Richmond <drichmond@eng.ucsd.edu>
2019-08-30 13:20:22 -0700

    Revert "Standardized make variable names"
    This reverts commit 33c788c5263c95fd7e584535042965cefe7e365a.

--- machines/tb_filelist.mk
+++
@@ -1,5 +0,0 @@
-TB_VSOURCES+=$(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_nonsynth_manycore_io_complex.v
-TB_VSOURCES+=$(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_nonsynth_manycore_spmd_loader.v
-TB_VSOURCES+=$(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_nonsynth_manycore_monitor.v
-
-TB_VSOURCES+=$(BSG_MANYCORE_DIR)/testbenches/common/v/spmd_testbench.v
---
+++ machines/tb_srcs.mk
@@ -0,0 +1,5 @@
+TB_SRCS+=$(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_nonsynth_manycore_io_complex.v
+TB_SRCS+=$(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_nonsynth_manycore_spmd_loader.v
+TB_SRCS+=$(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_nonsynth_manycore_monitor.v
+
+TB_SRCS+=$(BSG_MANYCORE_DIR)/testbenches/common/v/spmd_testbench.v
