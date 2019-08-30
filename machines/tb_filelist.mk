33c788c5263c95fd7e584535042965cefe7e365a
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-08-29 17:36:33 -0700

    Standardized make variable names

---
+++ machines/tb_filelist.mk
@@ -0,0 +1,5 @@
+TB_VSOURCES+=$(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_nonsynth_manycore_io_complex.v
+TB_VSOURCES+=$(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_nonsynth_manycore_spmd_loader.v
+TB_VSOURCES+=$(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_nonsynth_manycore_monitor.v
+
+TB_VSOURCES+=$(BSG_MANYCORE_DIR)/testbenches/common/v/spmd_testbench.v
