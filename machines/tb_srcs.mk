1f653f7532fbdd19e054c0fc96c942dc97f2d7ac
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-08-29 17:36:33 -0700

    Standardized make variable names

--- machines/tb_srcs.mk
+++
@@ -1,5 +0,0 @@
-TB_SRCS+=$(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_nonsynth_manycore_io_complex.v
-TB_SRCS+=$(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_nonsynth_manycore_spmd_loader.v
-TB_SRCS+=$(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_nonsynth_manycore_monitor.v
-
-TB_SRCS+=$(BSG_MANYCORE_DIR)/testbenches/common/v/spmd_testbench.v
