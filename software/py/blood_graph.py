8b0e77536cba164d76950845b93883fab266f16a
tommy <tommy.dc.jung@gmail.com>
2019-09-23 14:16:05 -0700

    calculate tg-dim from vanilla.log

--- software/py/blood_graph.py
+++ software/py/blood_graph.py
@@ -9,16 +9,26 @@
 #   @author tommy
 #
 #   How to use:
-#   python blood_graph.py {x_min}-{x_max} {y_min}-{y_max} {start_time} {end_time} {timestep} {vanilla.log}
+#   python blood_graph.py {start_time} {end_time} {timestep} {vanilla.log}
 #
-#   ex) python blood_graph.py 0-1 1-2 6000000 15000000 20 vanilla.log
+#   ex) python blood_graph.py 6000000 15000000 20 vanilla.log
 #
-#   {x_min/max}   x-cord range
-#   {y_min/max}   y-cord range
 #   {start_time}  start_time in picosecond
 #   {end_time}    end_time in picosecond
 #   {timestep}    time step in picosecond
-#   
+# 
+#   Color Code:  
+#   executed  = ffffff (white)
+#   bubble    = ff1493 (pink)
+#   ifetch    = ff0000 (red)
+#   lr_aq     = ff4500 (orange)
+#   istore    = ffff00 (yellow)
+#   fence     = a52a2a (brown)
+#   muldiv    = 800080 (purple)
+#   loadwb    = dc143c (crimson)
+#   memreq    = 2f4f4f (grey)
+#   flw       = 8b0000 (dark red)
+#
 
 
 import sys
@@ -29,29 +39,11 @@ from vanilla_trace_parser import *
 class BloodGraph:
 
   # default constructor
-  def __init__(self, xmin, xmax, ymin, ymax, start_time, end_time, timestep):
-
-    if xmin > xmax:
-      sys.exit("xmin cannot be greater than xmax.")
-    if ymin > ymax:
-      sys.exit("ymin cannot be greater than ymax.")
-  
-    self.xmin = xmin
-    self.xmax = xmax
-    self.ymin = ymin
-    self.ymax = ymax
-    
-    self.xdim = xmax-xmin+1
-    self.ydim = ymax-ymin+1
+  def __init__(self, start_time, end_time, timestep):
 
     self.start_time = start_time
     self.end_time = end_time
     self.timestep = timestep
-
-    self.img_width = 1024   # default
-    self.img_height = ((((end_time-start_time)//timestep)+self.img_width)//self.img_width)*(2+(self.xdim*self.ydim))
-    self.img = Image.new("RGB", (self.img_width, self.img_height), "black")
-    self.pixel = self.img.load()
 
     self.parser = VanillaTraceParser()
 
@@ -61,13 +53,44 @@ class BloodGraph:
     # parse vanilla.log
     traces = self.parser.parse(input_file)
 
+    # get tile-group dim
+    self.get_tg_dim(traces)
+
+    # init image
+    self.init_image()
+
     # create image
     for trace in traces:
       self.mark_trace(trace)
 
     #self.img.show()
     self.img.save("blood.bmp")
-      
+
+
+  # private function
+  # look through the input file to get the tile group dimension (x,y)
+  def get_tg_dim(self, traces):
+    xs = list(map(lambda t: t["x"], traces))
+    ys = list(map(lambda t: t["y"], traces))
+    self.xmin = min(xs)
+    self.xmax = max(xs)
+    self.ymin = min(ys)
+    self.ymax = max(ys)
+    
+    self.xdim = self.xmax-self.xmin+1
+    self.ydim = self.ymax-self.ymin+1
+
+
+  # private function
+  # initialize image
+  def init_image(self):
+    self.img_width = 1024   # default
+    self.img_height = ((((end_time-start_time)//timestep)+self.img_width)//self.img_width)*(2+(self.xdim*self.ydim))
+    self.img = Image.new("RGB", (self.img_width, self.img_height), "black")
+    self.pixel = self.img.load()
+  
+  
+  # private function
   # mark the trace on output image
   def mark_trace(self, trace):
 
@@ -84,16 +107,6 @@ class BloodGraph:
     row = floor*(2+(self.xdim*self.ydim)) + (tg_x+(tg_y*self.xdim))
 
     # determine color
-    # executed  = ffffff (white)
-    # bubble    = ff1493 (pink)
-    # ifetch    = ff0000 (red)
-    # lr_aq     = ff4500 (orange)
-    # istore    = ffff00 (yellow)
-    # fence     = a52a2a (brown)
-    # muldiv    = 800080 (purple)
-    # loadwb    = dc143c (crimson)
-    # memreq    = 2f4f4f (grey)
-    # flw       = 8b0000 (dark red)
     if "int_pc" not in trace:
       self.pixel[col,row] = (0xff, 0x14, 0x93)
     else:
@@ -121,24 +134,16 @@ class BloodGraph:
 # main()
 if __name__ == "__main__":
 
-  if len(sys.argv) != 7:
-    print("wrong argument.")
+  if len(sys.argv) != 5:
+    print("wrong number of arguments.")
+    print("python {start_time} {end_time} {timestep} vanilla.log")
     sys.exit()
  
-  x_range = sys.argv[1]
-  y_range = sys.argv[2]
-  start_time = int(sys.argv[3])
-  end_time = int(sys.argv[4])
-  timestep = int(sys.argv[5])
-  input_file = sys.argv[6]
+  start_time = int(sys.argv[1])
+  end_time = int(sys.argv[2])
+  timestep = int(sys.argv[3])
+  input_file = sys.argv[4]
 
-  xcord = x_range.split("-")
-  ycord = y_range.split("-")
-  
-  xmin = int(xcord[0])
-  xmax = int(xcord[1])
-  ymin = int(ycord[0])
-  ymax = int(ycord[1])
 
-  bg = BloodGraph(xmin,xmax,ymin,ymax,start_time,end_time,timestep)
+  bg = BloodGraph(start_time,end_time,timestep)
   bg.generate(input_file)
