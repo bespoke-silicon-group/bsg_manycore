91577f73dce3cffd02fcbd37000a12e5353933b5
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-10-20 22:06:29 -0700

    1. Added a cuda lite runtime memcpy kernel so user can call a grid of tile groups to perform memcpy on DRAM instead of waiting for the host to slowly do it through PCI-E.

--- software/spmd/bsg_cuda_lite_runtime/memcpy/kernel_memcpy.c
+++ software/spmd/bsg_cuda_lite_runtime/memcpy/kernel_memcpy.c
@@ -1,5 +1,5 @@
-// This kernel performs a memset on an array in DRAM 
-// With the given size (in words) and value
+// This kernel performs a memcpy from arrya src to 
+// array dst in DRAM with the given size (in words)
 
 #include "bsg_manycore.h"
 #include "bsg_set_tile_x_y.h"
@@ -11,7 +11,7 @@ INIT_TILE_GROUP_BARRIER(r_barrier, c_barrier, 0, bsg_tiles_X-1, 0, bsg_tiles_Y-1
 
 
 
-int  __attribute__ ((noinline)) kernel_memset(int *dst, int val, int size) {
+int  __attribute__ ((noinline)) kernel_memcpy(int *dst, int *src, int size) {
 
 	int __bsg_tile_group_id = (__bsg_tile_group_id_y * __bsg_grid_dim_x + __bsg_tile_group_id_x);
 	int __bsg_tile_group_dim = (bsg_tiles_X * bsg_tiles_Y);
@@ -21,7 +21,7 @@ int  __attribute__ ((noinline)) kernel_memset(int *dst, int val, int size) {
 	int end_x = start_x + block_size_x;
 
 	for (int iter_x = start_x + __bsg_id; iter_x < end_x; iter_x += __bsg_tile_group_dim) {
-		dst[iter_x] = val;
+		dst[iter_x] = src[iter_x];
 	}
 
 	bsg_tile_group_barrier(&r_barrier, &c_barrier); 
