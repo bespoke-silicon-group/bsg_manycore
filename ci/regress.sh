0e30dd7a3ce844d334237d32c54582e4835ee4ef
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-10-31 18:17:50 -0700

    Cosim in CI

--- ci/regress.sh
+++ ci/regress.sh
@@ -3,8 +3,19 @@
 NUM_CORES=${CI_NUM_CORES:-16}
 
 cd software/spmd
-make -j $NUM_CORES recurse-clean 
-make -j $NUM_CORES recurse-all
+make -j $NUM_CORES recurse-clean > /dev/null 2>&1
+make -j $NUM_CORES recurse-all > /dev/null 2>&1
+echo ""
+echo "=========================================="
+echo "Summary of error/fail messages"
+echo "=========================================="
+make summarize-bad
+echo ""
+echo "=========================================="
+echo "Regression summary"
+echo "=========================================="
+make BSG_FINISH.scrape BSG_FINISH.scrape.i BSG_FAIL.scrape BSG_TIMEOUT.scrape BSG_ERROR.scrape
+echo ""
 for file in recurse-results/*.log; do
   if grep --quiet BSG_FAIL $file; then
     echo $file failed!
