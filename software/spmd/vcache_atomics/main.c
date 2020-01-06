40d11a1d2f958757bb256a5165a84de5894dda22
Tommy Jung <tommy.dc.jung@gmail.com>
2020-01-06 11:10:08 -0800

    check __bsg_id

--- software/spmd/vcache_atomics/main.c
+++ software/spmd/vcache_atomics/main.c
@@ -10,48 +10,47 @@ int main()
 
   bsg_set_tile_x_y();
 
-  lock = 123;
+  if (__bsg_id == 0) 
+  {
+    lock = 123;
 
-  int result = bsg_amoswap(&lock, 1); 
-  if (result != 123) bsg_fail();
-  bsg_printf("%d\n", result);
+    int result = bsg_amoswap(&lock, 1); 
+    if (result != 123) bsg_fail();
+    bsg_printf("%d\n", result);
 
-  result = bsg_amoswap_aq(&lock, 2);
-  if (result != 1) bsg_fail();
-  bsg_printf("%d\n", result);
+    result = bsg_amoswap_aq(&lock, 2);
+    if (result != 1) bsg_fail();
+    bsg_printf("%d\n", result);
 
-  result = bsg_amoswap_rl(&lock, 3);
-  if (result != 2) bsg_fail();
-  bsg_printf("%d\n", result);
+    result = bsg_amoswap_rl(&lock, 3);
+    if (result != 2) bsg_fail();
+    bsg_printf("%d\n", result);
 
-  result = bsg_amoswap_aqrl(&lock, 4);
-  if (result != 3) bsg_fail();
-  bsg_printf("%d\n", result);
-
-
-  lock2= 1;
-  int result2 = bsg_amoor(&lock2,2);
-  if (result2 != 1) bsg_fail();
-  bsg_printf("%d\n", result2);
-
-  result2 = bsg_amoor_aq(&lock2,4);
-  if (result2 != 3) bsg_fail();
-  bsg_printf("%d\n", result2);
-
-  result2 = bsg_amoor_rl(&lock2,8);
-  if (result2 != 7) bsg_fail();
-  bsg_printf("%d\n", result2);
-
-  result2 = bsg_amoor_aqrl(&lock2,16);
-  if (result2 != 15) bsg_fail();
-  bsg_printf("%d\n", result2);
+    result = bsg_amoswap_aqrl(&lock, 4);
+    if (result != 3) bsg_fail();
+    bsg_printf("%d\n", result);
 
 
+    lock2= 1;
+    int result2 = bsg_amoor(&lock2,2);
+    if (result2 != 1) bsg_fail();
+    bsg_printf("%d\n", result2);
 
+    result2 = bsg_amoor_aq(&lock2,4);
+    if (result2 != 3) bsg_fail();
+    bsg_printf("%d\n", result2);
 
+    result2 = bsg_amoor_rl(&lock2,8);
+    if (result2 != 7) bsg_fail();
+    bsg_printf("%d\n", result2);
 
+    result2 = bsg_amoor_aqrl(&lock2,16);
+    if (result2 != 15) bsg_fail();
+    bsg_printf("%d\n", result2);
 
-  bsg_finish();
+    bsg_finish();
+  }
+
   bsg_wait_while(1);
 }
 
