bf141bb04796d5b3286cf9a9b1b7bde9fc1a87f1
Dustin Richmond <dustinar@uw.edu>
2020-01-16 13:53:35 -0800

    Added comments

--- software/py/vanilla_stats_parser.py
+++ software/py/vanilla_stats_parser.py
@@ -23,7 +23,6 @@
 #   {input}           Vanilla stats input file     default = vanilla_stats.log
 
 
-
 import sys
 import argparse
 import os
@@ -33,13 +32,7 @@ import numpy as np
 from enum import Enum
 from collections import Counter
 
-
-
-BSG_PRINT_STAT_KERNEL_TAG = 0x0
-
-
-
-# CudaStatTag class
+# CudaStatTag class 
 # Is instantiated by a packet tag value that is recieved from a 
 # bsg_cuda_print_stat(tag) insruction
 # Breaks down the tag into (type, y, x, tg_id, tag>
@@ -49,14 +42,26 @@ BSG_PRINT_STAT_KERNEL_TAG = 0x0
 # Formatting for bsg_cuda_print_stat instructions
 # Section                 stat type  -   y cord   -   x cord   -    tile group id   -        tag
 # of bits                <----2----> -   <--6-->  -   <--6-->  -   <------14----->  -   <-----4----->
-# Stat type value: {"stat":0, "start":1, "end":2}
+# Stat type value: {"Kernel Start":0, "Kernel End": 1, "Tag Start":2, "Tag End":3}
+
+# The CudaStatTag class encapsulates the tag argument used by bsg_cuda_print_stat_*
+# commands inside of bsg_manycore/software/bsg_manycore_lib/bsg_manycore.h.
+# There are four commands:
+
+#  bsg_cuda_print_stat_kernel_start() - Annotates the start of the kernel being profiled
+#  bsg_cuda_print_stat_kernel_end()   - Annotates the end of the kernel being profiled
+#  bsg_cuda_print_stat_start(tag)     - Annotates the start of a tagged section of the kernel being profiled
+#  bsg_cuda_print_stat_end(tag)       - Annotates the end of a tagged section of the kernel being profiled
+
+# Calls to bsg_cuda_print_stat_start(tag) and bsg_cuda_print_stat_kernel_start()
+# must be called first be paired with a matching call to
+# bsg_cuda_print_stat_end(tag) and bsg_cuda_print_stat_kernel_end().
 class CudaStatTag:
     # These values are used by the manycore library in bsg_print_stat instructions
     # they are added to the tag value to determine the tile group that triggered the stat
     # and also the type of stat (stand-alone stat, start, or end)
     # the value of these paramters should match their counterpart inside 
     # bsg_manycore/software/bsg_manycore_lib/bsg_manycore.h
-    # For formatting, see the CudaStatTag class
     _TAG_WIDTH   = 4
     _TAG_INDEX   = 0
     _TAG_MASK   = ((1 << _TAG_WIDTH) - 1)
@@ -80,54 +85,62 @@ class CudaStatTag:
         KERNEL_END     = 3
 
     def __init__(self, tag):
+        """ Initialize a CudaStatTag object """
         self.__s = tag;
         self.__type = self.StatType((self.__s >> self._TYPE_INDEX) & self._TYPE_MASK)
 
-    @property 
+    @property
     def tag(self):
+        """ Get the tag associated with this object """
         return ((self.__s >> self._TAG_INDEX) & self._TAG_MASK)
 
     @property 
     def tg_id(self):
+        """ Get the Tile-Group IP associated with this object """
         return ((self.__s >> self._TG_ID_INDEX) & self._TG_ID_MASK)
 
     @property 
     def x(self):
+        """ Get the X Coordinate associated with this object """
         return ((self.__s >> self._X_INDEX) & self._X_MASK)
 
     @property 
     def y(self):
+        """ Get the Y Coordinate associated with this object """
         return ((self.__s >> self._Y_INDEX) & self._Y_MASK)
 
     @property 
     def statType(self):
+        """ Get the StatType that this object defines"""
         return self.__type
 
     @property 
     def isStart(self):
+        """ Return true if this object corresponds to a call to
+        bsg_cuda_print_stat_start """
         return (self.__type == self.StatType.START)
 
     @property 
     def isEnd(self):
+        """ Return true if this object corresponds to a call to
+        bsg_cuda_print_stat_end """
         return (self.__type == self.StatType.END)
 
     @property 
     def isKernelStart(self):
+        """ Return true if this object corresponds to a call to
+        bsg_cuda_print_stat_kernel_start """
         return (self.__type == self.StatType.KERNEL_START)
 
     @property 
     def isKernelEnd(self):
+        """ Return true if this object corresponds to a call to
+        bsg_cuda_print_stat_kernel_end """
         return (self.__type == self.StatType.KERNEL_END)
-
-    @property 
-    def isStat(self):
-        return (self.__type == self.StatType.STAT)
-
-
 
  
 class VanillaStatsParser:
-    # Default coordinates of origin tile
+    # Default coordinates of origin tile in the manycore array.
     _BSG_ORIGIN_X = 0
     _BSG_ORIGIN_Y = 1
 
