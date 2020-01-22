2764b148285adff697b3dfadf179672491d2c4c1
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-01-22 15:54:51 -0800

    Tensor add kernel

--- software/torch/add/add.c
+++ software/torch/add/add.c
@@ -1,9 +1,20 @@
 #include "bsg_manycore.h"
 #include "bsg_set_tile_x_y.h"
+#include <stdint.h>
 
-int __attribute__ ((noinline)) add(int* a, int* b, int* c) {
+typedef struct {
+  uint32_t N;
+  uint32_t* strides;
+  float* data;
+} hb_mc_tensor_t;
+
+int __attribute__ ((noinline)) add(
+    hb_mc_tensor_t* res, hb_mc_tensor_t* a, hb_mc_tensor_t* b,
+    float* alpha) {
   if(__bsg_id == 0) {
-    *c = *a +* b;
+    for(uint32_t i=0; i < res->N; ++i)
+      res->data[i] = a->data[i] + (*alpha) * b->data[i];
+
     return 0;
   } else {
     bsg_wait_while(1);
