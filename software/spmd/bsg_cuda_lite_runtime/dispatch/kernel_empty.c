b0af06aefcdbced99b80bc33f79cdd64a7cdc0ef
Max Ruttenberg <mrutt@cs.washington.edu>
2020-02-21 18:37:08 -0800

    kernel does initialization in kernel_dispatch

--- software/spmd/bsg_cuda_lite_runtime/dispatch/kernel_empty.c
+++ software/spmd/bsg_cuda_lite_runtime/dispatch/kernel_empty.c
@@ -1,7 +1,8 @@
-//This is an empty kernel 
+//This is an empty kernel
 
 #include "bsg_manycore.h"
 #include "bsg_set_tile_x_y.h"
+#include "bsg_cuda_lite_runtime.h"
 
 #define BSG_TILE_GROUP_X_DIM bsg_tiles_X
 #define BSG_TILE_GROUP_Y_DIM bsg_tiles_Y
@@ -11,8 +12,11 @@
 INIT_TILE_GROUP_BARRIER(r_barrier, c_barrier, 0, bsg_tiles_X - 1, 0, bsg_tiles_Y - 1);
 
 int kernel_dispatch() {
+
     volatile unsigned long *ptr = (volatile unsigned long*) bsg_remote_ptr_io(IO_X_INDEX, 0xFFF0);
     *ptr = 1;
+
+    if (__bsg_id == 0) cuda_tile_group_origin_task();
 
     bsg_tile_group_barrier(&r_barrier, &c_barrier);
 
