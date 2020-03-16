967f34247e4e58d9a22c18204cf2ac8721063c9e
Dustin Richmond <dustinar@uw.edu>
2020-03-16 12:24:30 -0700

    Split bsg_set_tile_x_y into separate files

--- software/bsg_manycore_lib/bsg_set_tile_x_y.c
+++ software/bsg_manycore_lib/bsg_set_tile_x_y.c
@@ -1,20 +1,6 @@
 
 #include "bsg_manycore.h"
-
-// these are private variables
-// we do not make them volatile
-// so that they may be cached
-
-int __bsg_x = -1;
-int __bsg_y = -1;
-int __bsg_id = -1;
-int __bsg_grp_org_x = -1;
-int __bsg_grp_org_y = -1;
-int __bsg_grid_dim_x = -1;
-int __bsg_grid_dim_y = -1;
-int __bsg_tile_group_id_x = -1;
-int __bsg_tile_group_id_y = -1;
-int __bsg_tile_group_id = -1;
+#include "bsg_set_tile_x_y.h"
 
 void bsg_set_tile_x_y()
 {
---
+++ software/bsg_manycore_lib/bsg_tile_config_vars.c
@@ -0,0 +1,14 @@
+// these are private variables
+// we do not make them volatile
+// so that they may be cached
+
+int __bsg_x = -1;
+int __bsg_y = -1;
+int __bsg_id = -1;
+int __bsg_grp_org_x = -1;
+int __bsg_grp_org_y = -1;
+int __bsg_grid_dim_x = -1;
+int __bsg_grid_dim_y = -1;
+int __bsg_tile_group_id_x = -1;
+int __bsg_tile_group_id_y = -1;
+int __bsg_tile_group_id = -1;
