0181c7d16026442db7a7ad45334c679081cc83a0
Dustin Richmond <dustinar@uw.edu>
2020-03-16 15:03:23 -0700

    Addressed Bandhav's feedback
    * Added attributes to locate variables in dmem
    * Added comments

--- software/bsg_manycore_lib/bsg_tile_config_vars.h
+++ software/bsg_manycore_lib/bsg_tile_config_vars.h
@@ -1,3 +1,21 @@
+// bsg_tile_config_vars.h defines per-tile variables that are used
+// when launching execution on a tile. Some variables are only
+// relevant to CUDA-Lite programs. The actual definitions are in
+// bsg_tile_config_vars.c
+
+// __bsg_x: X coordinate (relative to the group's origin tile)
+// __bsg_y: Y coordinate (relative to the group's origin tile)
+// __bsg_id: Unique ID for each tile in a group
+//           (__bsg_id = __bsg_y * __bsg_tile_group_dim_x + __bsg_x)
+// __bsg_grp_org_x: Global X coordinate of the group origin tile
+// __bsg_grp_org_y: Global Y coordinate of the group origin tile
+// __bsg_grid_dim_x: Global Grid X-Dimension
+// __bsg_grid_dim_y: Global Grid Y-Dimension
+// __bsg_tile_group_id_x: Tile-Group X ID (X-coordinate of current grid iteration)
+// __bsg_tile_group_id_y: Tile-Group Y ID (X-coordinate of current grid iteration)
+// __bsg_tile_group_id: Unique ID for each tile group
+//          (__bsg_tile_group_id = __bsg_tile_group_id_y * __bsg_grid_dim_x + __bsg_tile_group_id_x)
+
 #ifndef __BSG_TILE_CONFIG_VARS_H
 #define __BSG_TILE_CONFIG_VARS_H
 
