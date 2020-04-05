790f6d5ea2139570b595e258aa7dd85cdcadcdae
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-04-05 03:40:33 -0700

    Seperated text and data in dram

--- software/py/bsg_manycore_link_gen.py
+++ software/py/bsg_manycore_link_gen.py
@@ -135,16 +135,19 @@ class bsg_manycore_link_gen:
     # LMA (Load Memory Address)    => NPA used by loader
     # VMA (Virtual Memory Address) => Logical address used by linker for
     #                                 symbol resolutions
-    _DMEM_VMA_START = 0x1000
-    _DMEM_VMA_SIZE  = 0x1000
-    _DRAM_LMA_START  = 0x80000000
-    _DRAM_LMA_SIZE   = self._dram_size
+    _DMEM_VMA_START   = 0x1000
+    _DMEM_VMA_SIZE    = 0x1000
+    _DRAM_T_LMA_START = 0x80000000
+    _DRAM_T_LMA_SIZE  = self._imem_size
+    _DRAM_D_LMA_START = 0x80000000 + self._imem_size
+    _DRAM_D_LMA_SIZE  = self._dram_size - self._imem_size
 
     mem_regions = [
       # Format:
       # [<mem region name>, <access>, <start address>, <size>]
       ['DMEM_VMA', 'rw', _DMEM_VMA_START, _DMEM_VMA_SIZE],
-      ['DRAM_LMA', 'rw', _DRAM_LMA_START, _DRAM_LMA_SIZE],
+      ['DRAM_T_LMA', 'rx', _DRAM_T_LMA_START, _DRAM_T_LMA_SIZE],
+      ['DRAM_D_LMA', 'rw', _DRAM_D_LMA_START, _DRAM_D_LMA_SIZE],
       ]
 
     section_map = [
@@ -221,11 +224,13 @@ class bsg_manycore_link_gen:
 
       if self._default_data_loc == 'dram' or re.search(".dram$", sec) != None:
         boundary = None
+        lma_region = 'DRAM_D_LMA'
 
         if sec == '.text.dram':
             boundary = self._imem_size
+            lma_region = 'DRAM_T_LMA'
 
-        sections += self._section(sec, vaddr, None, 'DRAM_LMA',
+        sections += self._section(sec, vaddr, None, lma_region,
             in_sections, in_objects, boundary)
 
       if sec == '.text.dram':
