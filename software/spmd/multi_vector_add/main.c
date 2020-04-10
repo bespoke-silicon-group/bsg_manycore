a1435a91895910d05e70b51b84234433cbbd8987
Tommy Jung <tommy.dc.jung@gmail.com>
2020-04-09 19:28:02 -0700

    reduce number of vectors

--- software/spmd/multi_vector_add/main.c
+++ software/spmd/multi_vector_add/main.c
@@ -7,10 +7,6 @@ int data1[N] __attribute__ ((section (".dram"))) = {0};
 int data2[N] __attribute__ ((section (".dram"))) = {0};
 int data3[N] __attribute__ ((section (".dram"))) = {0};
 int data4[N] __attribute__ ((section (".dram"))) = {0};
-int data5[N] __attribute__ ((section (".dram"))) = {0};
-int data6[N] __attribute__ ((section (".dram"))) = {0};
-int data7[N] __attribute__ ((section (".dram"))) = {0};
-int data8[N] __attribute__ ((section (".dram"))) = {0};
 int data9[N] __attribute__ ((section (".dram"))) = {0};
 
 int main()
@@ -23,10 +19,6 @@ int main()
     data2[i] = i+3;
     data3[i] = i+5;
     data4[i] = i+7;
-    data5[i] = i+11;
-    data6[i] = i+13;
-    data7[i] = i+17;
-    data8[i] = i+19;
   }
   
   for (int i = 0; i < N; i++)
@@ -35,16 +27,12 @@ int main()
     int a2 = data2[i];
     int a3 = data3[i];
     int a4 = data4[i];
-    int a5 = data5[i];
-    int a6 = data6[i];
-    int a7 = data7[i];
-    int a8 = data8[i];
-    data9[i] = a1+a2+a3+a4+a5+a6+a7+a8;
+    data9[i] = a1+a2+a3+a4;
   }
 
   for (int i = 0; i < N; i++)
   {
-    if (data9[i] != (i<<3)+77) bsg_fail();
+    if (data9[i] != (i<<2)+17) bsg_fail();
   }
 
 
