4eebdf84d2e7300da6f878448146d26161955e33
mrutt92 <mchezrutt@gmail.com>
2020-04-10 16:35:47 -0700

    Vanilla bean hash function fix (#277)
    * cuda kernel for exposing processor eva translation bug from host
    
    * fixes bug in vanilla bean hash function

---
+++ software/spmd/bsg_cuda_lite_runtime/eva_range/kernel_eva_range.cpp
@@ -0,0 +1,21 @@
+/////////////////////////////////////////////////////////////////////////
+// This kernel copies a single word from one memory address to another //
+/////////////////////////////////////////////////////////////////////////
+
+#include "bsg_manycore.h"
+#include "bsg_set_tile_x_y.h"
+
+#define bsg_n (bsg_tiles_X * bsg_tiles_Y)
+
+extern "C"
+int kernel_eva_range(
+    // in
+    const unsigned * ptr,
+    // out
+    unsigned * value_read_ptr
+    ) {
+    bsg_print_hexadecimal(reinterpret_cast<unsigned>(ptr));
+    bsg_print_hexadecimal(reinterpret_cast<unsigned>(value_read_ptr));
+    *value_read_ptr = *ptr;
+    return 0;
+}
