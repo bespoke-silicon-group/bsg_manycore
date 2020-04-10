d73783966397833e066ea19d8d59645dae96936c
Max Ruttenberg <mrutt@cs.washington.edu>
2020-04-10 15:51:07 -0700

    size of infinite memories scale down inversely to columns

--- testbenches/common/v/bsg_nonsynth_mem_infinite.v
+++ testbenches/common/v/bsg_nonsynth_mem_infinite.v
@@ -13,6 +13,7 @@ module bsg_nonsynth_mem_infinite
     , parameter x_cord_width_p="inv"
     , parameter y_cord_width_p="inv"
     , parameter id_p="inv"
+    , parameter infmem_addr_width_p=(addr_width_p-x_cord_width_p-1)
     , parameter data_mask_width_lp=(data_width_p>>3)
     , parameter link_sif_width_lp=`bsg_manycore_link_sif_width(addr_width_p,data_width_p,x_cord_width_p,y_cord_width_p)
   )
@@ -82,7 +83,7 @@ module bsg_nonsynth_mem_infinite
 
   bsg_nonsynth_mem_1rw_sync_mask_write_byte_dma #(
     .width_p(data_width_p)
-    ,.els_p(1<<addr_width_p)
+    ,.els_p(1<<infmem_addr_width_p)
     ,.id_p(id_p)
   ) assoc_mem (
     .clk_i(clk_i)
