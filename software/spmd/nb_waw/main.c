15751607cf74c74e524568ff4c747bc28021f6a2
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-04-12 00:32:47 -0700

    WAW bug reproduced

--- software/spmd/nb_waw/main.c
+++ software/spmd/nb_waw/main.c
@@ -1,20 +1,46 @@
 #include "bsg_manycore.h"
 #include "bsg_set_tile_x_y.h"
 
-volatile int data[4] __attribute__((section(".dram"))) = {1, 2, 3, 0};
+volatile int load_buf[4] __attribute__((section(".dram"))) = {1, 2, 3, 0};
+volatile int store_buf[20];
+volatile int* long_remote = bsg_remote_ptr(4, 4, 0x1200);
 
 int main()
 {
   bsg_set_tile_x_y();
 
   if (__bsg_id == 0) {
-    int last = data[3];
-    bsg_putchar('0');
+    // Assume these loads happned way before,
+    // in the program. Blocking them by putchar to
+    // emulate such a scenario.
+    int third = load_buf[2];
+    int last = load_buf[3];
+    *long_remote = 0;
+    bsg_putchar('0' + third + last + *long_remote);
 
-    int res = data[0];
 
+    // The bug arises in a scenario where a remote load to 
+    // a reg is followed by a remote load to same reg in
+    // possibly differen piece of code.
+    int res = load_buf[0];
+    int prod = 0;
+
+    // This models the delay of transistion to different
+    // section of the program.
+    #pragma GCC unroll 5
+    for(int i=0; i<5; ++i)
+      store_buf[i] = 0;
+
+    // Long remote to fill up the buffer causing force write back
+    int sec = *long_remote;
+
+    // This `if` models some code in totally different section
+    // of the program.
     if(last == 0)
-      res = data[3];
+      res = load_buf[3];
+      prod = third * last;
+
+    res += sec + prod;
 
     if (res != 0)
       bsg_fail();
