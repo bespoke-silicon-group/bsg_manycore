896ba392c71ea2b80499915069cd5537d187ecd3
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-05-11 00:58:02 -0700

    Remove some options temporariliy

--- software/py/vanilla_parser/__main__.py
+++ software/py/vanilla_parser/__main__.py
@@ -7,13 +7,5 @@ parser.add_argument("--stats", default="vanilla_stats.csv", type=str,
                     help="Vanilla stats log file")
 parser.add_argument("--log", default="vanilla.log", type=str,
                     help="Vanilla log file")
-parser.add_argument("--cycle", default="@", type=str,
-                    help="Cycle window of bloodgraph as start_cycle@end_cycle.")
-parser.add_argument("--abstract", default=False, action='store_true',
-                    help="Type of bloodgraph - abstract / detailed")
-parser.add_argument("--generate-key", default=False, action='store_true',
-                    help="Generate a key image with blood graph")
-parser.add_argument("--no-blood-graph", default=False, action='store_true',
-                    help="Skip blood graph generation")
 
 args = parser.parse_args()
