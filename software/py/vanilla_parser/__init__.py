9a8f406efb7ae9da74683550961fdce9838bf01c
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-05-12 20:24:13 -0700

    Moved function defs to __init__

--- software/py/vanilla_parser/__init__.py
+++ software/py/vanilla_parser/__init__.py
@@ -1,5 +1,20 @@
+import argparse
 import blood_graph
 import stats_parser
 import pc_histogram
 import trace_parser
 import vcache_stall_graph
+
+def parse_args(parser):
+    parser.add_argument("--trace", default="vanilla_operation_trace.csv", type=str,
+                        help="Vanilla operation log file")
+    parser.add_argument("--stats", default="vanilla_stats.csv", type=str,
+                        help="Vanilla stats log file")
+    parser.add_argument("--log", default="vanilla.log", type=str,
+                        help="Vanilla log file")
+
+def main():
+    parser = argparse.ArgumentParser(description="Argument parser for vanilla_parser"
+                                     prog="vanilla_parser")
+    parse_args(parser)
+    args = parser.parse_args()
--- software/py/vanilla_parser/__main__.py
+++ software/py/vanilla_parser/__main__.py
@@ -1,11 +1,3 @@
 from __init__ import *
 
-parser = argparse.ArgumentParser(description="Argument parser for vanilla_parser")
-parser.add_argument("--trace", default="vanilla_operation_trace.csv", type=str,
-                    help="Vanilla operation log file")
-parser.add_argument("--stats", default="vanilla_stats.csv", type=str,
-                    help="Vanilla stats log file")
-parser.add_argument("--log", default="vanilla.log", type=str,
-                    help="Vanilla log file")
-
-args = parser.parse_args()
+main()
