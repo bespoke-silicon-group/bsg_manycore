96ce0f1401b758c714ae1d9ebc1df5f4d8c03303
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-05-23 16:25:26 -0700

    Added a cpp header test

--- software/spmd/c++/main.cpp
+++ software/spmd/c++/main.cpp
@@ -1,11 +1,22 @@
+#include <cstring>
+#include "bsg_manycore.h"
+#include "bsg_set_tile_x_y.h"
+
 class A {
 public:
         A(int member = 0) : member(member) {}
         int member;
 };
+
 int main(void)
 {
         A a;
         a = 2;
-        return 0;
+
+        char s[] = "12345";
+        if(strlen(s) != 5) {
+          bsg_fail();
+        }
+
+        bsg_finish();
 }
