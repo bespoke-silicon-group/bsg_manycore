38ecb11430192d3efb65fa262f14397eda5e964f
Seyed Borna Ehsani <behsani@cs.washington.edu>
2020-06-09 17:39:19 -0700

    Fixup, updated calculating the tile group shared memory address

--- software/bsg_manycore_lib/bsg_shared_mem.hpp
+++ software/bsg_manycore_lib/bsg_shared_mem.hpp
@@ -3,7 +3,7 @@ extern "C" {
 #include "bsg_manycore.h"
 }
 #include <cstdlib>
-#include <math.h>
+#include <cmath>
 
 namespace bsg_manycore {
     /**
@@ -25,6 +25,7 @@ namespace bsg_manycore {
      * TODO: type of addresses should be int or uint32_t not TYPE
      * TODO: return error if address is larger than 12 bits
      * TODO: return error if index is larger than array size
+     * TODO: Assert tile group dimensions are power of two
      */
     template <typename TYPE, std::size_t SIZE, std::size_t TG_DIM_X, std::size_t TG_DIM_Y, std::size_t STRIPE_SIZE=1>
     class TileGroupSharedMem {
@@ -33,31 +34,23 @@ namespace bsg_manycore {
         static constexpr std::size_t STRIPES = SIZE/STRIPE_SIZE + SIZE%STRIPE_SIZE;
         static constexpr std::size_t STRIPES_PER_TILE = (STRIPES/TILES) + (STRIPES%TILES == 0 ? 0 : 1);
         static constexpr std::size_t ELEMENTS_PER_TILE = STRIPES_PER_TILE * STRIPE_SIZE;
-        static constexpr std::size_t HASH = STRIPE_SIZE;
 
-        static constexpr uint32_t DMEM_START_ADDR = 0x1000;                                  // Beginning of DMEM
-        static constexpr uint32_t SHARED_PREFIX = 0x1;                                       // Tile group shared memory EVA prefix
+        static constexpr uint32_t DMEM_START_ADDR = 0x1000;    // Beginning of DMEM
+        static constexpr uint32_t SHARED_PREFIX = 0x1;         // Tile group shared memory EVA prefix
+        static constexpr uint32_t HASH = 0x0;                  // Hash code representing the stripe size
 
-        static constexpr uint32_t WORD_ADDR_BITS = 2;                                        // Word addresable bits
-        static constexpr uint32_t STRIPE_BITS = ceil(log2(STRIPE_SIZE)) + WORD_ADDR_BITS;    // Number of bits used for STRIPE 
-        static constexpr uint32_t MAX_X_BITS = 6;                                            // Maximum bits needed for X coordinate
-        static constexpr uint32_t X_CORD_BITS = ceil(log2(TG_DIM_X));                        // Destination tile's X bits
-        static constexpr uint32_t MAX_Y_BITS = 5;                                            // Maximum bits needed for X coordinate
-        static constexpr uint32_t Y_CORD_BITS = ceil(log2(TG_DIM_Y));                        // Destination tile's Y bits
-        static constexpr uint32_t UNUSED_BITS = MAX_X_BITS - X_CORD_BITS +                   // Unused bits of x/y coordinates
-                                                MAX_Y_BITS - Y_CORD_BITS;                    
-        static constexpr uint32_t LOCAL_ADDR_BITS = 12 - STRIPE_BITS;                        // Local offset from Dmem[0] bits
-        static constexpr uint32_t HASH_BITS = 4;                                             // Hash function bits
-        static constexpr uint32_t PREFIX_BITS = 5;                                           // Tile group shared memory prefix bits
+        static constexpr uint32_t LOCAL_OFFSET_BITS = 12;      // # of Bits used for 
+        static constexpr uint32_t MAX_X_BITS = 6;              // Maximum bits needed for X coordinate
+        static constexpr uint32_t MAX_Y_BITS = 5;              // Maximum bits needed for X coordinate
+        static constexpr uint32_t HASH_BITS = 4;               // Bits used for EVA to NPA hash
 
-        static constexpr uint32_t X_SHIFT = STRIPE_BITS;                   
-        static constexpr uint32_t Y_SHIFT = X_SHIFT + X_CORD_BITS;                         
-        static constexpr uint32_t LOCAL_ADDR_SHIFT = Y_SHIFT + Y_CORD_BITS;                
-        static constexpr uint32_t HASH_SHIFT = LOCAL_ADDR_SHIFT + LOCAL_ADDR_BITS;        
-        static constexpr uint32_t SHARED_PREFIX_SHIFT = HASH_SHIFT + HASH_BITS;           
+        static constexpr uint32_t HASH_SHIFT = LOCAL_OFFSET_BITS + MAX_X_BITS + MAX_Y_BITS;
+        static constexpr uint32_t SHARED_PREFIX_SHIFT = HASH_SHIFT + HASH_BITS;
 
-        static constexpr uint32_t STRIPE_MASK = (1 << STRIPE_BITS) - 1;
-        static constexpr uint32_t LOCAL_ADDR_MASK = (1 << LOCAL_ADDR_BITS) - 1;
+        static constexpr uint32_t X_BITS = ceil(log2(TG_DIM_X));
+        static constexpr uint32_t Y_BITS = ceil(log2(TG_DIM_Y));
+
+        static constexpr uint32_t ALIGNMENT = ceil(log2(sizeof(TYPE) * STRIPE_SIZE));
 
 
 
@@ -65,13 +58,13 @@ namespace bsg_manycore {
             _local_addr = reinterpret_cast<TYPE> (_data);                  // Local address of array
             TYPE _local_offset = _local_addr - DMEM_START_ADDR;            // Offset from DMEM[0]
 
-            TYPE _address = ( ((_local_offset & STRIPE_MASK))                                             |
-                              (((_local_offset >> STRIPE_BITS) & LOCAL_ADDR_MASK) << LOCAL_ADDR_SHIFT)    |
-                              (HASH << HASH_SHIFT)                                                             |
+            TYPE _address = ( (_local_offset << (X_BITS + Y_BITS)) |
+                              (HASH << HASH_SHIFT)                 |
                               (SHARED_PREFIX << SHARED_PREFIX_SHIFT) );
 
             _addr = reinterpret_cast<TYPE*> (_address);
         };
+
 
         std::size_t size() const { return SIZE; }
         std::size_t stripe_size() const { return STRIPE_SIZE; }
@@ -95,10 +88,14 @@ namespace bsg_manycore {
 //            return _addr[i];
 //        }
 
+       
+//        constexpr bool is_powerof2(int v) {
+//            return v && ((v & (v - 1)) == 0);
+//        }
 
 //    private:
-
-        TYPE _data [ELEMENTS_PER_TILE];
+        // Local address should be aligned by a factor of data type times stripe size
+        TYPE _data [ELEMENTS_PER_TILE] __attribute__ ((aligned (1 << ALIGNMENT)));
         TYPE _local_addr;
         TYPE *_addr;
     };
