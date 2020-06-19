99791ecd3eaaca49ed5148679f661cc3c35dff31
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-06-18 17:49:50 -0700

    Check for 0

--- software/spmd/float_cmp/main.c
+++ software/spmd/float_cmp/main.c
@@ -13,8 +13,8 @@ int main()
     int a = (NAN >= NAN) ? 1 : 0;
     int b = (A[0] >= A[0]) ? 1 : 0;
 
-    if(a != b) {
-      bsg_printf("Error: mismatch with NAN >= NAN:\n");
+    if(a != 0 || b != 0) {
+      bsg_printf("Error: NAN >= NAN is equal to 0, but got following...\n");
       bsg_printf("     Hardcoded comparison reuslted in %d\n", a);
       bsg_printf("     Array element comparison reuslted in %d\n", b);
       bsg_fail();
