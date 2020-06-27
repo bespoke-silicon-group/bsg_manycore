015cab24b5b840aeac9a706c2b184c46cd8990c1
Tommy Jung <tommy.dc.jung@gmail.com>
2020-06-26 22:53:15 -0700

    move bsg_noc_crossbar

--- machines/sim_filelist.mk
+++ machines/sim_filelist.mk
@@ -76,4 +76,4 @@ VSOURCES += $(BSG_MANYCORE_DIR)/testbenches/common/v/spmd_testbench.v
 VSOURCES += $(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_manycore_top_crossbar.v
 VSOURCES += $(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_manycore_crossbar.v
 VSOURCES += $(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_manycore_link_to_crossbar.v
-VSOURCES += $(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_noc_crossbar.v
+VSOURCES += $(BASEJUMP_STL_DIR)/bsg_noc/bsg_noc_crossbar.v
