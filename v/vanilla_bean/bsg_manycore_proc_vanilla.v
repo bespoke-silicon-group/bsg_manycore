5b2e66a2f29dacb0e1f53cada804eecf7ff0ec05
Seyed Borna Ehsani <behsani@cs.washington.edu>
2020-07-02 01:46:11 -0700

    Added CSRs for tile group dimensions, used for Shared EVA translation

--- v/vanilla_bean/bsg_manycore_proc_vanilla.v
+++ v/vanilla_bean/bsg_manycore_proc_vanilla.v
@@ -152,6 +152,9 @@ module bsg_manycore_proc_vanilla
   logic freeze;
   logic [x_cord_width_p-1:0] tgo_x;
   logic [y_cord_width_p-1:0] tgo_y;
+  logic [x_cord_width_p-1:0] tg_dim_x;
+  logic [y_cord_width_p-1:0] tg_dim_y;
+
   logic [pc_width_lp-1:0] pc_init_val;
   logic dram_enable;
 
@@ -196,6 +199,8 @@ module bsg_manycore_proc_vanilla
     ,.tgo_y_o(tgo_y)
     ,.pc_init_val_o(pc_init_val)
     ,.dram_enable_o(dram_enable)
+    ,.tg_dim_x_o(tg_dim_x)
+    ,.tg_dim_y_o(tg_dim_y)
 
     ,.my_x_i(my_x_i)
     ,.my_y_i(my_y_i)
@@ -259,6 +264,8 @@ module bsg_manycore_proc_vanilla
     ,.tgo_x_i(tgo_x)
     ,.tgo_y_i(tgo_y) 
     ,.dram_enable_i(dram_enable)
+    ,.tg_dim_x_i(tg_dim_x)
+    ,.tg_dim_y_i(tg_dim_y) 
 
     ,.my_x_i(my_x_i)
     ,.my_y_i(my_y_i)
