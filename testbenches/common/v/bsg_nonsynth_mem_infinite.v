fd8e39ba656688398368fdd2f811d6442c72cec6
Tommy Jung <tommy.dc.jung@gmail.com>
2020-07-02 09:33:30 -0700

    credit_or_ready_o

--- testbenches/common/v/bsg_nonsynth_mem_infinite.v
+++ testbenches/common/v/bsg_nonsynth_mem_infinite.v
@@ -65,7 +65,7 @@ module bsg_nonsynth_mem_infinite
 
     ,.packet_i('0)
     ,.packet_v_i(1'b0)
-    ,.packet_ready_o()
+    ,.packet_credit_or_ready_o()
 
     ,.return_packet_o()
     ,.return_packet_v_o()
