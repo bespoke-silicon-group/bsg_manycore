effb6cddea244c2314f785d030e75dd05555f621
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-08-12 22:59:51 -0700

    preprocessor fix

--- software/bsg_manycore_lib/bsg_manycore.h
+++ software/bsg_manycore_lib/bsg_manycore.h
@@ -155,7 +155,7 @@ inline void bsg_fence()      { __asm__ __volatile__("fence" :::); }
 // on average.
 #ifdef __clang__
 #define bsg_attr_remote __attribute__((address_space(1)))
-#elif defined(__GNUC__) && defined(__cplusplus)
+#elif defined(__GNUC__) && !defined(__cplusplus)
 #define bsg_attr_remote remote
 #else
 #define bsg_attr_remote
