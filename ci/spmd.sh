300516fdcffc36eed8cc8e0243c075ed5489154d
Tommy Jung <tommy.dc.jung@gmail.com>
2020-09-30 13:56:39 -0700

    FIX CI

--- ci/spmd.sh
+++ ci/spmd.sh
@@ -3,8 +3,7 @@
 NUM_CORES=${CI_NUM_CORES:-5}
 
 cd software/spmd/
-make clean
-make -j $NUM_CORES recurse-clean > /dev/null 2>&1
+make clean > /dev/null 2>&1
 make -j $NUM_CORES recurse-all > /dev/null 2>&1
 echo ""
 echo "=========================================="
