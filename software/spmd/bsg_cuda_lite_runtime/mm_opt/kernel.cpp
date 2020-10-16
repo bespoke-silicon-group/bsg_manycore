2e13d4c2584579819e3ec69171c155668a3b551f
lc873 <lc873@cornell.edu>
2020-10-16 16:02:25 -0400

    [opt_mm] docs on the mm implementation

--- software/spmd/bsg_cuda_lite_runtime/mm_opt/kernel.cpp
+++ software/spmd/bsg_cuda_lite_runtime/mm_opt/kernel.cpp
@@ -1,6 +1,59 @@
 //====================================================================
-// addmml kernel
-// 03/09/2020 Kexin Zheng, Lin Cheng (kz73@cornell.edu, lc873@cornell.edu)
+// mm kernel
+//
+// 03/09/2020 Kexin Zheng
+// 06/10/2020 Lin Cheng
+//
+// This is a "output stationary" SPMD Matrix Multiplication implementation
+// To get good performance, we perform three levels of blocking, or tiling
+//  - blocking into L2
+//  - blocking into scratchpad
+//  - blocking into registers
+//
+// BLOCK_DIM is definted as the block size for "blocking into scratchpad"
+//
+// Blocing into L2:
+//   We spatially distribute the output blocks to minimize per-iteration
+//   L2 footprint. Say the output matrix has 4x2 blocks, and we have a
+//   2x2 tile group -- then the tile group will work on the left most 4 blocks
+//   0* 1* 2  3
+//   4* 5* 6  7
+//   So each iteration, we load 4 unique data blocks.
+//   If we compute another way
+//   0* 1* 2* 3*
+//   4  5  6  7
+//   We need to load 5 unique blocks each iteration.
+//
+// Blocking into scratchpad:
+//   We fully compute a BLOCK_DIM * BLOCK_DIM output block at a time
+//   Each iteration, we load one block of matrix A, and one block of Matrix B,
+//   then accumulate the result of A * B into partial sum.
+//   Since we transfer n^2 data and do n^3 computation, the larger the block,
+//   the better the performance.
+//
+// Blocking into registers:
+//   We want to reduce reads and writes to scratchpad, by keep data in the
+//   registers for as long as possible. Thus, we block into registers with 4x4
+//   sub-blocks
+//   For each 4x4 output sub-block, we do the following:
+//    - (1) load 4x4(16) partial sum into registers
+//    - (2) read 1 element per row, for 4 rows that are relevant
+//    - (3) read 1 element per col, for 4 cols that are relevant
+//    - (4) perform 16 fmadd
+//    - (5) goto step (2) until we reach the end of row/col
+//
+//          c0    c1    c2    c3
+//
+//    r0  r0*c0 r0*c1 r0*c2 r0*c3
+//
+//    r1  r1*c0 r1*c1 r1*c2 r1*c3
+//
+//    r2  r2*c0 r2*c1 r2*c2 r2*c3
+//
+//    r3  r3*c0 r3*c1 r3*c2 r3*c3
+//
+//    We use 16 + 8 = 24 registers in total
+//    Refer to compute_block for more details
 //====================================================================
 
 #define BLOCK_DIM 8 // sqrt(4KB/4 byte/4 data matrix) = 15 max
