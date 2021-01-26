27fda370cee5c70a609adc3d9443b9efe154f0cb
Tommy Jung <tommy.dc.jung@gmail.com>
2021-01-25 19:52:57 -0800

    offset dmem start addr by 8 bytes for interrupt handler

--- software/py/bsg_manycore_link_gen.py
+++ software/py/bsg_manycore_link_gen.py
@@ -135,8 +135,9 @@ class bsg_manycore_link_gen:
     # LMA (Load Memory Address)    => NPA used by loader
     # VMA (Virtual Memory Address) => Logical address used by linker for
     #                                 symbol resolutions
-    _DMEM_VMA_START   = 0x0000
-    _DMEM_VMA_SIZE    = 0x1000
+    # bsg-tommy: we reserve the first two words of DMEM for interrupt handlers.
+    _DMEM_VMA_START   = 0x0008
+    _DMEM_VMA_SIZE    = 0x0ff8
     _DRAM_T_LMA_START = 0x80000000
     _DRAM_T_LMA_SIZE  = self._imem_size
     _DRAM_D_LMA_START = 0x80000000 + self._imem_size
@@ -174,7 +175,7 @@ class bsg_manycore_link_gen:
     # DMEM sections
     for i, m in enumerate(section_map):
       sec = m[0]
-      laddr = '0x0000'
+      laddr = '0x0008'
       in_sections = m[1]
 
       # Place objects into dmem if default data loc is dmem
