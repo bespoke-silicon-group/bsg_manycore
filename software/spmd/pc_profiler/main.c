12dc2989fc3701260d904ae6c3be362df2a64cc1
sripathi-muralitharan <sripathimuralitharan@gmail.com>
2021-02-18 15:18:06 -0800

    Adding a pc profiler

---
+++ software/spmd/pc_profiler/main.c
@@ -0,0 +1,90 @@
+// Program to print 10 fibonacci numbers; A sample program that will get profiled
+
+#include "bsg_manycore.h"
+#include "bsg_set_tile_x_y.h"
+
+// Histogram location?
+// 1. Can histogram base address be accessed directly from symbol table?
+// 2. Should linker be modified so that histogram is statically assigned a region in memory?
+// Fixme: Histogram base address adjusted to the right value after looking at disassembly
+#define HISTOGRAM_BASE_ADDR 0x81000290
+
+#ifndef HIST_SPACE
+#define HIST_SPACE 16777216
+#endif
+
+#define NUM_TILES bsg_group_size
+
+// Sample program to print 10 fibonacci numbers
+void fibonacci()
+{
+  int i;
+  int a = 0;
+  int b = 1;
+  int temp = 0;
+
+  for (i = 0; i < 10; i++)
+  {
+    temp = a + b;
+    a = b;
+    b = temp;
+  }
+
+  if (a != 55)
+    bsg_fail();
+}
+
+void read_histogram()
+{
+  int val;
+  int count = 0;
+  // Fixme: This array is populated statically by observing disassembly. Need to make it standalone
+  int expectation[10] = {1 * NUM_TILES, 1 * NUM_TILES, 10 * NUM_TILES, 10 * NUM_TILES, 10 * NUM_TILES, 10 * NUM_TILES, 10 * NUM_TILES, 1 * NUM_TILES, 1 * NUM_TILES, 1 * NUM_TILES};
+  // Adjust start value based on disassembly since function is small and therefore inlined with O2 optimization
+  int *i;
+
+  for (i = (HISTOGRAM_BASE_ADDR); i < (HISTOGRAM_BASE_ADDR + HIST_SPACE); i++)
+  {
+    val = *i;
+    if (val != 0)
+    {
+      bsg_printf("%p: %x\n", i, val);
+      if (expectation[count] != val)
+        bsg_fail();
+      count++;
+    }
+      
+  }
+
+  bsg_finish();
+}
+
+void main()
+{
+  // tile setup
+  bsg_set_tile_x_y();
+
+
+  // Start profiling code, enable interrupts
+  int mie, mstatus;
+  mie = 0x20000;
+  mstatus = 0x8;
+  __asm__ __volatile__ ("csrw mie, %0": : "r" (mie));
+  __asm__ __volatile__ ("csrw mstatus, %0" : : "r" (mstatus));
+
+  // Code to be profiled
+  fibonacci();
+
+  // Reading histogram, disable interrupts
+  mie = mstatus = 0;
+  __asm__ __volatile__ ("csrw mie, %0": : "r" (mie));
+  __asm__ __volatile__ ("csrw mstatus, %0" : : "r" (mstatus));
+
+  if (__bsg_id == 0)
+  {
+    // read histogram
+    read_histogram();
+  }
+  
+  bsg_wait_while(1);
+}
