b7344699e590def3d10c1c4f3098171572fbd02a
Max Ruttenberg <mrutt@cs.washington.edu>
2021-03-10 13:51:10 -0800

    Fixes bug in gups; refactors init to reduce load time

--- software/spmd/gups/gups.c
+++ software/spmd/gups/gups.c
@@ -1,20 +1,38 @@
 #include <bsg_manycore.h>
-
-#define UNROLL 16
+#include <bsg_set_tile_x_y.h>
+#define UNROLL 8
+__attribute__((noinline))
 int gups(bsg_attr_remote int *__restrict G, int *__restrict A, int n)
 {
-    int k[UNROLL];
-    int v[UNROLL];
     for (int i = 0; i < n; i += UNROLL) {
-        bsg_unroll(32)
-        for (int j = 0; j < UNROLL; ++j) {
-            k[j] = A[i+j];
-            v[j] = A[k[j]];
-        }
+        int g[UNROLL];
 
         bsg_unroll(32)
         for (int j = 0; j < UNROLL; ++j)
-            G[k[j]] = v[j] ^ k[j];
+            g[j] = G[A[i+j]] ^ A[i+j];
+
+        bsg_unroll(32)
+        for (int j = 0; j < UNROLL; ++j)
+            G[A[i+j]] = g[j];
     }
+
+    return 0;
+}
+
+#define BLOCK_SIZE 32
+__attribute__((noinline))
+int cuda_gups(bsg_attr_remote int *__restrict G, bsg_attr_remote int *__restrict A, int n_per_core)
+{
+    int block_size = n_per_core < BLOCK_SIZE ? n_per_core : BLOCK_SIZE;
+    int A_local[block_size];
+
+    for (int i = 0; i < n_per_core; i += block_size) {
+        for (int j = 0; j < block_size; j++) {
+            A_local[j] = A[bsg_id * n_per_core + i + j];
+        }
+
+        gups(G, A_local, block_size);
+    }
+
     return 0;
 }
--- software/spmd/gups/spmd_tb_main.c
+++ software/spmd/gups/spmd_tb_main.c
@@ -4,28 +4,26 @@
 #define BSG_TILE_GROUP_Y_DIM bsg_tiles_Y
 #include <bsg_tile_group_barrier.h>
 
-#define A_SIZE (16)
-#define G_SIZE (1024*1024)
+#define A_n_per_core (1024)
+#define A_SIZE (A_n_per_core * bsg_tiles_X * bsg_tiles_Y)
+#define G_SIZE (1024*1024*1024)
 
 #define dram_data __attribute__((section(".dram")))
 
-static int A[A_SIZE];
-static dram_data bsg_attr_remote int G[G_SIZE];
+//static dram_data bsg_attr_remote int G[G_SIZE];
+static dram_data bsg_attr_remote int G[1];
 
 INIT_TILE_GROUP_BARRIER(rbar, cbar, 0, bsg_tiles_X-1, 0, bsg_tiles_Y-1);
 
 void gups_init(bsg_attr_remote int *__restrict G,
-              int G_n,
-              int *__restrict A,
-              int A_n)
+               int *__restrict A)
 {
     int x = 0xdeadbeef ^ (bsg_id << 17);
-    for (int i = 0; i < A_n; ++i) {
+    for (int i = 0; i < A_n_per_core; ++i) {
         x ^= x << 11;
         x ^= x >> 5;
         x ^= x << 15;
         A[i] = x & (G_SIZE-1);
-        bsg_print_int(A[i]);
     }
 
     return;
@@ -33,19 +31,20 @@ void gups_init(bsg_attr_remote int *__restrict G,
 
 int gups(bsg_attr_remote int *__restrict G, int *__restrict A, int n);
 
-#define STREAM_READ
-
 int main()
 {
     bsg_set_tile_x_y();
+
+    int A[A_n_per_core];
+    gups_init(G, A);
+
+    bsg_tile_group_barrier(&rbar, &cbar);
     bsg_cuda_print_stat_kernel_start();
 
-    gups_init(G, G_SIZE, A, A_SIZE);
+    gups(G, A, A_n_per_core);
 
-    //bsg_tile_group_barrier(&rbar, &cbar);
-    gups(G, A, A_SIZE);
-
-    bsg_cuda_print_stat_kernel_end();
     bsg_tile_group_barrier(&rbar, &cbar);
+    bsg_cuda_print_stat_kernel_end();
+
     bsg_finish();
 }
