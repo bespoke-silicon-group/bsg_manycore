b66227ec37cb28255311efbdd9bf9d886c0e6073
Max Ruttenberg <mrutt@cs.washington.edu>
2021-09-01 07:30:59 -0700

    assembly implementation

---
+++ software/bsg_manycore_lib/bsg_mcs_mutex.h
@@ -0,0 +1,26 @@
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif    
+
+    // must live in tile's local memory (DMEM)
+    typedef struct bsg_mcs_mutex_node {
+        struct bsg_mcs_mutex_node *next;
+        int unlocked;        
+    } bsg_mcs_mutex_node_t;    
+
+    // must live in dram
+    typedef bsg_mcs_mutex_node_t* bsg_mcs_mutex_t;
+
+    void bsg_mcs_mutex_acquire(bsg_mcs_mutex_t *mtx                //!< An MCS mutex
+                               , bsg_mcs_mutex_node_t *lcl         //!< A local pointer to a node allocated in tile's local memory
+                               , bsg_mcs_mutex_node_t *lcl_as_glbl //!< A global pointer to a node allocated in tile's local memory
+        );
+
+    void bsg_mcs_mutex_release(bsg_mcs_mutex_t *mtx                //!< An MCS mutex
+                               , bsg_mcs_mutex_node_t *lcl         //!< A local pointer to a node allocated in tile's local memory
+                               , bsg_mcs_mutex_node_t *lcl_as_glbl //!< A global pointer to a node allocated in tile's local memory
+        );
+#ifdef __cplusplus
+}
+#endif
