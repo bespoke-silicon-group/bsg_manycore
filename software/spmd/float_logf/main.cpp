8705a1b2bdc4eca2bfbcc4ec0c24fb568ef52c9b
Dustin Richmond <dustinar@uw.edu>
2021-09-29 16:33:20 -0700

    Add comment to describe issue resolution

--- software/spmd/float_logf/main.cpp
+++ software/spmd/float_logf/main.cpp
@@ -1,3 +1,14 @@
+// The intent of this test is to demonstrate a behavior that caused an RTL simulation error.
+// The error occured when this test is run, and produced this message:
+// bsg_manycore/v/vanilla_bean/fcsr.v", 187: spmd_testbench.tb.DUT.py[0].podrow.px[0].pod.mc_y[0].mc_x[0].mc.y[0].x[0].tile.proc.h.z.vcore.fcsr0.unnamed$$_3: started at 1897000ps failed at 1897000ps
+//        Offending '(~(|fflags_v_i))'
+// Error: "bsg_manycore/v/vanilla_bean/fcsr.v", 187: spmd_testbench.tb.DUT.py[0].podrow.px[0].pod.mc_y[0].mc_x[0].mc.y[0].x[0].tile.proc.h.z.vcore.fcsr0.unnamed$$_3: at time 1897000 ps
+// Exception cannot be accrued while being written by fcsr op.
+// 
+// This issue was resolved by PR 587 in BSG Manycore
+//
+// Author: drichmond
+
 #include "bsg_manycore.h"
 #include "bsg_set_tile_x_y.h"
 #include <math.h>
