e79273058c303bba33ffc45eed38bf42abe4e613
Max Ruttenberg <mrutt@cs.washington.edu>
2021-10-18 22:57:46 -0700

    syntax sugar

--- software/spmd/work_queue/main.cpp
+++ software/spmd/work_queue/main.cpp
@@ -10,17 +10,59 @@ int b_l = 0;
 int b_s = 1;
 extern "C" void bsg_barrier_amoadd(int *l, int *s);
 
+template <int N>
+struct job {
+    static constexpr int WORDS = N;
+    task t;
+    int  w [N-1];
+};
+
+template <>
+struct job<0> {
+    static constexpr int WORDS = 0;    
+    task t;    
+};
+
+template <typename job_type>
+static void manager_enqueue_job_0(manager *m, job_type *j)
+{
+    j->t.data_words = job_type::WORDS;
+    manager_enqueue(m, &j->t);
+}
+
+template <int I, typename job_type>
+static void manager_enqueue_job_unpack(manager *m, job_type *j)
+{
+    return;
+}
+
+template <int I, typename job_type, typename arg_type>
+static void manager_enqueue_job_unpack(manager *m, job_type *j,  arg_type arg)
+{
+    j->t.data[I] = reinterpret_cast<int&>(arg);
+}
+
+template <int I, typename job_type, typename arg_type, typename ... arg_types>
+static void manager_enqueue_job_unpack(manager *m, job_type *j, arg_type arg, arg_types ...args)
+{
+    j->t.data[I] = reinterpret_cast<int&>(arg);
+    manager_enqueue_job_unpack<I+1>(m, j, args...);
+}
+
+template <typename job_type, typename ... arg_types>
+static void manager_enqueue_job(manager *m, job_type *j,  arg_types ...args)
+{
+    manager_enqueue_job_unpack<0>(m, j, args...);
+    manager_enqueue_job_0(m, j);
+}
 
 __attribute__((section(".dram")))
 task_queue workq;
 
-__attribute__((section(".dram")))
-bsg_mcs_mutex_t lock;
-bsg_mcs_mutex_node_t  node;
-bsg_mcs_mutex_node_t *self;
 void say_hi(task *t)
 {
     bsg_print_int(t->data[0]);
+    bsg_print_float(reinterpret_cast<float&>(t->data[1]));
 }
 
 //#define DEBUG
@@ -28,8 +70,6 @@ int main()
 {
 
   bsg_set_tile_x_y();
-
-  self = bsg_tile_group_remote_pointer<bsg_mcs_mutex_node_t>(__bsg_x, __bsg_y, &node);
 
   manager m;
   if (__bsg_x == bsg_tiles_X/2
@@ -41,18 +81,16 @@ int main()
   if (__bsg_x == bsg_tiles_X/2
       && __bsg_y == bsg_tiles_Y/2) {
       // jobs
-      task job  [JOBS];
+      job<2> job[JOBS];
       int  _sync [JOBS] = {};
       int *sync = bsg_tile_group_remote_pointer(__bsg_x, __bsg_y, _sync);
 
       // enqueue jobs
       for (int i = 0; i < JOBS; i++) {
-          task_clear(&job[i]);
-          job[i].call = say_hi;
-          job[i].done = &sync[i];
-          job[i].data[0] = i;
-          job[i].data_words = 1;
-          manager_enqueue(&m, &job[i]);
+          task_clear(&job[i].t);
+          job[i].t.call = say_hi;
+          job[i].t.done = &sync[i];
+          manager_enqueue_job(&m, &job[i], i, 3.14159f);
       }
 
       // dispatch all jobs
