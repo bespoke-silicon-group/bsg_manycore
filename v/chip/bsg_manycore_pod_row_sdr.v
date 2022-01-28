033c6045c01041599d4ae4a996d2de7be9332aa0
Dan Petrisko <petrisko@cs.washington.edu>
2022-01-28 14:29:37 -0800

    Adding block size in words

--- v/chip/bsg_manycore_pod_row_sdr.v
+++ v/chip/bsg_manycore_pod_row_sdr.v
@@ -26,6 +26,7 @@ module bsg_manycore_pod_row_sdr
     , `BSG_INV_PARAM(dmem_size_p)
     , `BSG_INV_PARAM(icache_entries_p)
     , `BSG_INV_PARAM(icache_tag_width_p)
+    , `BSG_INV_PARAM(icache_block_size_in_words_p)
 
     , parameter num_vcache_rows_p=1
     , `BSG_INV_PARAM(vcache_addr_width_p)
@@ -180,6 +181,7 @@ module bsg_manycore_pod_row_sdr
     ,.dmem_size_p         (dmem_size_p)
     ,.icache_entries_p    (icache_entries_p)
     ,.icache_tag_width_p  (icache_tag_width_p)
+    ,.icache_block_size_in_words_p  (icache_block_size_in_words_p)
 
     ,.num_vcache_rows_p   (num_vcache_rows_p)
     ,.vcache_addr_width_p (vcache_addr_width_p)
