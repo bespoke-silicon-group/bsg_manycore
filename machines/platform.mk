21ec50073c6453ba384645d0889a58e11733f2ed
Tommy Jung <tommy.dc.jung@gmail.com>
2022-02-04 09:43:13 -0800

    add pod_4x4_hbm2 CI

--- machines/platform.mk
+++ machines/platform.mk
@@ -2,7 +2,7 @@
 BSG_PLATFORM ?= vcs
 
 ifeq ($(BSG_PLATFORM),vcs)
-DEFAULT_MACHINES = pod_1x1 pod_1x1_hbm2 pod_4x4
+DEFAULT_MACHINES = pod_1x1 pod_1x1_hbm2 pod_4x4 pod_4x4_hbm2
 BSG_SIM_BASE = simv
 else ifeq ($(BSG_PLATFORM),verilator)
 DEFAULT_MACHINES = pod_1x1_4X2Y
