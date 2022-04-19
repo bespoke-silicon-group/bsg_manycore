80fd57e238d4b94aa4397ad7f56eea34d7cd4e7a
Max Ruttenberg <mrutt@cs.washington.edu>
2022-04-19 15:23:21 -0700

    revert

--- testbenches/common/v/vanilla_core_profiler.cpp
+++ testbenches/common/v/vanilla_core_profiler.cpp
@@ -6,12 +6,7 @@
 #include <cstdlib>
 #include "profiler.hpp"
 
-class vanilla_core_profiler : public bsg_profiler::profiler {
-public:
-    vanilla_core_profiler(): bsg_profiler::profiler (){}
-};
-
-vanilla_core_profiler bsg_vanilla_core_profiler;
+bsg_profiler::profiler bsg_vanilla_core_profiler;
 
 DEFINE_PROFILER(bsg_vanilla_core_profiler);
 
