52fad2e3a3ad8e0c1e037cb97892c76fcc130de3
Dustin Richmond <dustinar@uw.edu>
2022-05-24 13:38:26 -0700

    Refactor for cleanliness/polish

--- software/py/remote_prof.py
+++ software/py/remote_prof.py
@@ -17,7 +17,131 @@ parser.add_argument("--first", default=0, type=int, help="First Cycle for Spacet
 parser.add_argument("--last", default=np.Inf, type=int, help="Last Cycle for Spacetime Graph")
 parser.add_argument("--cycbin", default=None, type=int, help="Cycles per Bin in Heatmap. Cannot specify both --cycbin and --nbins simultaneously.")
 parser.add_argument("--nbins", default=None, type=int, help="Number of Bins in Heatmap. Cannot specify both --cycbin and --nbins simultaneously.")
+parser.add_argument("--filename", default="./remote_load_trace.csv", type=str, help="Remote Load Trace CSV File")
 
+# Figure formatting parameters
+# Rows per Inch
+ROWS_PER_INCH = 4
+# Columns per inch
+COLS_PER_INCH = 40
+# Labelsize y/x
+LABELSIZE_Y = 10
+LABELSIZE_X = 8
+
+# It isn't useful to show huge latencies since it changes the heatmap
+# scale. Use this to cap the maximum latency in a bin when plotting.
+MAX_LATENCY = 1000
+
+# Writes (normalized) request heatmap. Assumes each cache can process
+# 1 READ OR WRITE request on each cycle.
+def write_heatmap_req(df, f):
+    # Create Bins
+    r = pd.interval_range(start = df.start_cycle.min(), end = df.start_cycle.max(), freq = f)
+    bins = pd.cut(df.start_cycle, bins=r, precision=1)
+    
+    # Bin, and count number of requests in each bin
+    df = df.groupby([bins, "dest_y", "dest_x"]).size()
+    df = df.unstack(level=0)
+
+    # Normalize by dividing by cache bandwidth
+    df = 100.0 *(df/f)
+    
+    # Generate Figure
+    height = len(df.index) / ROWS_PER_INCH
+    width = len(df.columns) / COLS_PER_INCH
+    
+    fig = plt.figure(figsize=(width, height))
+    ax = sns.heatmap(df, cbar_kws={'label': 'Percent of Cache Request Bandwidth (# Requests / # of Cycles in bin)'}, vmin =0, vmax=max(df.max().max(), 100.00))
+    ax.tick_params(axis='x', labelsize=LABELSIZE_X)
+    ax.tick_params(axis='y', labelsize=LABELSIZE_Y)
+    
+    ax.set_xlabel(f"Cycle Range ({len(df.columns)} bins of {f} Cycles)")
+    ax.set_ylabel(f"Cache Location (Y-X)")
+    ax.set_title(f"HammerBlade Spacetime Read & Write Request Heatmap")
+    
+    plt.tight_layout()
+    fig.savefig("request_heatmap.pdf")
+    plt.close(fig)
+
+
+# Writes mean read latency heatmap. Takes the maximum write latency of
+# each request in each bucket. Writes are ignored since there is no way
+# to track the latency of write responses
+def write_heatmap_maxlat(df, f):
+    # Remove write requests, since they do not have a valid latency
+    df = df[df.type != "write"]
+    df = df.fillna(0)
+    df.latency = df.latency.astype(int)
+
+    # Create Bins
+    r = pd.interval_range(start = df.start_cycle.min(), end = df.start_cycle.max(), freq = f)
+    bins = pd.cut(df.start_cycle, bins = r, precision=0)
+
+    # TODO: Find out why reported latency is so high on some of these requests.
+    df.latency = df.latency.apply(lambda x: MAX_LATENCY if x > MAX_LATENCY else x)
+
+    # Bin, and determine max in each bin
+    df = df.groupby([bins, "dest_y", "dest_x"]).latency.max()
+    df = df.unstack(level=0)
+    df = df.fillna(0)
+
+    # Generate Figure
+    height = len(df.index) / ROWS_PER_INCH
+    width = len(df.columns) / COLS_PER_INCH
+
+    fig = plt.figure(figsize=(width, height))
+    ax = sns.heatmap(df, cbar_kws={'label': 'Max Latency of Read Requests in Bin'})
+    ax.tick_params(axis='x', labelsize=LABELSIZE_X)
+    ax.tick_params(axis='y', labelsize=LABELSIZE_Y)
+    ax.set_xlabel(f"Cycle Range ({len(df.columns)} bins of {f} Cycles)")
+    ax.set_ylabel(f"Cache Location (Y,X)")
+    ax.set_title(f"HammerBlade Spacetime Max Read Latency Heatmap")
+
+    plt.tight_layout()
+    fig.savefig("maxlat_heatmap.pdf")
+    plt.close(fig)
+
+
+
+# Writes max read latency heatmap. Takes the maximum write latency of
+# each request in each bucket. Writes are ignored since there is no way
+# to track the latency of write responses
+def write_heatmap_meanlat(df, f):
+    # Remove write requests, since they do not have a valid latency
+    df = df[df.type != "write"]
+    df = df.fillna(0)
+    df.latency = df.latency.astype(int)
+
+    # Create Bins
+    r = pd.interval_range(start = df.start_cycle.min(), end = df.start_cycle.max(), freq = f)
+    bins = pd.cut(df.start_cycle, bins = r, precision=0)
+
+    # TODO: Find out why reported latency is so high on some of these requests.
+    df.latency = df.latency.apply(lambda x: MAX_LATENCY if x > MAX_LATENCY else x)
+
+    # Bin, and determine max in each bin
+    df = df.groupby([bins, "dest_y", "dest_x"]).latency.mean()
+    df = df.unstack(level=0)
+    df = df.fillna(0)
+    
+    # Generate Figure
+    height = len(df.index) / ROWS_PER_INCH
+    width = len(df.columns) / COLS_PER_INCH
+    
+    fig = plt.figure(figsize=(width, height))
+    ax = sns.heatmap(df, cbar_kws={'label': 'Mean Latency of Read Requests in Bin'})
+    ax.tick_params(axis='x', labelsize=LABELSIZE_X)
+    ax.tick_params(axis='y', labelsize=LABELSIZE_Y)
+    ax.set_xlabel(f"Cycle Range ({len(df.columns)} bins of {f} Cycles)")
+    ax.set_ylabel(f"Cache Location (Y,X)")
+    ax.set_title(f"HammerBlade Spacetime Mean Read Latency Heatmap")
+    
+    plt.tight_layout()
+    fig.savefig("meanlat_heatmap.pdf")
+    plt.close(fig)
+
+
+# Parse Arguments
 args = parser.parse_args()
 args.source = set(tuple(map(int, c.split(","))) for c in args.source)
 
@@ -27,126 +151,32 @@ if(args.cycbin and args.nbins):
 if(not args.nbins):
     args.nbins = 1000
 
-# Set up figure formatting
-# Rows per Inch
-rpi = 4
-# Columns per inch
-cpi = 40
+# Create DataFrame
+df = pd.read_csv(args.filename)
 
-lsy = 10
-lsx = 8
-    
-p = "remote_load_trace.csv"
-
-df = pd.read_csv(p)
-
+# Filter requests.
+# 1. Remove icache requests
+# 2. Remove packets to host
+# 3. Keep cache packets
 df = df[df.type != "icache"]
 df = df[df.dest_y != 0] # Filter host packets
-
 df = df[(df.dest_y == df.dest_y.min()) | (df.dest_y == df.dest_y.max())]
 
-# Filter out specified sources
+# 4. Filter outside of first/last cycle
+df = df[df.start_cycle > args.first]
+df = df[df.start_cycle < args.last]
+
+# If a source was specified, filter out all other sources
 if(args.source != set()):
     srcs = (df.src_y.combine(df.src_x, lambda y,x: (int(y),int(x))))
     df = df[srcs.apply(lambda l: l in args.source)]
 
-# Filter outside of first/last cycle
-df = df[df.start_cycle > args.first]
-df = df[df.start_cycle < args.last]
-
-# Bin the CSV entries
-stdf = df
-
-# Create bins
+# Determine bin duration (aka Frequency)
 if(args.cycbin):
     f = args.cycbin
 else:
-    f = int((stdf.start_cycle.max() - stdf.start_cycle.min()) / args.nbins)
+    f = int((df.start_cycle.max() - df.start_cycle.min()) / args.nbins)
 
-r = pd.interval_range(start = stdf.start_cycle.min(), end = stdf.start_cycle.max(), freq = f)
-bins = pd.cut(stdf.start_cycle, bins=r, precision=1)
-
-stdf = stdf.groupby([bins, "dest_y", "dest_x"]).size()
-stdf = stdf.unstack(level=0)
-stdf = 100.0 *(stdf/f)
-
-height = len(stdf.index) / rpi
-width = len(stdf.columns) / cpi
-
-print()
-fig = plt.figure(figsize=(width, height))
-ax = sns.heatmap(stdf, cbar_kws={'label': 'Percent of Cache Request Bandwidth (# Requests / # of Cycles in bin)'}, vmin =0, vmax=max(stdf.max().max(), 100.00))
-ax.tick_params(axis='x', labelsize=lsx)
-ax.tick_params(axis='y', labelsize=lsy)
-
-ax.set_xlabel(f"Cycle Range ({len(stdf.columns)} bins of {f} Cycles)")
-ax.set_ylabel(f"Cache Location (Y-X)")
-ax.set_title(f"HammerBlade Spacetime Read & Write Request Heatmap")
-
-plt.tight_layout()
-fig.savefig("request_heatmap.pdf")
-plt.close(fig)
-
-
-ldf = df.copy()
-ldf = ldf[ldf.type != "write"]
-ldf = ldf.fillna(0)
-ldf.latency = ldf.latency.astype(int)
-r = pd.interval_range(start = ldf.start_cycle.min(), end = ldf.start_cycle.max(), freq = f)
-bins = pd.cut(ldf.start_cycle, bins = r, precision=0)
-#pd.IntervalIndex([*filter(lambda i: i.left < ldf.start_cycle.max(), bins)], retbins =True)
-# TODO: Find out why reported latency is so high on some of these requests.
-ldf.latency = ldf.latency.apply(lambda x: 1000 if x > 1000 else x)
-ldf = ldf.groupby([bins, "dest_y", "dest_x"]).latency.max()
-ldf = ldf.unstack(level=0)
-ldf = ldf.fillna(0)
-
-height = len(ldf.index) / rpi
-width = len(ldf.columns) / cpi
-
-fig = plt.figure(figsize=(width, height))
-ax = sns.heatmap(ldf, cbar_kws={'label': 'Max Latency of Read Requests in Bin'})
-ax.tick_params(axis='x', labelsize=lsx)
-ax.tick_params(axis='y', labelsize=lsy)
-ax.set_xlabel(f"Cycle Range ({len(ldf.columns)} bins of {f} Cycles)")
-ax.set_ylabel(f"Cache Location (Y,X)")
-ax.set_title(f"HammerBlade Spacetime Max Read Latency Heatmap")
-
-plt.tight_layout()
-fig.savefig("maxlat_heatmap.pdf")
-plt.close(fig)
-
-
-
-
-ldf = df.copy()
-ldf = ldf[ldf.type != "write"]
-ldf = ldf.fillna(0)
-ldf.latency = ldf.latency.astype(int)
-
-r = pd.interval_range(start = ldf.start_cycle.min(), end = ldf.start_cycle.max(), freq = f)
-bins = pd.cut(ldf.start_cycle, bins = r, precision=0)
-ldf.latency = ldf.latency.apply(lambda x: 1000 if x > 1000 else x)
-ldf = ldf.groupby([bins, "dest_y", "dest_x"]).latency.mean()
-ldf = ldf.unstack(level=0)
-ldf = ldf.fillna(0)
-
-height = len(ldf.index) / rpi
-width = len(ldf.columns) / cpi
-
-fig = plt.figure(figsize=(width, height))
-ax = sns.heatmap(ldf, cbar_kws={'label': 'Mean Latency of Read Requests in Bin'})
-ax.tick_params(axis='x', labelsize=lsx)
-ax.tick_params(axis='y', labelsize=lsy)
-ax.set_xlabel(f"Cycle Range ({len(ldf.columns)} bins of {f} Cycles)")
-ax.set_ylabel(f"Cache Location (Y,X)")
-ax.set_title(f"HammerBlade Spacetime Mean Read Latency Heatmap")
-
-plt.tight_layout()
-fig.savefig("meanlat_heatmap.pdf")
-plt.close(fig)
-
-
-
-
-
+write_heatmap_req(df, f)
+write_heatmap_meanlat(df, f)
+write_heatmap_maxlat(df, f)
