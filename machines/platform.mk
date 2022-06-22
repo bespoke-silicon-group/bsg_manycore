d78cbc12d39c23115012fb7fa0fa85ca20047f0f
Dan Petrisko <petrisko@cs.washington.edu>
2022-06-22 09:34:36 -0700

    Initial xcelium commit (#643)
    * Initial xcelium commit
    
    * Switching to XMSIM flow
    
    * Update platform.mk

--- machines/platform.mk
+++ machines/platform.mk
@@ -7,5 +7,8 @@ BSG_SIM_BASE = simv
 else ifeq ($(BSG_PLATFORM),verilator)
 DEFAULT_MACHINES = pod_1x1_4X2Y
 BSG_SIM_BASE = simsc
+else ifeq ($(BSG_PLATFORM),xcelium)
+DEFAULT_MACHINES = pod_1x1 pod_1x1_hbm2 pod_4x4 pod_4x4_hbm2
+BSG_SIM_BASE = simx
 endif
 
