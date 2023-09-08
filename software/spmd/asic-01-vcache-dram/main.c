dec4e86bedb2be17cfc7bdedba8a63ded3aa8d2b
Tommy Jung <tommy.dc.jung@gmail.com>
2023-09-07 17:23:02 -0700

    fix vcache noc paths

--- software/spmd/asic-01-vcache-dram/main.c
+++ software/spmd/asic-01-vcache-dram/main.c
@@ -11,31 +11,35 @@ int main()
 {
   bsg_set_tile_x_y();
   
+  int stop = N/(bsg_tiles_Y*bsg_tiles_X);
+  int* ptr = &dram_ptr[__bsg_id*stop];
+
   // store;
   bsg_unroll(1)
-  for (int i = __bsg_id*VCACHE_LINE_WORDS; i < N; i+=bsg_tiles_X*bsg_tiles_Y*VCACHE_LINE_WORDS) {
-    dram_ptr[i+0] = i+0;
-    dram_ptr[i+1] = i+1;
-    dram_ptr[i+2] = i+2;
-    dram_ptr[i+3] = i+3;
-    dram_ptr[i+4] = i+4;
-    dram_ptr[i+5] = i+5;
-    dram_ptr[i+6] = i+6;
-    dram_ptr[i+7] = i+7;
+  for (int i = 0; i < stop; i+=VCACHE_LINE_WORDS) {
+    ptr[i+0] = i+0;
+    ptr[i+1] = i+1;
+    ptr[i+2] = i+2;
+    ptr[i+3] = i+3;
+    ptr[i+4] = i+4;
+    ptr[i+5] = i+5;
+    ptr[i+6] = i+6;
+    ptr[i+7] = i+7;
   }
 
   // load;
   int words[VCACHE_LINE_WORDS];
+
   bsg_unroll(1)
-  for (int i = __bsg_id*VCACHE_LINE_WORDS; i < N; i+=bsg_tiles_X*bsg_tiles_Y*VCACHE_LINE_WORDS) {
-    words[0] = dram_ptr[i+0]; 
-    words[1] = dram_ptr[i+1]; 
-    words[2] = dram_ptr[i+2]; 
-    words[3] = dram_ptr[i+3]; 
-    words[4] = dram_ptr[i+4]; 
-    words[5] = dram_ptr[i+5]; 
-    words[6] = dram_ptr[i+6]; 
-    words[7] = dram_ptr[i+7]; 
+  for (int i = 0; i < stop; i+=VCACHE_LINE_WORDS) {
+    words[0] = ptr[i+0]; 
+    words[1] = ptr[i+1]; 
+    words[2] = ptr[i+2]; 
+    words[3] = ptr[i+3]; 
+    words[4] = ptr[i+4]; 
+    words[5] = ptr[i+5]; 
+    words[6] = ptr[i+6]; 
+    words[7] = ptr[i+7]; 
     if (words[0] != i+0) bsg_fail();
     if (words[1] != i+1) bsg_fail();
     if (words[2] != i+2) bsg_fail();
