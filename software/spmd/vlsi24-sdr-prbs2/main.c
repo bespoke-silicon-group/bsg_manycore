c654891fc1125f3273be32b8de3457d3ff4b3aae
Paul Gao <gaozihou@uw.edu>
2024-02-03 11:51:33 -0800

    save

--- software/spmd/vlsi24-sdr-prbs2/main.c
+++ software/spmd/vlsi24-sdr-prbs2/main.c
@@ -62,45 +62,55 @@ int main()
     );
   }
   
+  uint32_t random_data_0 = 0x4b296c82;
+  uint32_t random_data_1 = 0x75d4b8a5;
+  uint32_t random_data_2 = 0x73f3e966;
+  uint32_t random_data_3 = 0xe2b4dc6c;
+  uint32_t random_data_4 = 0x5f5a1d85;
+  uint32_t random_data_5 = 0x7f4854a6;
+  uint32_t random_data_6 = 0xd4a4e4f1;
+  uint32_t random_data_7 = 0x32673c0b;
+  if (__bsg_y % 4 == 1) {
+    random_data_0 = 0xd39d7a9e;
+    random_data_1 = 0x85d78695;
+    random_data_2 = 0xb38daa33;
+    random_data_3 = 0x3a20fe97;
+    random_data_4 = 0x7d3936c0;
+    random_data_5 = 0xc028d072;
+    random_data_6 = 0x63a788a1;
+    random_data_7 = 0xfaa2fa60;
+  } else if (__bsg_y % 4 == 2) {
+    random_data_0 = 0x7af9072e;
+    random_data_1 = 0xb0403600;
+    random_data_2 = 0x953e060e;
+    random_data_3 = 0x6e7073a4;
+    random_data_4 = 0xe88de494;
+    random_data_5 = 0x7c1c5286;
+    random_data_6 = 0x2161f2ef;
+    random_data_7 = 0x09598c20;
+  } else if (__bsg_y % 4 == 3) {
+    random_data_0 = 0x0345da72;
+    random_data_1 = 0x6ba9bfc4;
+    random_data_2 = 0x3eae6217;
+    random_data_3 = 0xefa1a0a9;
+    random_data_4 = 0xf2c991ae;
+    random_data_5 = 0x9e91caab;
+    random_data_6 = 0x69e182d0;
+    random_data_7 = 0x2056f8bb;
+  }
+  
   if (send_up || send_down) {
     for (uint32_t i = 0; i < REPEAT; i++) {
-      dest_addr[ 0] = 0x4b296c82;
-      dest_addr[ 1] = 0x75d4b8a5;
-      dest_addr[ 2] = 0x73f3e966;
-      dest_addr[ 3] = 0xe2b4dc6c;
-      dest_addr[ 4] = 0x5f5a1d85;
-      dest_addr[ 5] = 0x7f4854a6;
-      dest_addr[ 6] = 0xd4a4e4f1;
-      dest_addr[ 7] = 0x32673c0b;
-      dest_addr[ 8] = 0xd39d7a9e;
-      dest_addr[ 9] = 0x85d78695;
-      dest_addr[10] = 0xb38daa33;
-      dest_addr[11] = 0x3a20fe97;
-      dest_addr[12] = 0x7d3936c0;
-      dest_addr[13] = 0xc028d072;
-      dest_addr[14] = 0x63a788a1;
-      dest_addr[15] = 0xfaa2fa60;
+      dest_addr[ 0] = random_data_0;
+      dest_addr[ 1] = random_data_1;
+      dest_addr[ 2] = random_data_2;
+      dest_addr[ 3] = random_data_3;
+      dest_addr[ 4] = random_data_4;
+      dest_addr[ 5] = random_data_5;
+      dest_addr[ 6] = random_data_6;
+      dest_addr[ 7] = random_data_7;
       asm volatile("": : :"memory");
     }
-    //for (uint32_t i = 0; i < REPEAT; i++) {
-    //  dest_addr[16] = 0x7af9072e;
-    //  dest_addr[17] = 0xb0403600;
-    //  dest_addr[18] = 0x953e060e;
-    //  dest_addr[19] = 0x6e7073a4;
-    //  dest_addr[20] = 0xe88de494;
-    //  dest_addr[21] = 0x7c1c5286;
-    //  dest_addr[22] = 0x2161f2ef;
-    //  dest_addr[23] = 0x09598c20;
-    //  dest_addr[24] = 0x0345da72;
-    //  dest_addr[25] = 0x6ba9bfc4;
-    //  dest_addr[26] = 0x3eae6217;
-    //  dest_addr[27] = 0xefa1a0a9;
-    //  dest_addr[28] = 0xf2c991ae;
-    //  dest_addr[29] = 0x9e91caab;
-    //  dest_addr[30] = 0x69e182d0;
-    //  dest_addr[31] = 0x2056f8bb;
-    //  asm volatile("": : :"memory");
-    //}
   }
   
   if (read_up || read_down) {
@@ -119,67 +129,16 @@ int main()
       uint32_t val5  = dest_addr[ 5];
       uint32_t val6  = dest_addr[ 6];
       uint32_t val7  = dest_addr[ 7];
-      uint32_t val8  = dest_addr[ 8];
-      uint32_t val9  = dest_addr[ 9];
-      uint32_t val10 = dest_addr[10];
-      uint32_t val11 = dest_addr[11];
-      uint32_t val12 = dest_addr[12];
-      uint32_t val13 = dest_addr[13];
-      uint32_t val14 = dest_addr[14];
-      uint32_t val15 = dest_addr[15];
       asm volatile("": : :"memory");
-      if (val0  != 0x4b296c82) bsg_fail();
-      if (val1  != 0x75d4b8a5) bsg_fail();
-      if (val2  != 0x73f3e966) bsg_fail();
-      if (val3  != 0xe2b4dc6c) bsg_fail();
-      if (val4  != 0x5f5a1d85) bsg_fail();
-      if (val5  != 0x7f4854a6) bsg_fail();
-      if (val6  != 0xd4a4e4f1) bsg_fail();
-      if (val7  != 0x32673c0b) bsg_fail();
-      if (val8  != 0xd39d7a9e) bsg_fail();
-      if (val9  != 0x85d78695) bsg_fail();
-      if (val10 != 0xb38daa33) bsg_fail();
-      if (val11 != 0x3a20fe97) bsg_fail();
-      if (val12 != 0x7d3936c0) bsg_fail();
-      if (val13 != 0xc028d072) bsg_fail();
-      if (val14 != 0x63a788a1) bsg_fail();
-      if (val15 != 0xfaa2fa60) bsg_fail();
+      if (val0  != random_data_0) bsg_fail();
+      if (val1  != random_data_1) bsg_fail();
+      if (val2  != random_data_2) bsg_fail();
+      if (val3  != random_data_3) bsg_fail();
+      if (val4  != random_data_4) bsg_fail();
+      if (val5  != random_data_5) bsg_fail();
+      if (val6  != random_data_6) bsg_fail();
+      if (val7  != random_data_7) bsg_fail();
     }
-    //for (uint32_t i = 0; i < REPEAT/10; i++) {
-    //  uint32_t val16 = dest_addr[16];
-    //  uint32_t val17 = dest_addr[17];
-    //  uint32_t val18 = dest_addr[18];
-    //  uint32_t val19 = dest_addr[19];
-    //  uint32_t val20 = dest_addr[20];
-    //  uint32_t val21 = dest_addr[21];
-    //  uint32_t val22 = dest_addr[22];
-    //  uint32_t val23 = dest_addr[23];
-    //  uint32_t val24 = dest_addr[24];
-    //  uint32_t val25 = dest_addr[25];
-    //  uint32_t val26 = dest_addr[26];
-    //  uint32_t val27 = dest_addr[27];
-    //  uint32_t val28 = dest_addr[28];
-    //  uint32_t val29 = dest_addr[29];
-    //  uint32_t val30 = dest_addr[30];
-    //  uint32_t val31 = dest_addr[31];
-    //  asm volatile("": : :"memory");
-    //  if (val16 != 0x7af9072e) bsg_fail();
-    //  if (val17 != 0xb0403600) bsg_fail();
-    //  if (val18 != 0x953e060e) bsg_fail();
-    //  if (val19 != 0x6e7073a4) bsg_fail();
-    //  if (val20 != 0xe88de494) bsg_fail();
-    //  if (val21 != 0x7c1c5286) bsg_fail();
-    //  if (val22 != 0x2161f2ef) bsg_fail();
-    //  if (val23 != 0x09598c20) bsg_fail();
-    //  if (val24 != 0x0345da72) bsg_fail();
-    //  if (val25 != 0x6ba9bfc4) bsg_fail();
-    //  if (val26 != 0x3eae6217) bsg_fail();
-    //  if (val27 != 0xefa1a0a9) bsg_fail();
-    //  if (val28 != 0xf2c991ae) bsg_fail();
-    //  if (val29 != 0x9e91caab) bsg_fail();
-    //  if (val30 != 0x69e182d0) bsg_fail();
-    //  if (val31 != 0x2056f8bb) bsg_fail();
-    //}
   }
 
 
