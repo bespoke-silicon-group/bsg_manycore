6dc4bc499196efe01976b9d6db8c0a83a941adac
Dan Petrisko <petrisko@cs.washington.edu>
2024-02-04 19:17:37 -0800

    Adding ipoly definition

--- machines/platform.mk
+++ machines/platform.mk
@@ -1,11 +1,11 @@
 
-BSG_PLATFORM ?= vcs
+BSG_PLATFORM ?= verilator
 
 ifeq ($(BSG_PLATFORM),vcs)
 DEFAULT_MACHINES = pod_1x1 pod_1x1_hbm2 pod_4x4 pod_4x4_hbm2
 BSG_SIM_BASE = simv
 else ifeq ($(BSG_PLATFORM),verilator)
-DEFAULT_MACHINES = pod_1x1_4X2Y
+DEFAULT_MACHINES = pod_1x1_2X2Y
 BSG_SIM_BASE = simsc
 else ifeq ($(BSG_PLATFORM),xcelium)
 DEFAULT_MACHINES = pod_1x1 pod_1x1_hbm2 pod_4x4 pod_4x4_hbm2
