2825d9583a3def2438b6b5d58f8a20fdda17c193
Tommy Jung <tommy.dc.jung@gmail.com>
2024-11-26 11:46:00 -0800

    switch between amoadd and hw barrier

--- software/bsg_manycore_lib/bsg_cuda_lite_barrier.h
+++ software/bsg_manycore_lib/bsg_cuda_lite_barrier.h
@@ -8,20 +8,28 @@ extern "C" {
 #endif
 extern int *__cuda_barrier_cfg;
 
+#ifndef ENABLE_HW_BARRIER
+// amoadd sense variable in DMEM;
+int sense = 1;
+#endif
+
 /**
  * Initialize the tile-group barrier.
  * This function should only be called once for the lifetime of the tile-group.
  */
 static inline void bsg_barrier_hw_tile_group_init()
 {
-    int sense = 1;
+    #ifdef ENABLE_HW_BARRIER
+    // A temporary sense variable used for amoadd, which is used to set up hw barrier;
+    int sense0 = 1;
     // initalize csr
     int cfg = __cuda_barrier_cfg[1+__bsg_id];
     asm volatile ("csrrw x0, 0xfc1, %0" : : "r" (cfg));
     // reset Pi
     asm volatile ("csrrwi x0, 0xfc2, 0");
     // sync with amoadd barrier
-    bsg_barrier_amoadd(&__cuda_barrier_cfg[0], &sense);
+    bsg_barrier_amoadd(&__cuda_barrier_cfg[0], &sense0);
+    #endif
 }
 
 /**
@@ -29,8 +37,12 @@ static inline void bsg_barrier_hw_tile_group_init()
  */
 static inline void bsg_barrier_hw_tile_group_sync()
 {
+    #ifdef ENABLE_HW_BARRIER
     bsg_barsend();
     bsg_barrecv();
+    #else
+    bsg_barrier_amoadd(&__cuda_barrier_cfg[0], &sense);
+    #endif
 }
 #ifdef __cplusplus
 }
