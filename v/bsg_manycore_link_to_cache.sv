86a36c3b26d80a9f179fc44a01a43117648cb867
Yuan-Mao <ymchueh@gmail.com>
2025-06-10 13:02:30 -0700

    Fix meaningless logic

--- v/bsg_manycore_link_to_cache.sv
+++ v/bsg_manycore_link_to_cache.sv
@@ -372,7 +372,7 @@ module bsg_manycore_link_to_cache
         end
 
         cache_pkt.data = packet_lo.payload;
-        cache_pkt.mask = (packet_lo.op_v2 == e_remote_sw && e_remote_uncached_store)
+        cache_pkt.mask = (packet_lo.op_v2 == e_remote_sw)
           ? 4'b1111
           : packet_lo.reg_id.store_mask_s.mask;
         
