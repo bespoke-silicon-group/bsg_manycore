1afb9bf7beee98de8949005904979f44d5dab940
Michael Bedford Taylor <taylor-bsg@users.noreply.github.com>
2026-09-05 03:01:19 -0700

    Restore tag trace replay enable

--- testbenches/common/v/bsg_nonsynth_manycore_tag_master.sv
+++ testbenches/common/v/bsg_nonsynth_manycore_tag_master.sv
@@ -47,6 +47,7 @@ module bsg_nonsynth_manycore_tag_master
   ) tr (
     .clk_i(clk_i)
     ,.reset_i(reset_i)
+    ,.en_i(1'b1)
 
     ,.rom_addr_o(rom_addr)
     ,.rom_data_i(rom_data)
@@ -89,4 +90,3 @@ module bsg_nonsynth_manycore_tag_master
 endmodule
 
 `BSG_ABSTRACT_MODULE(bsg_nonsynth_manycore_tag_master)
-
